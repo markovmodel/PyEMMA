81e2f1571dddee8313e3bb78b1857479ac5836eb
mscherer <m.scherer@fu-berlin.de>
2013-09-24 15:05:37 +0200

    added packages

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 
+# prefer setuptools in favour of distutils
 try:
-    # prefer setuptools in favour of distutils
     from setuptools.core import setup
 except ImportError:
     from distutils.core import setup
@@ -11,8 +11,14 @@ setup(
       version='2.0',
       description='EMMA 2',
       url='http://compmolbio.biocomputing-berlin.de/index.php',
+      # think about this... maybe we can reduce redundancy in packages listing
       # package_dir={'emma2' : ''},  # all packages are in 'src/emma2' folder
-      packages=['emma2', 'emma2.msm'],  # list packages here
+      
+      # list packages here
+      packages=['emma2',
+                'emma2.msm.analyze',
+                'emma2.msm.estimate',
+                'emma2.pmm'],  
       # requires=['Pycluster']  # example dependency to Pycluster package, which may be used for kmeans etc.
       #test_suite='test.test_msmanalyze'
 )
