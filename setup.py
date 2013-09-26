5a7350d5f85a0b43f8980a8f5f6d7daab9c75c3c
mscherer <m.scherer@fu-berlin.de>
2013-09-26 15:31:51 +0200

    use py.test for testing

--- setup.py
+++ setup.py
@@ -6,19 +6,37 @@ try:
 except ImportError:
     from distutils.core import setup
 
+from setuptools.command.test import test as TestCommand
+import sys
+
+class PyTest(TestCommand):
+    """
+    little wrapper for pytest for direct usage with setup() function
+    """
+    def finalize_options(self):
+        TestCommand.finalize_options(self)
+        self.test_args = []
+        self.test_suite = True
+    def run_tests(self):
+        #import here, cause outside the eggs aren't loaded
+        import pytest
+        errno = pytest.main(self.test_args)
+        sys.exit(errno)
+
 setup(
       name='Emma2',
       version='2.0',
       description='EMMA 2',
       url='http://compmolbio.biocomputing-berlin.de/index.php',
-      # think about this... maybe we can reduce redundancy in packages listing
-      # package_dir={'emma2' : ''},  # all packages are in 'src/emma2' folder
+      author='',
       
       # list packages here
       packages=['emma2',
                 'emma2.msm',
                 'emma2.pmm'],
       # runtime dependencies
-      requires=['numpy'],
-      test_suite='emma2.test.testsuite'
+      requires=['numpy (>=1.7)'],
+      # testing dependencies
+      tests_require=['pytest'],
+      cmdclass = {'test': PyTest},
 )
