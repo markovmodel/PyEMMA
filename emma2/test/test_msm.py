6fd826a7cf62e7a538f21a482e9d8a33f8c955bb
mscherer <m.scherer@fu-berlin.de>
2013-10-01 16:25:03 +0200

    implemented is_transition_matrix and eigenvalues with test cases

--- emma2/test/test_msm.py
+++ emma2/test/test_msm.py
@@ -1,29 +1,51 @@
 '''
 Created on 24.09.2013
 
-@author: marscher
+.. moduleauthor: marscher <m.scherer@fu-berlin.de>
+
 '''
 import unittest
 import numpy as np
-from emma2.msm.estimate import *
+from scipy.sparse import coo_matrix
+from msm.analysis.api import *
 
 class _TestMSM(unittest.TestCase):
 
-    def test_is_transition_matrix(self):
+    def test_is_transition_matrix_dense(self):
         """
             tests the function msm.util.isTransitionMatrix(T)
         """
-        size = 10
+        size = 100
         # create count matrix with samples between [0, 100] and shape size^2
-        # and cast it to float
-        C = np.asarray(np.random.randint(0, 100, size=(size, size)), np.float, 2)
-        row_sums = C.sum(axis = 1)
-        for i in xrange(size):
-            C[i] /= row_sums[i]
+        # and cast it to double precision
+        C = np.asarray(np.random.randint(0, 100, size=(size, size)), np.float64, 2)
+        T = C / C.sum(axis=1)[ : , np.newaxis]
         
-        self.assert_(is_transitionmatrix(C), "T should be a transition matrix")
+        self.assert_(is_transition_matrix(T, 1e-15), "T should be a transition matrix")
+
+    def test_is_transition_matrix_sparse(self):
+        """
+            tests the function msm.util.isTransitionMatrix(T)
+        """
+        size = 100
+        # create count matrix with samples between [0, 100] and shape size^2
+        # and cast it to double precision
+        C = np.asarray(np.random.randint(0, 100, size=(size, size)), np.float64, 2)
+        T = C / C.sum(axis=1)[ : , np.newaxis]
+        
+        self.assert_(is_transition_matrix(T, 1e-15), "T should be a transition matrix")
 
 
+    def test_eigenvalues(self):
+        size = 50
+        #X = np.random.rand( size, size)
+        X = np.asarray([[2,0,0], [0,3,4], [0,4,9]])
+        print X
+        #print eigenvalues(X, (1,3))
+        print eigenvalues(X, 2)
+        print eigenvalues(X, 1) 
+        print eigenvalues(X)
+        
     def test_is_rate_matrix(self):
         l = 10
         mu = 3
