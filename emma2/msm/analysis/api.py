f5851945617d08300e37d930e441998d1aa45d48
Benjamin Trendelkamp-Schroer <trendelkamp@mi.fu-berlin.de>
2013-10-01 14:00:11 +0200

    New api definitions in subpackage/api.py.

---
+++ emma2/msm/analysis/api.py
@@ -0,0 +1,347 @@
+################################################################################
+# Assessment tools
+################################################################################
+
+# TODO: Implement in Python directly
+def is_transition_matrix(T, tol=1e-15):
+    """
+        True if T is a transition matrix
+    """
+    #return emma2.msm.util.isProbabilisticMatrix(T)
+
+# TODO: Implement in Python directly
+def is_rate_matrix(K, tol=1e-15):
+    """
+        True if K is a rate matrix
+    """
+
+# TODO: Implement in Python directly
+def is_ergodic(T, tol=1e-15):
+    """
+        True if T is connected (irreducible) and aperiodic
+    """
+
+# TODO: Implement in Python directly
+def is_reversible(T, mu=None, tol=1e-15):
+    """
+        True if T is a transition matrix
+        mu : tests with respect to this stationary distribution
+    """
+    
+    
+################################################################################
+# Eigenvalues and eigenvectors
+################################################################################
+
+# TODO: Implement in Python directly
+def mu(T):
+    """
+        compute the stationary distribution of T
+    """
+
+# TODO: Implement in Python directly
+def mu_sensitivity(T):
+    """
+        compute the sensitivity matrix of the stationary distribution of T
+    """
+
+# TODO: Implement in Python directly
+def statdist(T):
+    """
+        compute the stationary distribution of T
+    """
+
+# TODO: Implement in Python directly
+def statdist_sensitivity(T):
+    """
+        compute the sensitivity matrix of the stationary distribution of T
+    """    
+
+# TODO: Implement in Python directly
+def eigenvalues(T, k=None):
+    """
+        computes the eigenvalues
+
+        T : transition matrix
+        k : int (optional) or tuple of ints
+            Compute the first k eigenvalues of T.
+    """
+
+# TODO: Implement in Python directly
+def eigenvalues_sensitivity(T, k=None):
+    """
+        computes the sensitivity of the specified eigenvalue
+
+        k : int (optional)
+            Compute the sensitivity of the first k eigenvalues of T.
+    """
+
+# TODO: Implement in Python directly
+def timescales(T, tau=1, k=None):
+    """
+        T: transition matrix
+        tau: lag time
+        k : int (optional)
+            Compute the first k implied time scales of T.
+    """
+
+# TODO: Implement in Python directly
+def eigenvectors(T, k=None, right=True):
+    """Compute eigenvectors of T
+
+    k : int (optional)
+        Compute eigenvectors to 
+    right : bool
+        If True compute right eigenvectors, otherwise compute left eigenvectors.
+    """
+
+# TODO: Implement in Python directly
+def eigenvectors_sensitivity(T, k=None, right=True):
+    """Compute eigenvector snesitivity of T
+
+    k : int (optional)
+        Compute eigenvectors to 
+    right : bool
+        If True compute right eigenvectors, otherwise compute left eigenvectors.
+    """
+
+# TODO: Implement in Python directly
+def rdl_decomposition(T, k=None, norm='standard'):
+    """Compute eigenvector snesitivity of T
+
+        T : transition matrix    
+        k : int (optional)
+            Number of eigenvector/eigenvalue pairs
+        norm: standard | reversible
+            standard: (L'R) = Id, l[0] is a probability distribution, r[i] have a 2-norm of 1
+            reversible: R and L are related by stationary distribution of T
+            
+        Returns
+        -------
+        (R,D,L):    tuple of nd_arrays for right and left eigenvector matrix. Eigenvalues are passed as 1-d array
+    """
+    
+# TODO: Implement in Python directly
+def mfpt(T, i):
+    """
+    Computes vector of mean first passage times for given target state.
+
+    Parameters
+    ----------
+    P : ndarray, shape=(n,n) 
+        Transition matrix.
+    i : Integer
+        Target state for mfpt calculation.
+
+    Returns
+    -------
+    x : ndarray, shape=(n,)
+        Vector of mean first passage times.
+
+   """
+
+# TODO: Implement in Python directly
+def mfpt_sensitivity(T, i):
+    """
+    """
+
+
+################################################################################
+# Expectations
+################################################################################
+
+# TODO: Implement in Python directly
+def expectation(T, a):
+    """
+        computes the expectation value of a
+    """
+
+# TODO: Implement in Python directly
+def expectation_sensitivity(T, a):    
+    """
+        computes the sensitivity of the expectation value of a
+    """
+
+# TODO: Implement in Python directly
+def expected_counts(p0, T, N):
+   """
+   Expected transition counts for Markov chain with n steps. 
+   
+                                \sum_{k=0}^n-1 diag(p^{T} T^{k})*T  n \geq 1
+   E[C^{(n)}(x_0,\dotsc,x_n)]=   
+                                0                                  n=0
+
+   Parameters
+   ----------
+   p0 : numpy array, shape=(n,)
+      Starting (probability) vector of the chain, numpy.sum(p)=1.0.
+   T : numpy array, shape=(n,n)
+      Transition matrix for the chain. T\geq 0, numpy.sum(T,axis=1)=(1,...,1)
+   N : int
+      Number of steps for chain.
+
+   Returns
+   --------
+   EC : numpy array, shape=(n,n)
+       Expected value for transition counts after a propagation of n steps. 
+
+   """
+
+# TODO: Implement in Python directly
+def expected_counts_stationary(P, N, mu=None):
+   """
+   Expected transition counts for Markov chain in equilibrium. 
+
+   Since mu is stationary for T we have 
+
+      E(C^{(n)})(x_0,dotsc,x_{n-1})=n diag(mu)*T.
+
+   Parameters
+   -----------
+   P : numpy array, shape=(n,n)
+      Transition matrix for the chain. T\geq 0, numpy.sum(T,axis=1)=(1,...,1)
+   n : int
+      Number of steps for chain.
+   mu : numpy array, shape=(n,)
+      Stationary probability vector of the chain, numpy.sum(p)=1.0. 
+      If mu is not specified it will be computed via diagonalization of T.  
+
+   Returns:
+   --------
+   EC : numpy array, shape=(n,n)
+       Expected value for transition counts after a propagation of n steps. 
+
+   """
+
+################################################################################
+# Fingerprints
+################################################################################
+
+# TODO: Implement in Python directly
+def autocorrelation(P, obs):
+    """Compute dynamical fingerprint crosscorrelation.
+
+    The dynamical fingerprint autocorrelation is the timescale
+    amplitude spectrum of the autocorrelation of the given observables 
+    under the action of the dynamics P
+
+    Parameters
+    ----------
+    P : ndarray, shape=(n, n) or scipy.sparse matrix
+       Transition matrix
+    obs : ndarray, shape=(n,)
+        Vector representing observable on discrete states
+
+    Returns
+    -------
+
+    """
+
+# TODO: Implement in Python directly
+def crosscorrelation(P, obs1, obs2):
+    """Compute dynamical fingerprint crosscorrelation.
+
+    The dynamical fingerprint crosscorrelation is the timescale
+    amplitude spectrum of the crosscorrelation of the given observables 
+    under the action of the dynamics P
+
+    Parameters
+    ----------
+    P : ndarray, shape=(n, n) or scipy.sparse matrix
+       Transition matrix
+    obs1 : ndarray, shape=(n,)
+        Vector representing observable on discrete states
+    obs2 : ndarray, shape=(n,)
+        Vector representing observable on discrete states
+
+    Returns
+    -------
+
+    """
+
+# TODO: Implement in Python directly
+def perturbation(P, obs, p0):
+    """
+
+    Parameters
+    ----------
+    P : ndarray, shape=(n, n) or scipy.sparse matrix
+       Transition matrix
+    obs : ndarray, shape=(n,)
+        Vector representing observable on discrete states
+    p0 : ndarray, shape=(n,)
+        Vector of initial probabilities.
+
+    Returns :
+    ---------
+    
+    """
+
+################################################################################
+# PCCA
+################################################################################
+
+# TODO: Implement in Python directly
+def pcca(T, n):
+    """
+        returns a PCCA object
+        T: transition matrix
+        n: number of metastable processes
+    """
+
+
+################################################################################
+# Transition path theory
+################################################################################
+
+# TODO: Implement in Python directly
+def committor(P, A, B, forward=True):
+    """Compute the committor between sets of microstates.
+
+    Parameters
+    ----------
+    P : ndarray, shape=(n, n) or scipy.sparse matrix
+        Transition matrix
+    A : array_like
+        List of integer state labels for set A
+    B : array_like
+        List of integer state labels for set B
+    forward : bool
+        If True compute the forward committor, else
+        compute the backward committor.
+
+    Returns
+    -------
+    x : ndarray, shape=(n, )
+        Commitor vector.
+
+    """
+
+# TODO: Implement in Python directly
+def committor_sensitivity(P, A, B, forward=True):
+    """Compute the committor between sets of microstates.
+
+    Parameters
+    ----------
+    P : ndarray, shape=(n, n) or scipy.sparse matrix
+        Transition matrix
+    A : array_like
+        List of integer state labels for set A
+    B : array_like
+        List of integer state labels for set B
+    forward : bool
+        If True compute the forward committor, else
+        compute the backward committor.
+
+    Returns
+    -------
+    x : ndarray, shape=(n, )
+        Commitor vector.
+
+    """
+
+# TODO: Translate from stallone
+def tpt(T, A, B):
+    """
+        returns a TPT object
+    """
---
+++ emma2/msm/estimation/api.py
@@ -0,0 +1,272 @@
+################################################################################
+# Discrete trajectory IO
+################################################################################
+
+# TODO: Implement in Python directly
+def read_discrete_trajectory(filename):
+    """
+    Read one or multiple ascii textfiles, each containing a single column with integer
+    entries into a list of integers.
+
+    Parameters
+    ---------- 
+    filename : str or list of str
+        The filename of the discretized trajectory file. 
+        The filename can either contain the full or the 
+        relative path to the file.
+
+    Returns
+    -------
+    dtraj : array-like (or list of array-like)
+        A list with integer entries.
+    """
+
+# TODO: Implement in Python directly
+def read_dtraj(filename):
+    """
+    (short version of the above)
+    """
+
+################################################################################
+# Count matrix
+################################################################################
+
+# TODO: Implement in Python directly
+def count_matrix(dtraj, lag, sliding=True):
+    r"""Generate a count matrix in from a given list of integers.
+
+    Parameters
+    ----------
+    dtraj : array_like
+        Discretized trajectory
+    lag : int
+        Lagtime in trajectory steps
+    sliding : bool, optional
+        If true the sliding window approach 
+        is used for transition counting.
+
+    Returns
+    -------
+    C : scipy.sparse.coo_matrix
+        The countmatrix at given lag in coordinate list format.
+    
+    """
+
+# def count_matrix(dtrajs, lag=1, subset=None):
+#     """
+#         count matrix on the given set of states
+#         dtrajs: list of files or list of integer lists corresponding to discrete trajectories
+#         lag: lag time in time units
+#     """
+
+# TODO: Implement in Python directly
+def cmatrix(dtraj, lag, sliding=True):
+    r"""Generate a count matrix in from a given list of integers.
+
+    Parameters
+    ----------
+    dtraj : array_like
+        Discretized trajectory
+    lag : int
+        Lagtime in trajectory steps
+    sliding : bool, optional
+        If true the sliding window approach 
+        is used for transition counting.
+
+    Returns
+    -------
+    C : scipy.sparse.coo_matrix
+        The countmatrix at given lag in coordinate list format.
+        
+    """
+
+# TODO: Implement in Python directly
+def cmatrix_cores(dtraj, cores, lag, sliding=True):
+    r"""Generate a countmatrix for the milestoning process on the given core sets
+    """
+
+################################################################################
+# Connectivity
+################################################################################
+
+# TODO: Implement in Python directly
+def connected_sets(C):
+    """
+    Compute connected components for a directed graph with weights
+    represented by the given count matrix.
+
+    Parameters
+    ----------
+    C : scipy.sparse matrix 
+        Count matrix specifying edge weights.
+
+    Returns
+    -------
+    cc : list of lists of integers
+        Each entry is a list containing all vertices (states) in 
+        the corresponding connected component.
+
+    """
+
+# TODO: Implement in Python directly
+def largest_connected_set(C):
+    """
+    Compute connected components for a directed graph with weights
+    represented by the given count matrix.
+
+    Parameters
+    ----------
+    C : scipy.sparse matrix 
+        Count matrix specifying edge weights.
+
+    Returns
+    -------
+    lcc : list of integers
+        The largest connected component of the directed graph.
+
+    """
+
+# TODO: Implement in Python directly
+def connected_cmatrix(C):
+    """
+    Compute the count matrix of the largest connected set.
+
+    The input count matrix is used as a weight matrix for the
+    construction of a directed graph. The largest connected set of
+    the constructed graph is computed. Vertices belonging to the
+    largest connected component are used to generate a completely
+    connected subgraph. The weight matrix of the subgraph is the
+    desired completely connected count matrix.
+    
+    Parameters
+    ----------
+    C : scipy.sparse matrix 
+        Count matrix specifying edge weights.
+
+    Returns
+    -------
+    C_cc : scipy.sparse matrix
+        Count matrix of largest completely 
+        connected set of vertices (states)
+
+    """
+
+# TODO: Implement in Python directly
+def is_connected(C):
+    """Return true if C is a countmatrix for a completely connected process.    
+    """
+
+# TODO: Implement in Python directly
+def mapping(set):
+    """
+    Constructs two dictionaries that map from the set values to their indexes, and vice versa.
+    
+    Parameters
+    ----------
+    set : array-like of integers 
+
+    Returns
+    -------
+    dict : python dictionary mapping original to internal states 
+    dict : python dictionary mapping internal to original states 
+
+    """
+    
+
+
+################################################################################
+# Transition matrix
+################################################################################
+
+# TODO: Implement in Python directly (Nonreversible)
+# TODO: Implement in Python directly (Reversible with stat dist)
+# TODO: Map to Stallone (Reversible)
+def transition_matrix(C, reversible=False, mu=None, **kwargs):
+    """
+    Estimate the transition matrix from the given countmatrix.
+
+    The transition matrix is a maximum likelihood estimate (MLE)
+    of the probability distribution of transition matrices
+    with parameters given by the countmatrix. 
+
+    Parameters
+    ----------
+    C : scipy.sparse matrix
+        Count matrix
+    reversible : bool (optional)
+        If True restrict the ensemble of transition matrices
+        to those having a detailed balance symmetry otherwise
+        the likelihood optimization is carried out over the whole
+        space of stochastic matrices.
+    mu : array_like
+        The stationary distribution of the MLE transition matrix.
+    **kwargs: Optional algorithm-specific parameters
+
+    Returns
+    -------
+    P : numpy ndarray, shape=(n, n) or scipy.sparse matrix
+       The MLE transition matrix
+
+    """
+
+# TODO: Implement in Python directly (Nonreversible)
+# TODO: Implement in Python directly (Reversible with stat dist)
+# TODO: Map to Stallone (Reversible)
+def tmatrix(C, reversible=False, mu=None):
+    r"""Estimate the transition matrix from the given countmatrix.
+    """
+
+# TODO: Implement in Python directly
+def tmatrix_cov(C, k=None):
+    """
+    Computes a nonreversible covariance matrix of transition matrix elments
+    
+    Parameters
+    ----------
+    C : scipy.sparse matrix
+        Count matrix
+    k : row index (optional). 
+        If set, only the covariance matrix for this row is returned.
+        
+    """
+    
+# TODO: Implement in Python directly
+def log_likelihood(C, T):
+    """
+        likelihood of C given T
+    """
+    
+# TODO: Implement in Python directly
+def error_perturbation(C, sensitivity):
+    """
+        C: count matrix
+        sensitivity: sensitivity matrix or tensor of size (m x n x n) where m is the dimension of the target quantity and (n x n) is the size of the transition matrix.
+        The sensitivity matrix should be evaluated at an appropriate maximum likelihood or mean of the transition matrix estimated from C.
+        returns: (m x m) covariance matrix of the target quantity 
+    """
+
+# TODO: Map to Stallone (Reversible)
+def tmatrix_sampler(C, reversible=False, mu=None, P0=None):
+    """
+    Parameters
+    ----------
+    C : ndarray, shape=(n, n) or scipy.sparse matrix
+        Count matrix    
+    reversible : bool
+        If true sample from the ensemble of transition matrices
+        restricted to those obeying a detailed balance condition,
+        else draw from the whole ensemble of stochatic matrices.
+    mu : array_like
+        The stationary distribution of the transition matrix samples.
+    P0 : ndarray, shape=(n, n) or scipy.sparse matrix
+        Starting point of the MC chain of the sampling algorithm.
+        Has to obey the required constraints.    
+
+    Returns :
+    ---------
+    sampler : A TransitionMatrixSampler object.
+        
+    """
+
+
+
