16d7478a544a80fff0f12d57cb77b9c244e6e254
Benjamin Trendelkamp-Schroer <trendelkamp@mi.fu-berlin.de>
2013-10-04 14:39:01 +0200

    Implemented sparse assessment routine is_transition_matrix

--- emma2/msm/analysis/sparse/assessment.py
+++ emma2/msm/analysis/sparse/assessment.py
@@ -1,4 +1,30 @@
-from scipy.sparse import coo_matrix
+import numpy as np
+  
+def is_transition_matrix(T, tol=1e-15):
+    """
+    True if T is a transition matrix
+    
+    Parameters
+    ----------
+    T : sipy.sparse matrix
+        Matrix to check
+    tol : float
+        tolerance to check with
+    
+    Returns
+    -------
+    Truth value: bool
+        True, if T is positive and normed
+        False, otherwise
+    
+    """
+    T=T.tocsr() # compressed sparse row for fast row slicing
+    values=T.data # non-zero entries of T
 
-def _is_stochastic_matrix_impl(T, tol):
-    raise NotImplementedError
+    """Check entry-wise positivity"""
+    is_positive=np.allclose(values, np.abs(values), rtol=0.0, atol=tol)
+
+    """Check row normalization"""
+    is_normed=np.allclose(T.sum(axis=1), 1.0, rtol=0.0, atol=tol)
+
+    return is_positive and is_normed
