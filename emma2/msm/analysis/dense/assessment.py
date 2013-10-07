e94dcecbd82978c15a4f0e5b31164715480435b4
mscherer <m.scherer@fu-berlin.de>
2013-10-07 16:44:33 +0200

    impled is_rate_matrix

--- emma2/msm/analysis/dense/assessment.py
+++ emma2/msm/analysis/dense/assessment.py
@@ -1,7 +1,32 @@
 import numpy as np
 
-def _is_stochastic_matrix_impl(T, tol=1e-15):
+def is_stochastic_matrix(T, tol=1e-15):
     dim = T.shape[0]
     X = np.abs(T) - T
     x = np.sum(T, axis = 1)
     return np.abs(x - np.ones(dim)).max() < dim * tol and X.max() < 2.0 * tol
+
+
+def is_rate_matrix(K, tol=1e-15):
+    """
+    True if K is a rate matrix
+    Parameters
+    ----------
+    K : scipy.sparse matrix
+        Matrix to check
+    tol : float
+        tolerance to check with
+        
+    Returns
+    -------
+    Truth value : bool
+        True, if K negated diagonal is positive and row sums up to zero.
+        False, otherwise
+    """
+    R = K - K.diagonal()
+    off_diagonal_positive = np.allclose(R, abs(R), 0.0, tol)
+    
+    row_sum = K.sum( axis = 1)
+    row_sum_eq_1 = np.allclose(row_sum, 1.0, 0.0, tol)
+    
+    return off_diagonal_positive and row_sum_eq_1
