701aa9eb3170f295187207c9507e8ad36a0b30f8
mscherer <m.scherer@fu-berlin.de>
2013-10-07 16:44:33 +0200

    impled is_rate_matrix

--- emma2/msm/analysis/sparse/assessment.py
+++ emma2/msm/analysis/sparse/assessment.py
@@ -1,12 +1,12 @@
 import numpy as np
   
-def is_transition_matrix(T, tol=1e-15):
+def is_transition_matrix(T, tol):
     """
     True if T is a transition matrix
     
     Parameters
     ----------
-    T : sipy.sparse matrix
+    T : scipy.sparse matrix
         Matrix to check
     tol : float
         tolerance to check with
@@ -28,3 +28,30 @@ def is_transition_matrix(T, tol=1e-15):
     is_normed=np.allclose(T.sum(axis=1), 1.0, rtol=0.0, atol=tol)
 
     return is_positive and is_normed
+
+
+def is_rate_matrix(K, tol):
+    """
+    True if K is a rate matrix
+    Parameters
+    ----------
+    K : scipy.sparse matrix
+        Matrix to check
+    tol : float
+        tolerance to check with
+        
+    Returns
+    -------
+    Truth value : bool
+        True, if K negated diagonal is positive and row sums up to zero.
+        False, otherwise
+    """
+    values = K.data
+    org_diag = K.diagonal().copy()
+    diag = K.diagonal()
+    diag[:] = 0
+    
+    gt_zero = np.allclose(values, 0.0, atol = tol, rtol = np.inf)
+    diag = org_diag
+    
+    return gt_zero
