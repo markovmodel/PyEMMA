6d48499c8249487a126b05668d82a0eba853f4f6
marscher <m.scherer@fu-berlin.de>
2013-10-09 17:08:05 +0200

    * fixed typo in slicing for matrix vector product in is_stochastic_matrix. * added docstring for is_stochastic_matrix * changed tolerance checking in is_rate_matrix to absolute

--- emma2/msm/analysis/dense/assessment.py
+++ emma2/msm/analysis/dense/assessment.py
@@ -25,18 +25,39 @@ def is_rate_matrix(K, tol=1e-15):
         False, otherwise
     """
     R = K - K.diagonal()
-    off_diagonal_positive = np.allclose(R, abs(R), 0.0, tol)
+    off_diagonal_positive = np.allclose(R, abs(R), 0.0, atol=tol)
     
     row_sum = K.sum(axis = 1)
-    row_sum_eq_0 = np.allclose(row_sum, 0.0, rtol=0.0, atol=tol)
+    row_sum_eq_0 = np.allclose(row_sum, 0.0, atol=tol)
     
     return off_diagonal_positive and row_sum_eq_0
 
 def is_reversible(T, mu=None, tol):
+    r"""
+        checks whether T is reversible in terms of given stationary distribution.
+        If no distribution is given, it will be calculated out of T.
+        
+        performs follwing check:
+        :math:`\pi_i P_{ij} = \pi_j P_{ji}
+        Parameters
+        ----------
+        T : numpy.ndarray matrix
+            Transition matrix
+        mu : numpy.ndarray vector
+            stationary distribution
+        tol : float
+            tolerance to check with
+            
+        Returns
+        -------
+        Truth value : bool
+            True, if T is a stochastic matrix
+            False, otherwise
+    """
     if is_stochastic_matrix(T, tol):
         if mu is None:
             mu = mu(T)
-        
-        return np.allclose( T * mu[: np.newaxis ], T*mu, ltol=tol)
+        return np.allclose(T * mu[ : , np.newaxis ], \
+                           T[ : , np.newaxis] * mu,  atol=tol)
     else:
         raise ValueError("given matrix is not a valid transition matrix.")
