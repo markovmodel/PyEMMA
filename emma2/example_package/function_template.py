97e1e4370c56965533c002fddcd2662299de8964
Chris Wehmeyer <christoph.wehmeyer@fu-berlin.de>
2013-10-10 11:58:28 +0200

    fixed docstring

--- emma2/example_package/function_template.py
+++ emma2/example_package/function_template.py
@@ -1,35 +1,35 @@
 """
-    module doc string goes here?
+module doc string goes here?
 """
 from numpy import *
 
 def check_nonnegativity(A):
-    """
+    r"""
     Checks nonnegativity of a matrix.
 
     Matrix A=(a_ij) is nonnegative if 
-
+    
         a_ij>=0 for all i, j.
-
-    Parameters :
-    ------------
+    
+    Parameters
+    ----------
     A : ndarray, shape=(M, N)
         The matrix to test.
-
-    Returns :
-    ---------
+    
+    Returns
+    -------
     nonnegative : bool
         The truth value of the nonnegativity test.
-
-    Notes :
-    -------
+    
+    Notes
+    -----
     The nonnegativity test is performed using
     boolean ndarrays.
-
+    
     Nonnegativity is import for transition matrix estimation.
-
-    Examples :
-    ----------
+    
+    Examples
+    --------
     >>> import numpy as np
     >>> A=np.array([[0.4, 0.1, 0.4], [0.2, 0.6, 0.2], [0.3, 0.3, 0.4]])
     >>> x=check_nonnegativity(A)
@@ -40,7 +40,7 @@ def check_nonnegativity(A):
     >>> x=check_nonnegativity(A)
     >>> x
     False
-
+    
     """
     ind=(A<=0.0)
     return sum(ind)==0
