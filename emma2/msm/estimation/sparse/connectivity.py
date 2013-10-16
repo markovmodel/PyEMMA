5952de4f29b07c5990a0dc25bec8364bbf67ac69
Benjamin Trendelkamp-Schroer <benjamin.trendelkamp-schroer@fu-berlin.de>
2013-10-16 16:58:30 +0200

    Added largest_connected_set and connected_count_matrix method

--- emma2/msm/estimation/sparse/connectivity.py
+++ emma2/msm/estimation/sparse/connectivity.py
@@ -46,15 +46,66 @@ def connected_sets(C):
     csum=np.zeros(len(count)+1)
     csum[1:]=np.cumsum(count)
 
-    """Generate list containing components"""
+    """Generate list containing components, sort each conponent by
+    increasing state label"""
     cc=[]
     for i in range(nc):
-        cc.append(states[csum[i]:csum[i+1]])
+        cc.append(np.sort(states[csum[i]:csum[i+1]]))
     
     """Sort by size of component - largest component first"""
     cc=sorted(cc, key=lambda x: -len(x))
 
     return cc
 
-    
+def largest_connected_set(C):
+    r"""Compute connected components for a directed graph with weights
+    represented by the given count matrix.
 
+    Parameters
+    ----------
+    C : scipy.sparse matrix 
+        Count matrix specifying edge weights.
+
+    Returns
+    -------
+    lcc : array of integers
+        The largest connected component of the directed graph.
+
+    """
+    return connected_sets(C)[0]
+
+def connected_count_matrix(C):
+    r"""Compute the count matrix of the largest connected set.
+
+    The input count matrix is used as a weight matrix for the
+    construction of a directed graph. The largest connected set of the
+    constructed graph is computed. Vertices belonging to the largest
+    connected component are used to generate a completely connected
+    subgraph. The weight matrix of the subgraph is the desired
+    completely connected count matrix.
+    
+    Parameters
+    ----------
+    C : scipy.sparse matrix 
+        Count matrix specifying edge weights.
+
+    Returns
+    -------
+    C_cc : scipy.sparse matrix
+        Count matrix of largest completely 
+        connected set of vertices (states)
+
+    """
+    lcc=largest_connected_set(C)
+    
+    """Row slicing"""
+    C_cc=C.tocsr()
+    C_cc=C_cc[lcc, :]
+
+    """Column slicing"""
+    C_cc=C_cc.tocsc()
+    C_cc=C_cc[:, lcc]
+
+    return C_cc.tocoo()
+
+    
