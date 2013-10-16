4b0f44b55cf881b3d9b965bb1957457ed0dac35d
marscher <m.scherer@fu-berlin.de>
2013-10-16 17:23:28 +0200

    * renamed util/logging.py -> util/log.py * use logger in stallone.py

--- emma2/util/stallone.py
+++ emma2/util/stallone.py
@@ -5,24 +5,25 @@ Created on 15.10.2013
 
 @author: marscher
 '''
+from log import log
 
 """is the stallone python binding available?"""
 stallone_available = None
 jenv = None
 try:
-    #log.debug('try to initialize stallone module')
+    log.debug('try to initialize stallone module')
     import pystallone as stallone
     from pystallone.ArrayWrapper import ArrayWrapper as _ArrayWrapper
     # add ArrayWrapper to stallone module 
     stallone.ArrayWrapper = _ArrayWrapper
     jenv = stallone.initVM(initialheap='32m', maxheap='512m')
     stallone_available = True
+    log.debug('stallone initialized successfully.')
 except ImportError:
-    #log.error('not found.')
-    print "stallone could not be found."
+    log.error('stallone could not be found.')
     stallone_available = False
 except ValueError as ve:
     stallone_available = False
-    print "java vm initialization for stallone went wrong: ", ve
-except stallone.JavaError as je:
-    print 'java exception thrown: ', je
+    log.error('java vm initialization for stallone went wrong: ', ve)
+except:
+    log.error('unknown exception occured.')
