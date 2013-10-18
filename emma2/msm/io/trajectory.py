7904a61a282c566e57993544cd26c664f13e733d
Benjamin Trendelkamp-Schroer <benjamin.trendelkamp-schroer@fu-berlin.de>
2013-10-18 12:31:59 +0200

    Added implementation and test for ascii discrete trajectory io

---
+++ emma2/msm/io/trajectory.py
@@ -0,0 +1,59 @@
+"""This module implements IO function for discrete state trajectory files
+
+Discrete trajectories are assumed to be stored either as single column ascii files
+or as ndarrays of shape (n,) in binary .npy format."""
+
+import numpy as np
+import scipy.sparse
+
+def read_discrete_trajectory(filename):
+    r"""Read one or multiple ascii textfiles,
+    each containing a single column with integer
+    entries into a list of integers.
+    
+    Parameters
+    ---------- 
+    filename : str or list of str
+        The filename of the discretized trajectory file. 
+        The filename can either contain the full or the 
+        relative path to the file.
+    
+    Returns
+    -------
+    dtraj : array-like (or list of array-like)
+        A list with integer entries.
+    
+    """
+    if isinstance(filename, str):
+        dtraj=read_discrete_trajectory_single(filename)
+        return dtraj
+    else:
+        dtraj=[]
+        for name in filename:
+            dtraj.append(read_discrete_trajectory_single(name))
+        return dtraj        
+    
+def read_discrete_trajectory_single(filename):
+    r"""Read one ascii text file
+
+    The file contains a single column with
+    integer entries.
+
+    Parameters
+    ---------- 
+    filename : str or list of str
+        The filename of the discretized trajectory file. 
+        The filename can either contain the full or the 
+        relative path to the file.
+
+    Returns
+    -------
+    dtraj : array
+        An array with integer entries.
+        
+    """
+    f=open(filename, "r")
+    lines=f.read()
+    dtraj=np.fromstring(lines, dtype=int, sep="\n")
+    return dtraj
+
