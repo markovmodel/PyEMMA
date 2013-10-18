4837d8c9dee4dace88d677baada2c170720f0c3f
marscher <m.scherer@fu-berlin.de>
2013-10-18 19:37:03 +0200

    impl wrapper for ITPT class of stallone, which performs automatic array wrapping for all getter of the java class

---
+++ emma2/msm/analysis/_impl.py
@@ -0,0 +1,120 @@
+'''
+Created on 18.10.2013
+
+@author: marscher
+'''
+import emma2.util.stallone
+from emma2.util.stallone import ArrayWrapper
+
+class TPT():
+    """
+        This class wraps around a stallone ITPTFlux class.
+        Performs internal array wrapping between python and java (bad performance)
+
+    """
+    def __init__(self, T, A, B):
+        """
+        initializes this with given matrix
+        Parameters
+        ----------
+        T : Transition matrix
+            ndarray
+        A : set of states
+        # TODO: shape?
+            ndarray( dtype = int, shape=?)
+        B : set of states 
+            ndarray( dtype = int)
+        """
+        try:
+            A = stallone.ndarray_to_stallone_array(A)
+            B = stallone.ndarray_to_stallone_array(B)
+            T = stallone.ndarray_to_stallone_array(T)
+            self.ITPT = stallone.API.msmNew.createTPT(T, A, B)
+        except stallone.JavaError as je:
+            raise RuntimeError(je.getJavaException().getStackTrace())
+    
+    def getBackwardCommittor(self):
+        """
+        Returns
+        -------
+        Backward Committor : ndarray
+        """
+        return ArrayWrapper(self.ITPT.getBackwardCommittor())
+    
+    def getFlux(self):
+        """
+        Returns
+        -------
+        Flux : ndarray
+        """
+        return ArrayWrapper(self.ITPT.getFlux())
+    
+    def getForwardCommittor(self):
+        """
+        Returns
+        -------
+        Forward Committor : ndarray
+        
+        """
+        return ArrayWrapper(self.ITPT.getForwardCommittor())
+    
+    def getNetFlux(self):
+        """
+        Returns
+        -------
+        Net flux : ndarray
+        
+        """
+        return ArrayWrapper(self.ITPT.getNetFlux())
+    
+    def getRate(self):
+        """
+        Returns
+        -------
+        Rate : float        
+        """
+        return self.ITPT.getRate()
+    
+    def getStationaryDistribution(self):
+        """
+        Returns
+        -------
+        Stationary distribution : ndarray
+        
+        """
+        return ArrayWrapper(self.ITPT.getStationaryDistribution())
+    
+    def getTotalFlux(self):
+        """
+        Returns
+        -------
+        Total flux : float
+        
+        """
+        return self.ITPT.getTotalFlux()
+        
+    def setRateMatrix(self, K):
+        """
+        Parameters
+        ----------
+        K (Rate matrix) : ndarray
+        
+        """
+        self.K = stallone.ndarray_to_stallone_array(K)
+        
+    def setStationaryDistribution(self, pi):
+        """
+        Parameters
+        ----------
+        pi : stationary distribution
+        
+        """
+        self.pi = stallone.ndarray_to_stallone_array(pi)
+        
+    def setTransitionMatrix(self, T):
+        """
+        Parameters
+        ----------
+        T : ndarray
+        """
+        self.T = stallone.ndarray_to_stallone_array(T)
