7446044c9d41c8e75f208f575fe34e2297cd0e6d
marscher <m.scherer@fu-berlin.de>
2013-10-20 23:29:27 +0200

    [util] logging filename changed to parent path of emma2 module (may not work in installed environment) - WIP hack

--- emma2/util/log.py
+++ emma2/util/log.py
@@ -6,6 +6,7 @@ Created on 15.10.2013
 # import log levels
 from logging import DEBUG, INFO, WARN, ERROR, CRITICAL
 import logging
+import emma2 as _emma2
 
 # import log levels to this namespace
 """
@@ -20,10 +21,13 @@ import logging
 log = logging.getLogger('emma2')
 
 # set up logging to file - see previous section for more details
+# TODO: martin prepend the path of emma2 module (+ '..') to filename
+# and think about putting these options to an ini file etc.
+_filename = _emma2.__path__[0] + '/../emma2.log'
 logging.basicConfig(level=logging.DEBUG,
             format='%(asctime)s %(name)-12s %(levelname)-8s %(message)s',
             datefmt='%m-%d %H:%M:%S',
-            filename='emma2.log',
+            filename=_filename,
             filemode='w')
 # define a Handler which writes INFO messages or higher to the sys.stderr
 _console = logging.StreamHandler()
