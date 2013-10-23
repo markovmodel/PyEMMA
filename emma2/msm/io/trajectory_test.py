ad4f0861af55778489d2cc1b1f872d85ec2418e3
Benjamin Trendelkamp-Schroer <benjamin.trendelkamp-schroer@fu-berlin.de>
2013-10-23 13:49:05 +0200

    Added ascii and binary trajectory IO implementations and tests

--- emma2/msm/io/trajectory_test.py
+++ emma2/msm/io/trajectory_test.py
@@ -3,46 +3,23 @@ import os
 import unittest
 
 import numpy as np
-import scipy.sparse 
 
 import trajectory
 
-class TestReadDiscreteTrajectorySingle(unittest.TestCase):
+class TestReadDiscreteTrajectory(unittest.TestCase):
     
     def setUp(self):
-        self.name='test/dtraj1.dat'
-        # self.name2='test/dtraj2.dat'
-
-    def tearDown(self):
-        pass
-
-    def test_read_discrete_trajectory_single(self):
-        dtraj_np=np.loadtxt(self.name, dtype=int)
-        dtraj=trajectory.read_discrete_trajectory_single(self.name)
-        self.assertTrue(np.all(dtraj_np==dtraj))
-
-class TestReadDiscreteTrajectory(unittest.TestCase):
-    def setUp(self):
-        self.name1='test/dtraj1.dat'
-        self.name2='test/dtraj2.dat'
-        self.names=[self.name1, self.name2]
-        self.dtraj1_np=np.loadtxt(self.name1, dtype=int)
-        self.dtraj2_np=np.loadtxt(self.name2, dtype=int)
-        self.dtraj_np=[self.dtraj1_np, self.dtraj2_np]
-
+        self.filename='test/dtraj.dat'
 
     def tearDown(self):
         pass
 
     def test_read_discrete_trajectory(self):
-        dtraj=trajectory.read_discrete_trajectory(self.name1)
-        self.assertTrue(np.all(self.dtraj1_np==dtraj))
+        dtraj_np=np.loadtxt(self.filename, dtype=int)
+        dtraj=trajectory.read_discrete_trajectory(self.filename)
+        self.assertTrue(np.all(dtraj_np==dtraj))
 
-        dtraj=trajectory.read_discrete_trajectory(self.names)
-        for i in range(len(dtraj)):
-            self.assertTrue(np.all(self.dtraj_np[i]==dtraj[i]))
-
-class TestWriteDiscreteTrajectorySingle(unittest.TestCase):
+class TestWriteDiscreteTrajectory(unittest.TestCase):
     def setUp(self):
         self.filename='test/out_dtraj.dat'
         self.dtraj=np.arange(10000)
@@ -50,34 +27,37 @@ class TestWriteDiscreteTrajectorySingle(unittest.TestCase):
     def tearDown(self):
         os.remove(self.filename)
 
-    def test_write_discrete_trajectory_single(self):
-        trajectory.write_discrete_trajectory_single(self.filename, self.dtraj)
+    def test_write_discrete_trajectory(self):
+        trajectory.write_discrete_trajectory(self.filename, self.dtraj)
         dtraj_n=np.loadtxt(self.filename)
         self.assertTrue(np.all(dtraj_n==self.dtraj))
 
-class TestWriteDiscreteTrajectory(unittest.TestCase):
+class TestLoadDiscreteTrajectory(unittest.TestCase):
+    
     def setUp(self):
-        self.filename1='test/out_dtraj1.dat'
-        self.dtraj1=np.arange(10000)
-        self.filename2='test/out_dtraj2.dat'
-        self.dtraj2=np.arange(20000)    
-        
-        self.filenames=[self.filename1, self.filename2]
-        self.dtraj=[self.dtraj1, self.dtraj2]
+        self.filename='test/dtraj.npy'
 
     def tearDown(self):
-        os.remove(self.filename1)
-        os.remove(self.filename2)
+        pass
 
-    def test_write_discrete_trajectory(self):
-        trajectory.write_discrete_trajectory(self.filename1, self.dtraj1)
-        dtraj_n=np.loadtxt(self.filename1)
-        self.assertTrue(np.all(dtraj_n==self.dtraj1))
+    def test_load_discrete_trajectory(self):
+        dtraj_n=np.load(self.filename)
+        dtraj=trajectory.load_discrete_trajectory(self.filename)
+        self.assertTrue(np.all(dtraj_n==dtraj))
 
-        trajectory.write_discrete_trajectory(self.filenames, self.dtraj)
-        for i in range(len(self.filenames)):
-            dtraj_n=np.loadtxt(self.filenames[i])
-            self.assertTrue(np.all(dtraj_n==self.dtraj[i]))
+class TestSaveDiscreteTrajectory(unittest.TestCase):
+    
+    def setUp(self):
+        self.filename='test/out_dtraj.npy'
+        self.dtraj=np.arange(10000)
+
+    def tearDown(self):
+        os.remove(self.filename)
+
+    def test_save_discrete_trajectory(self):
+        trajectory.save_discrete_trajectory(self.filename, self.dtraj)
+        dtraj_n=np.load(self.filename)
+        self.assertTrue(np.all(dtraj_n==self.dtraj))
 
 if __name__=="__main__":
     unittest.main()
