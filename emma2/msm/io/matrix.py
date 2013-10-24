9a36c2044ede785fecc8335c3f2ce0299258c4cf
Benjamin Trendelkamp-Schroer <benjamin.trendelkamp-schroer@fu-berlin.de>
2013-10-24 13:58:30 +0200

    Added matrix IO implementations for sparse matrices

--- emma2/msm/io/matrix.py
+++ emma2/msm/io/matrix.py
@@ -4,7 +4,7 @@ import numpy as np
 import scipy.sparse
 
 def is_integer(x):
-    """Check if elements of array are integers
+    """Check if elements of an array can be represented by integers.
     
     Parameters 
     ----------
@@ -14,38 +14,57 @@ def is_integer(x):
     Returns
     -------
     is_int : ndarray of bool
-        is_int[i] is True if x[i] is int otherwise False.
+        is_int[i] is True if x[i] can be represented
+        as int otherwise is_int[i] is False.
         
     """
-    return np.equal(np.mod(x, 1), 0)
+    is_int=np.equal(np.mod(x, 1), 0)
+    return is_int
     
 def read_matrix_dense(filename, dtype=float, comments='#'):
     A=np.loadtxt(filename, dtype=dtype, comments=comments)
     return A
 
+def write_matrix_dense(filename, A, fmt='%.18e', header='', comments='#'):
+    np.savetxt(filename, A, fmt=fmt, header=header, comments=comments)
+
 def read_matrix_sparse(filename, dtype=float, comments='#'):
-    coo=np.loadtxt(filename, comments=comments)
+    coo=np.loadtxt(filename, comments=comments, dtype=dtype)
     
     """Check if coo is (M, 3) ndarray"""
     if len(coo.shape)==2 and coo.shape[1]==3:
-        row=coo_data[:, 0]
-        col=coo_data[:, 1]
+        row=coo[:, 0]
+        col=coo[:, 1]
+        values=coo[:, 2]
 
-        """Convert values to specified data-type"""
-        values=coo_data[:, 2].astype(dtype)
+        """Check if imaginary part of row and col is zero"""
+        if np.all(np.isreal(row)) and np.all(np.isreal(col)):
+            row=row.real
+            col=col.real
 
-        """Check if first and second column contain integer entries"""
-        if np.all(is_integer(row)) and np.all(is_integer(col)):           
+            """Check if first and second column contain only integer entries"""
+            if np.all(is_integer(row)) and np.all(is_integer(col)):           
 
-            """Convert row and col to int"""
-            row=row.astype(int)
-            col=col.astype(int)
+                """Convert row and col to int"""
+                row=row.astype(int)
+                col=col.astype(int)
 
-            """Create coo-matrix"""
-            A=scipy.sparse.coo_matrix((values,(row, col)))
-            return A
+                """Create coo-matrix"""
+                A=scipy.sparse.coo_matrix((values,(row, col)))
+                return A
+            else:
+                raise ValueError('File contains non-integer entries for row and col.')        
         else:
-            raise ValueError('File contains non-integer entries for row and col.')
+            raise ValueError('File contains complex entries for row and col.')
     else:
         raise ValueError('Given file is not a sparse matrix in coo-format.')
 
+def write_matrix_sparse(filename, A, fmt='%.18e', header='', comments='#'):
+
+    if scipy.sparse.issparse(A):
+        """Convert to coordinate list (coo) format"""
+        A=A.tocoo()
+        coo=np.transpose(np.vstack((A.row, A.col, A.data)))
+        np.savetxt(filename, coo, fmt=fmt, header=header, comments=comments)
+    else:
+        raise ValueError('The given matrix is not a sparse matrix')
--- emma2/msm/io/matrix_test.py
+++ emma2/msm/io/matrix_test.py
@@ -1,5 +1,5 @@
 """Unit tests for matrix io implementations"""
-
+import os
 import unittest
 
 import numpy as np
@@ -7,17 +7,20 @@ import scipy.sparse
 
 import matrix
 
+################################################################################
+# ascii
+################################################################################
+
+################################################################################
+# dense
+################################################################################
+
 class TestReadMatrixDense(unittest.TestCase):
     
     def setUp(self):
-        self.filename_int='test/matrix_dense_int.dat'
-        self.filename_float='test/matrix_dense_float.dat'
-        self.filename_complex='test/matrix_dense_complex.dat'
-        
-        # self.A_int=np.arange(3*3).reshape(3, 3)
-        # self.A_float=1.0*self.A_int
-        # self.A_complex=np.arange(3*3).reshape(3, 3)+\
-        #     1j*np.arange(9,3*3+9).reshape(3, 3)
+        self.filename_int='test/matrix_int.dat'
+        self.filename_float='test/matrix_float.dat'
+        self.filename_complex='test/matrix_complex.dat'        
 
         self.A_int=np.loadtxt(self.filename_int, dtype=np.int)
         self.A_float=np.loadtxt(self.filename_float, dtype=np.float)
@@ -36,6 +39,163 @@ class TestReadMatrixDense(unittest.TestCase):
         A=matrix.read_matrix_dense(self.filename_complex, dtype=np.complex)
         self.assertTrue(np.all(A==self.A_complex))
         
+class TestWriteMatrixDense(unittest.TestCase):
+
+    def setUp(self):
+        self.filename_int='test/matrix_int_out.dat'
+        self.filename_float='test/matrix_float_out.dat'
+        self.filename_complex='test/matrix_complex_out.dat'
+        
+        self.A_int=np.arange(3*3).reshape(3, 3)
+        self.A_float=1.0*self.A_int
+        self.A_complex=np.arange(3*3).reshape(3, 3)+\
+            1j*np.arange(9,3*3+9).reshape(3, 3)
+
+    def tearDown(self):
+        os.remove(self.filename_int)
+        os.remove(self.filename_float)
+        os.remove(self.filename_complex)
+
+    def test_write_matrix_dense(self):
+        matrix.write_matrix_dense(self.filename_int, self.A_int, fmt='%d')
+        An=np.loadtxt(self.filename_int, dtype=np.int)
+        self.assertTrue(np.all(An==self.A_int))
+
+        matrix.write_matrix_dense(self.filename_float, self.A_float)
+        An=np.loadtxt(self.filename_int)
+        self.assertTrue(np.all(An==self.A_float))
+
+        matrix.write_matrix_dense(self.filename_complex, self.A_complex)
+        An=np.loadtxt(self.filename_complex, dtype=np.complex)
+        self.assertTrue(np.all(An==self.A_complex))
+
+################################################################################
+# sparse
+################################################################################
+
+class TestReadMatrixSparse(unittest.TestCase):       
+
+    def setUp(self):
+        self.filename_int='test/spmatrix_int.coo.dat'
+        self.filename_float='test/spmatrix_float.coo.dat'
+        self.filename_complex='test/spmatrix_complex.coo.dat'
+
+        """Reference matrices in dense storage"""
+        self.reference_int='test/spmatrix_int_reference.dat'
+        self.reference_float='test/spmatrix_float_reference.dat'
+        self.reference_complex='test/spmatrix_complex_reference.dat'
+
+    def tearDown(self):
+        pass
+
+    def test_read_matrix_sparse(self):
+        A=np.loadtxt(self.reference_int, dtype=np.int)
+        A_n=matrix.read_matrix_sparse(self.filename_int, dtype=np.int).toarray()
+        self.assertTrue(np.all(A==A_n))
+
+        A=np.loadtxt(self.reference_float)
+        A_n=matrix.read_matrix_sparse(self.filename_float).toarray()
+        self.assertTrue(np.all(A==A_n))
+
+        A=np.loadtxt(self.reference_complex, dtype=np.complex)
+        A_n=matrix.read_matrix_sparse(self.filename_complex, dtype=np.complex).toarray()
+        self.assertTrue(np.all(A==A_n))
+
+class TestWriteMatrixSparse(unittest.TestCase):
+
+    def is_integer(self, x):
+        """Check if elements of an array can be represented by integers.
+        
+        Parameters 
+        ----------
+        x : ndarray
+            Array to check.
+        
+        Returns
+        -------
+        is_int : ndarray of bool
+            is_int[i] is True if x[i] can be represented
+            as int otherwise is_int[i] is False.
+        
+        """
+        is_int=np.equal(np.mod(x, 1), 0)
+        return is_int
+
+    def sparse_matrix_from_coo(self, coo):
+        row=coo[:, 0]
+        col=coo[:, 1]
+        values=coo[:, 2]
+
+        """Check if imaginary part of row and col is zero"""
+        if np.all(np.isreal(row)) and np.all(np.isreal(col)):
+            row=row.real
+            col=col.real
+
+            """Check if first and second column contain only integer entries"""
+            if np.all(self.is_integer(row)) and np.all(self.is_integer(col)):           
+
+                """Convert row and col to int"""
+                row=row.astype(int)
+                col=col.astype(int)
+
+                """Create coo-matrix"""
+                A=scipy.sparse.coo_matrix((values,(row, col)))
+                return A
+            else:
+                raise ValueError('coo contains non-integer entries for row and col.')        
+        else:
+            raise ValueError('coo contains complex entries for row and col.')
+        
+
+    def extract_coo(self, A):
+        A=A.tocoo()
+        coo=np.transpose(np.vstack((A.row, A.col, A.data)))
+        return coo
+
+    def setUp(self):
+        self.filename_int='test/spmatrix_int_out.coo.dat'
+        self.filename_float='test/spmatrix_float_out.coo.dat'
+        self.filename_complex='test/spmatrix_complex_out.coo.dat'
+
+        """Tri-diagonal test matrices"""
+        dim=10
+        d0=np.arange(0, dim)
+        d1=np.arange(dim, 2*dim-1)
+        d_1=np.arange(2*dim, 3*dim-1)
+
+        self.A_int=scipy.sparse.diags((d0, d1, d_1), (0, 1, -1), dtype=np.int).tocoo()
+        self.A_float=scipy.sparse.diags((d0, d1, d_1), (0, 1, -1)).tocoo()
+        self.A_complex=self.A_float+1j*self.A_float
+
+    def tearDown(self):
+        os.remove(self.filename_int)
+        os.remove(self.filename_float)
+        os.remove(self.filename_complex)
+    
+    def test_write_matrix_sparse(self):
+        matrix.write_matrix_sparse(self.filename_int, self.A_int, fmt='%d')
+        coo_n=np.loadtxt(self.filename_int, dtype=np.int)
+        """Create sparse matrix from coo data"""
+        A_n=self.sparse_matrix_from_coo(coo_n)
+        diff=(self.A_int-A_n).tocsr()
+        """Check for empty array of non-zero entries"""
+        self.assertTrue(np.all(diff.data==0.0))        
+
+        matrix.write_matrix_sparse(self.filename_float, self.A_float)
+        coo_n=np.loadtxt(self.filename_int, dtype=np.float)
+        """Create sparse matrix from coo data"""
+        A_n=self.sparse_matrix_from_coo(coo_n)
+        diff=(self.A_float-A_n).tocsr()
+        """Check for empty array of non-zero entries"""
+        self.assertTrue(np.all(diff.data==0.0))
+
+        matrix.write_matrix_sparse(self.filename_complex, self.A_complex)
+        coo_n=np.loadtxt(self.filename_complex, dtype=np.complex)
+        """Create sparse matrix from coo data"""
+        A_n=self.sparse_matrix_from_coo(coo_n)
+        diff=(self.A_complex-A_n).tocsr()
+        """Check for empty array of non-zero entries"""
+        self.assertTrue(np.all(diff.data==0.0))
 
 if __name__=="__main__":
     unittest.main()
