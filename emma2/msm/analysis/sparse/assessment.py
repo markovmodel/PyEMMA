c0e4d226137b8db769a5feae826e34a16a888c9f
Benjamin Trendelkamp-Schroer <benjamin.trendelkamp-schroer@fu-berlin.de>
2013-10-28 10:59:05 +0100

    Copied allclose sparse into sparse/assessment

--- emma2/msm/analysis/sparse/assessment.py
+++ emma2/msm/analysis/sparse/assessment.py
@@ -6,9 +6,40 @@ from scipy.sparse.construct import diags
 from scipy.sparse.csgraph import connected_components
 from scipy.sparse.sputils import isdense
 
-from emma2.msm.util import allclose_sparse
-
 import numpy as np
+
+def allclose_sparse(A, B, rtol=1e-5, atol=1e-9):
+    """
+    Compares two sparse matrices in the same matter like numpy.allclose()
+    Parameters
+    ----------
+    A : scipy.sparse matrix
+        first matrix to compare
+    B : scipy.sparse matrix
+        second matrix to compare
+    rtol : float
+        relative tolerance
+    atol : float
+        absolute tolerance
+    
+    Returns
+    -------
+    True, if given matrices are equal in bounds of rtol and atol
+    False, otherwise
+    
+    Notes
+    -----
+    If the following equation is element-wise True, then allclose returns
+    True.
+
+     absolute(`a` - `b`) <= (`atol` + `rtol` * absolute(`b`))
+
+    The above equation is not symmetric in `a` and `b`, so that
+    `allclose(a, b)` might be different from `allclose(b, a)` in
+    some rare cases.
+    """
+    diff = (A - B).data
+    return np.allclose(diff, 0.0, rtol=rtol, atol=atol)
 
 def is_transition_matrix(T, tol):
     """
