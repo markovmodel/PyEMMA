3ec1fa815e445e3e3b56fc1cec34e93e91c06096
Benjamin Trendelkamp-Schroer <benjamin.trendelkamp-schroer@fu-berlin.de>
2013-10-29 09:45:25 +0100

    Named decomposition.mu to decomposition.stationary_distribution

--- emma2/msm/analysis/sparse/assessment.py
+++ emma2/msm/analysis/sparse/assessment.py
@@ -105,7 +105,7 @@ def is_reversible(T, mu=None, tol=1e-15):
     T = T.tocsr()
     
     if mu is None:
-        from decomposition import mu as statdist
+        from decomposition import stationary_distribution as statdist
         mu = statdist(T)
     
     Mu = diags(mu, 0)
