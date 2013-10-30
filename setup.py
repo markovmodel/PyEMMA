f0d28634140a7994a64bcf7f6ed36857455bcaea
marscher <m.scherer@fu-berlin.de>
2013-10-30 20:26:28 +0100

    Added direct call to jcc (which invokes another setup() method). Currently works with easy_install (not with pip)

--- setup.py
+++ setup.py
@@ -7,39 +7,6 @@ try:
     from setuptools.core import setup
 except ImportError:
     from distutils.core import setup
-    from distutils.command.install import install as build
-
-# TODO: integrate jcc call in build phase
-# create egg and integrate this somehow in build process
-
-def create_packages(packages):
-    cp_string = ''.join("--package %s " \
-                        % ''.join(map(str, x)) for x in packages)
-    return cp_string
-
-class build_JCC_Wrapper(build):
-    """
-    invokes apache jcc to build a wrapper for the public api of stallone 
-    """
-    def run(self):
-        import sys, shlex, subprocess
-        additional_packages = ['stallone.mc', 
-                               'stallone.algebra',
-                               'stallone.cluster']
-        
-        stallone_api_jar = 'lib/stallone/stallone-1.0-SNAPSHOT-api.jar'
-
-        call = sys.executable + ' -m jcc --jar ' + stallone_api_jar \
-             + ' ' + create_packages(additional_packages) \
-             + '--include stallone-1.0-SNAPSHOT-jar-with-dependencies.jar' \
-             + ' --use_full_names' \
-             + " --python " + __name__ + ' ' \
-             + " --version " + str(__version__) + " --reserved extern" \
-             + " --module util/ArrayWrapper.py" \
-             + ' --files 2 --use_full_names'
-        
-        print call
-        return subprocess.call(shlex.split(call))
 
 setup(
       name = 'Emma2',
@@ -47,16 +14,17 @@ setup(
       description = 'EMMA 2',
       url = 'http://compmolbio.biocomputing-berlin.de/index.php',
       author = 'The emma2 team',
-      cmdclass = dict(install = build_JCC_Wrapper),
       # list packages here
-      packages = ['emma2',
+      packages = [#'emma2',
                  'emma2.msm.analysis',
                  'emma2.msm.analysis.dense',
                  'emma2.msm.analysis.sparse',
                  'emma2.msm.estimation',
                  'emma2.msm.estimation.sparse',
                  'emma2.msm.io',
-                 'emma2.pmm'],
+                 'emma2.msm.shared',
+                 #'emma2.pmm',
+                 'emma2.util'],
       # runtime dependencies
       install_requires = ['numpy >=1.7',
                          'scipy >=0.11',
@@ -64,3 +32,42 @@ setup(
       # build time dependencies
       requires = ['JCC (>=2.17)']
 )
+
+def setupPyStallone():
+    try:
+        from jcc import cpp as cpp
+    except ImportError:
+        raise RuntimeError('apache jcc not available')
+    
+    stallone_api_jar = 'lib/stallone/stallone-1.0-SNAPSHOT-api.jar'
+    stallone_whole_in_one_jar = \
+        'lib/stallone/stallone-1.0-SNAPSHOT-jar-with-dependencies.jar'
+    args = ['--jar', stallone_api_jar,
+         '--package', 'stallone.mc',
+         '--package', 'stallone.algebra',
+         '--package', 'stallone.cluster',
+         '--include', stallone_whole_in_one_jar,
+         '--use_full_names',
+         '--python', 'stallone',
+         '--version', '1.0',
+         '--reserved', 'extern',
+         #'--use-distutils',
+         '--egg-info',
+         '--files', '2', '--build', '--bdist']
+    args.insert(0, __file__)
+    cpp.jcc(args)
+
+#import argparse
+#parser = argparse.ArgumentParser()
+#parser.add_argument('--recompile', metavar='N', type=bool, default=False,
+#                   help='should stallone be recompiled even if already installed?')
+#args = parser.parse_args()
+
+try:
+    import stallone
+    print "stallone module found. Not Installing"
+    if False:
+        print "forcing reinstallation of stallone."
+        setupPyStallone()
+except ImportError:
+    setupPyStallone()
