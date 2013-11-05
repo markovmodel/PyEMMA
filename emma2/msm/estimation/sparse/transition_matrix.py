bdd353928fb9272527bfb8e74bf8cf365de93a15
Benjamin Trendelkamp-Schroer <benjamin.trendelkamp-schroer@fu-berlin.de>
2013-11-05 10:09:34 +0100

    Refactored transition_matrix.transition_matrix to transition_matrix.transition_matrix_non_reversible

--- emma2/msm/estimation/sparse/transition_matrix.py
+++ emma2/msm/estimation/sparse/transition_matrix.py
@@ -3,13 +3,15 @@
 import numpy as np
 import scipy.sparse
 
-
-def transition_matrix(C, reversible=False, mu=None, **kwargs):
+def transition_matrix_non_reversible(C):
     """implementation of transition_matrix"""
+    if scipy.sparse.issparse(C):
+        C=C
+    else:
+        C=scipy.sparse.csr_matrix(C)    
     rowsum = C.tocsr().sum(axis=1)
     if(np.min(rowsum) == 0.0):
-        raise ValueError("matrix C contains rows with sum zero. ")
-    
+        raise ValueError("matrix C contains rows with sum zero. ")    
     rowsum = np.array(1. / rowsum).flatten()
     norm = scipy.sparse.diags(rowsum, 0)
     return norm * C
