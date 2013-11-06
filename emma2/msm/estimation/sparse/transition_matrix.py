306f6431fbb4226db68be5c93ffc3493b2276ca8
marscher <m.scherer@fu-berlin.de>
2013-11-06 16:34:34 +0100

    [msm/estimation/sparse/tmatrix_non_rev] only convert/assign matrix if it is not sparse.

--- emma2/msm/estimation/sparse/transition_matrix.py
+++ emma2/msm/estimation/sparse/transition_matrix.py
@@ -5,14 +5,11 @@ import scipy.sparse
 
 def transition_matrix_non_reversible(C):
     """implementation of transition_matrix"""
-    if scipy.sparse.issparse(C):
-        C=C
-    else:
-        C=scipy.sparse.csr_matrix(C)    
+    if not scipy.sparse.issparse(C):
+        C = scipy.sparse.csr_matrix(C)
     rowsum = C.tocsr().sum(axis=1)
     if(np.min(rowsum) == 0.0):
-        raise ValueError("matrix C contains rows with sum zero. ")    
+        raise ValueError("matrix C contains rows with sum zero.")
     rowsum = np.array(1. / rowsum).flatten()
     norm = scipy.sparse.diags(rowsum, 0)
     return norm * C
-    
