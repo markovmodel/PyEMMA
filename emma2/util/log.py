ecd1ad21c99b21c160e10f7c0738da7ad00e8a37
marscher <m.scherer@fu-berlin.de>
2013-11-07 13:42:04 +0100

    [util/log] use a config file (ini style) to configure log system, instead of commandline arguments, which conflicts with other parsers. Removed StreamHandler as it logs things twice, since we already have a BasicConfig.

--- emma2/util/log.py
+++ emma2/util/log.py
@@ -4,59 +4,49 @@ Created on 15.10.2013
 @author: marscher
 '''
 import logging
-from logging import _levelNames as loglevel_names
-import args
+import ConfigParser
 
-import argparse
+class _AttribStore(dict):
 
-__all__ = ['log']
+    def __getattr__(self, name):
+        return self[name]
 
-"""
-    Arguments for logging
-"""
-parser = argparse.ArgumentParser(parents=[args.root_parser],
-                                 add_help=False,
-                                 description='Emma2 logging arguments')
+    def __setattr__(self, name, value):
+        self[name] = value
 
-parser.add_argument('--logging', dest='logging', default='on',
-                    choices = ['on', 'off'],
-                    help='turn logging on or off')
-parser.add_argument('--console', action='store_true', default=True,
-                     help='should be logged to console?')
-parser.add_argument('--logfile', dest='logfilename', type=str,
-                    default='emma2.log',
-                    help='logfile to use')
-parser.add_argument('--loglevel', dest='loglevel',
-                    default=logging.INFO,
-                    choices = ['NOTSET','DEBUG','INFO','WARNING',\
-                               'ERROR','CRITICAL'],
-                    help='minimum loglevel')
+defaults = {'enabled': True,
+            'console' : True,
+            'tofile' : False,
+            'file' : 'emma2.log',
+            'level' : 'DEBUG'}
 
-args = parser.parse_args()
+config = ConfigParser.SafeConfigParser(defaults)
+""" this filenames are being tried to read """
+filenames = ['emma2.cfg', '/etc/emma2.cfg']
+used_filenames = config.read(filenames)
+
+section = 'Logging'
+args = _AttribStore()
+args.logging = config.getboolean(section, 'enabled')
+args.console = config.getboolean(section, 'toconsole')
+args.tofile = config.getboolean(section, 'tofile')
+args.logfilename = config.get(section, 'file')
+args.loglevel = config.get(section, 'level')
 
 #TODO: setup file logging here, but do not define a logger object and use
 # import logging; log=logging.getLogger(__name__) in each unit instead.
 # this should work, because everthing is derived from root logger
 log = logging.getLogger('emma2')
 
-if args.logging == 'on':
+if args.logging:
     # if console flag is on, ignore filename
     if args.console:
         _filename = None
     else:
         _filename = args.logfilename
 
-    logging.basicConfig(level=logging.DEBUG,
+    logging.basicConfig(level=args.loglevel,
                 format='%(asctime)s %(name)-12s %(levelname)-8s %(message)s',
-                datefmt='%m-%d %H:%M:%S',
+                datefmt='%d-%m-%y %H:%M:%S',
                 filename=_filename,
                 filemode='a')
-    # define a Handler which writes INFO messages or higher to the sys.stderr
-    _console = logging.StreamHandler()
-    _console.setLevel(args.loglevel)
-    # set a format which is simpler for console use
-    _formatter = logging.Formatter('%(name)-12s: %(levelname)-8s %(message)s')
-    # tell the handler to use this format
-    _console.setFormatter(_formatter)
-    
-    log.addHandler(_console)
