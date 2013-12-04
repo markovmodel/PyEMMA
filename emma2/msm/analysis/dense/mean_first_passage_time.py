359af799f438e362118b3a0ada55692b749ddf54
Benjamin Trendelkamp-Schroer <benjamin.trendelkamp-schroer@fu-berlin.de>
2013-12-04 10:41:11 +0100

    Dense implementation of mean first passage time computation

---
+++ emma2/msm/analysis/dense/mean_first_passage_time.py
@@ -0,0 +1,38 @@
+"""Dense implementation of mean first passage time computations"""
+import numpy as np
+
+def mfpt(T, target):
+    r"""Compute vector of mean first passage times to given target state.
+
+    The vector m_t of mean first passage times to a given target state t 
+    solves the following system of linear equations,
+
+                0                       i=t
+        m_t[i]=    
+                1+\sum_j p_ij m_t[j]    i \neq t
+        
+    
+    Parameters
+    ----------
+    T : ndarray, shape=(n,n) 
+        Transition matrix.
+    target : int
+        Target state for mfpt calculation.
+    
+    Returns
+    -------
+    m_t : ndarray, shape=(n,)
+        Vector of mean first passage times to target state t.
+    
+    """
+    dim=T.shape[0]
+    A=T-np.eye(dim)
+    A[target, :]=0.0
+    A[target, target]=1.0
+    b=np.ones(dim)
+    b[target]=0.0
+    m_t=np.solve(A, b)
+    return m_t
+
+
+    
