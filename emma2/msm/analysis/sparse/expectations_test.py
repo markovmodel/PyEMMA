b0dc5c59b4b0f9663edbf761bbc8c534a7c8f117
Benjamin Trendelkamp-Schroer <benjamin.trendelkamp-schroer@fu-berlin.de>
2014-01-07 14:20:34 +0100

    Added sparse implementation of expected_counts_stationary and corresponding unit test

--- emma2/msm/analysis/sparse/expectations_test.py
+++ emma2/msm/analysis/sparse/expectations_test.py
@@ -47,9 +47,7 @@ def sparse_allclose(A, B, rtol=1e-5, atol=1e-08):
 
     return allclose_values and equal_indices and equal_indptr
     
-
-
-class TestDecomposition(unittest.TestCase):
+class TestExpectedCounts(unittest.TestCase):
     
     def setUp(self):
         self.k=20
@@ -97,6 +95,63 @@ class TestDecomposition(unittest.TestCase):
         EC_true=N*D_mu.dot(T)
 
         self.assertTrue(sparse_allclose(EC_true, EC_n))        
+
+class TestExpectedCountsStationary(unittest.TestCase):
+    def setUp(self):
+        self.k=20
+        self.d=10000
+        """Generate a random kxk dense transition matrix"""
+        C=np.random.random_integers(0, 100, size=(self.k, self.k))
+        C=C+np.transpose(C) # Symmetric count matrix for real eigenvalues
+        T=1.0*C/np.sum(C, axis=1)[:, np.newaxis]
+        v, L, R=scipy.linalg.eig(T, left=True, right=True)
+        """Sort eigenvalues and eigenvectors, order is decreasing absolute value"""
+        ind=np.argsort(np.abs(v))[::-1]
+        v=v[ind]
+        L=L[:, ind]
+        R=R[:, ind]
+        
+        nu=L[:, 0]
+        mu=nu/np.sum(nu)
+
+        """
+        Generate k random sparse
+        orthorgonal vectors of dimension d
+        """
+        Q=random_orthonormal_sparse_vectors(self.d, self.k)
+        
+        """Push forward dense decomposition to sparse one via Q"""
+        self.L_sparse=Q.dot(scipy.sparse.csr_matrix(L))
+        self.R_sparse=Q.dot(scipy.sparse.csr_matrix(R))
+        self.v_sparse=v # Eigenvalues are invariant
+
+        """Push forward transition matrix and stationary distribution"""
+        self.T_sparse=Q.dot(scipy.sparse.csr_matrix(T)).dot(Q.transpose())
+        self.mu_sparse=Q.dot(mu)/np.sqrt(self.k)
+
+    def tearDown(self):
+        pass
+
+    def test_expected_counts_stationary(self):
+        n=50
+        T=self.T_sparse        
+        mu=self.mu_sparse
+
+        D_mu=scipy.sparse.diags(self.mu_sparse, 0)
+        EC_true=n*D_mu.dot(T)
+
+        """Compute mu on the fly"""
+        EC_n=expectations.expected_counts_stationary(T, n)
+        self.assertTrue(sparse_allclose(EC_true, EC_n))   
+
+        """With precomputed mu"""
+        EC_n=expectations.expected_counts_stationary(T, n, mu=mu)
+        self.assertTrue(sparse_allclose(EC_true, EC_n))   
+
+        n=0
+        EC_true=scipy.sparse.coo_matrix(T.shape)
+        EC_n=expectations.expected_counts_stationary(T, n)
+        self.assertTrue(sparse_allclose(EC_true, EC_n))
         
 
 if __name__=="__main__":
