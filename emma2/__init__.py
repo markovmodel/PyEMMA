88257fa8eb65e6a2f1e91932fb05b6f6d6afd5b3
marscher <m.scherer@fu-berlin.de>
2014-01-24 13:55:11 +0100

    [emma2/__init__] require 1.8.0 of numpy (this is reused in all subsequent imports of numpy, since imports are cached)

--- emma2/__init__.py
+++ emma2/__init__.py
@@ -2,11 +2,14 @@
     Emma2 package
 """
 
+import pkg_resources
+pkg_resources.require('numpy >= 1.8.0')
+
 import coordinates
 import msm
 import pmm
 import util
 
-""" global logging support. See documentation of python library 
+""" global logging support. See documentation of python library
 logging module for more information"""
 from util.log import log
