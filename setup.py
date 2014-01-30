e173e4869fab18380102c083cc10981dba4d5a8c
marscher <m.scherer@fu-berlin.de>
2014-01-30 19:08:00 +0100

    added backward compatibility to scipy 0.9.0. This is achieved via adding the copied 'diags' method code to the scipy.sparse module at top level.

--- setup.py
+++ setup.py
@@ -63,6 +63,6 @@ setup(name = 'Emma2',
       #      setup_requires = ['numpy >= 1.8'],
       # runtime dependencies
       install_requires = ['numpy >= 1.6.0',
-                         'scipy >= 0.11',
+                         'scipy >= 0.9',
                          'JPype1 >= 0.5.4.5'],
 )
