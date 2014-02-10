1be83dac652a69c11cd75b92cdd18d8eaa1f5939
Benjamin Trendelkamp-Schroer <benjamin.trendelkamp-schroer@fu-berlin.de>
2014-02-10 15:33:26 +0100

    Resolved merge conflict

--- emma2/__init__.py
+++ emma2/__init__.py
@@ -7,7 +7,7 @@ import msm
 import pmm
 import util
 
-""" global logging support. See documentation of python library
+""" global logging support. See documentation of python library 
 logging module for more information"""
 from util.log import log
 
---
+++ sphinx/api.rst
@@ -0,0 +1,6 @@
+
+API
+===
+
+.. automodule:: emma2.msm.analysis.api
+
