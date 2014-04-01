1aa3c436b068494e15e56f9e0da0e328f83a1ca7
marscher <m.scherer@fu-berlin.de>
2014-04-01 16:34:58 +0200

    [config] minor refactoring

--- emma2/util/config.py
+++ emma2/util/config.py
@@ -20,18 +20,7 @@ import pkg_resources
 
 __all__ = ['configParser', 'used_filenames', 'AttribStore']
 
-""" this filenames are being tried to read to obtain basic configuration values 
-    for the logging system."""
-cfg = 'emma2.cfg'
-
-ultimate_backup = pkg_resources.resource_filename('emma2', os.path.join('..', 'emma2.cfg'))
-filenames = [cfg, # config in current dir
-            '/etc/' + cfg, # config in global installation
-            os.path.join(os.path.expanduser('~'), os.path.sep, cfg), # config in user dir
-            # This should always be last, since it provides all default values
-            # and things will horribly fail, if this file can not be found.
-            ultimate_backup
-            ]
+configParser = None
 
 class AttribStore(dict):
     def __getattr__(self, name):
@@ -40,9 +29,26 @@ class AttribStore(dict):
     def __setattr__(self, name, value):
         self[name] = value
 
-configParser = ConfigParser.SafeConfigParser()
-""" this is a list of used configuration filenames during parsing the configuration"""
-used_filenames = configParser.read(filenames)
+def readConfiguration():
+    global configParser, used_filenames
+    """ this filenames are being tried to read to obtain basic configuration values 
+        for the logging system."""
+    cfg = 'emma2.cfg'
+    ultimate_backup = pkg_resources.resource_filename('emma2', os.path.join('..', 'emma2.cfg'))
+    filenames = [cfg, # config in current dir
+                '/etc/' + cfg, # config in global installation
+                os.path.join(os.path.expanduser('~'), os.path.sep, cfg), # config in user dir
+                # This should always be last, since it provides all default values
+                # and things will horribly fail, if this file can not be found.
+                ultimate_backup
+                ]
+    
+    configParser = ConfigParser.SafeConfigParser()
+    """ this is a list of used configuration filenames during parsing the configuration"""
+    used_filenames = configParser.read(filenames)
+    
+    if ultimate_backup not in used_filenames:
+        raise RuntimeError('Default configuration values could not be red!')
 
-if ultimate_backup not in used_filenames:
-    raise RuntimeError('Default configuration values could not be red!')
+
+readConfiguration()
