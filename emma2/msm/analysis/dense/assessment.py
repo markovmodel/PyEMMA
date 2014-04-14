321f86c301dc18b07e11f1b8d9a2a24aed42f7b5
noe <frank.noe@fu-berlin.de>
2014-04-14 17:48:40 +0200

    set assessment tolerances to more realistic values

--- emma2/msm/analysis/dense/assessment.py
+++ emma2/msm/analysis/dense/assessment.py
@@ -2,7 +2,7 @@ import numpy as np
 
 import decomposition
 
-def is_transition_matrix(T, tol=1e-15):
+def is_transition_matrix(T, tol=1e-10):
     """
     Tests whether T is a transition matrix
     
@@ -28,7 +28,7 @@ def is_transition_matrix(T, tol=1e-15):
     return np.abs(x - np.ones(dim)).max() < dim * tol and X.max() < 2.0 * tol
 
 
-def is_rate_matrix(K, tol=1e-15):
+def is_rate_matrix(K, tol=1e-10):
     """
     True if K is a rate matrix
     Parameters
@@ -52,7 +52,7 @@ def is_rate_matrix(K, tol=1e-15):
     
     return off_diagonal_positive and row_sum_eq_0
 
-def is_reversible(T, mu=None, tol=1e-15):
+def is_reversible(T, mu=None, tol=1e-10):
     r"""
     checks whether T is reversible in terms of given stationary distribution.
     If no distribution is given, it will be calculated out of T.
@@ -76,7 +76,7 @@ def is_reversible(T, mu=None, tol=1e-15):
     """
     if is_transition_matrix(T, tol):
         if mu is None:
-            mu = decomposition.stationary_distribution(T)
+            mu = decomposition.stationary_distribution_from_linearsystem(T)
         return np.allclose(T * mu[ : , np.newaxis ], \
                            T[ : , np.newaxis] * mu,  atol=tol)
     else:
