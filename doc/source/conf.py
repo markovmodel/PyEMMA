8420c3286fd3fe0a7bd0e45486ebf8dd6721f3a8
marscher <m.scherer@fu-berlin.de>
2014-05-05 17:57:14 +0200

    removed unused import

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -11,7 +11,7 @@
 # All configuration values have a default; values that are commented out
 # serve to show the default.
 
-import sys, os, re
+import sys, os
 
 # Check Sphinx version
 import sphinx
