049f6b6599583a97d17573e308d03a956a7c8544
marscher <m.scherer@fu-berlin.de>
2014-05-07 19:03:38 +0200

    [pcca] only calc right eigenvectors, removed trailing whitespaces

--- emma2/msm/analysis/dense/pcca.py
+++ emma2/msm/analysis/dense/pcca.py
@@ -5,22 +5,23 @@ Created on 30.11.2013
 '''
 
 import pcca_impl
-import decomposition
+#import decomposition
 
 import numpy
+from decomposition import eigenvectors
 
 def pcca(T, n):
-    
-    eigenvalues,left_eigenvectors,right_eigenvectors = decomposition.rdl_decomposition(T, n)
-    
+    #eigenvalues,left_eigenvectors,right_eigenvectors = decomposition.rdl_decomposition(T, n)
+    right_eigenvectors = eigenvectors(T, right = True)
+    # TODO: complex warning maybe?
     right_eigenvectors = numpy.real(right_eigenvectors)
-    
+
     # create initial solution that works using the old method
-    
+
     (c_f, indic, chi, rot_matrix) = pcca_impl.cluster_by_isa(right_eigenvectors, n)
-        
+
     rot_matrix = pcca_impl.opt_soft(right_eigenvectors, rot_matrix, n)
-                
+
     memberships = numpy.dot(right_eigenvectors[:,:], rot_matrix)
-    
+
     return memberships
