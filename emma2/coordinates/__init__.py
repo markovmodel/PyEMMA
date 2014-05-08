16da6842e4bc1712265b6d43092902a387377148
marscher <m.scherer@fu-berlin.de>
2014-05-08 15:08:09 +0200

    [coordinates] use relative imports

--- emma2/coordinates/__init__.py
+++ emma2/coordinates/__init__.py
@@ -17,9 +17,6 @@ Submodules
    transform - perform coordinate transformations
    tica - perform PCA and TICA on data
 """
-from __future__ import division, print_function, absolute_import
 
-import emma2.coordinates.tica
-import emma2.coordinates.transform
-
-__all__=[s for s in dir() if not s.startswith('_')]
+from . import tica
+from . import transform
