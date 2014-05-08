5c8ef6920dce072cee13e560972223c01ed563a7
marscher <m.scherer@fu-berlin.de>
2014-05-08 14:38:20 +0200

    [emma2.__init__] handle case get_distribution raises

--- emma2/__init__.py
+++ emma2/__init__.py
@@ -9,7 +9,11 @@ import pmm
 import util
 
 # note that this works only for installed (or egg links created via setup.py develop)
-from pkg_resources import get_distribution as _get_dist
-__version__ = _get_dist('emma2').version
-
+import pkg_resources
+try:
+    __version__ = pkg_resources.get_distribution('emma2').version
+except pkg_resources.DistributionNotFound:
+    __version__ = 'undefined-cause-not-installed'
+finally:
+    del pkg_resources
 
