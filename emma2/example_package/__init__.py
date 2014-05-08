9e1c265ccc69ebe05a4552150334f70288d59747
marscher <m.scherer@fu-berlin.de>
2014-05-08 15:15:29 +0200

    removed example package

--- emma2/example_package/__init__.py
+++
@@ -1,6 +0,0 @@
-"""
-just an example package to demonstrate desired package structure and
-documentation style.
-This is the package doc string, which is shown as description of this package.
-The example package is not deployed with setuptools/distutils
-"""
