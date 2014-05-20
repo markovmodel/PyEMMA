3f45f0c161539d52ae566a6a861aa7e81f8b2e67
Benjamin Trendelkamp-Schroer <benjamin.trendelkamp-schroer@fu-berlin.de>
2014-05-20 17:04:40 +0200

    [msm/analysis] Reimplemented dense committor module and corresponding unit tests

--- emma2/msm/analysis/dense/committor.py
+++ emma2/msm/analysis/dense/committor.py
@@ -3,89 +3,134 @@ Created on 08.11.2013
 
 @author: marscher
 '''
-import numpy
+import numpy as np
+from scipy.linalg import solve
+
+from decomposition import stationary_distribution_from_backward_iteration as statdist
 
 def forward_committor(T, A, B):
-    """ 
-    calculate forward committor from A to B given transition matrix T.
+    r"""Forward committor between given sets.
+
+    The forward committor u(x) between sets A and B is the probability
+    for the chain starting in x to reach B before reaching A.
+
     Parameters
     ----------
-    T : numpy.ndarray shape = (n, n)
+    T : (M, M) ndarray
         Transition matrix
-    A : array like
+    A : array_like
         List of integer state labels for set A
-    B : array like
+    B : array_like
         List of integer state labels for set B
-        
+
     Returns
     -------
-    x : ndarray, shape=(n, )
-    Committor vector.
+    u : (M, ) ndarray
+        Vector of forward committor probabilities    
+
+    Notes
+    -----
+    The forward committor is a solution to the following
+    boundary-value problem
+
+    .. math::
+
+        \sum_j L_{ij} u_{j}=0    for i in X\(A u B) (I)
+                          u_{i}=0    for i \in A        (II)
+                          u_{i}=1    for i \in B        (III)
+
+    with generator matrix L=(P-I).
+                          
     """
-    n = len(T)
-    set_X = numpy.arange(n)
-    set_A = numpy.unique(A)
-    set_B = numpy.unique(B)
-    set_AB = numpy.intersect1d(set_A, set_B, True)
-    notAB = numpy.setdiff1d(set_X, set_AB, True)
-    m = len(notAB)
+    X=set(range(T.shape[0]))
+    A=set(A)
+    B=set(B)
+    AB=A.intersection(B)
+    notAB=X.difference(A).difference(B)
+    if len(AB)>0:
+        raise ValueError("Sets A and B have to be disjoint")
+    L=T-np.eye(T.shape[0]) # Generator matrix
 
-    K = T - numpy.diag(numpy.ones((n)))
+    """Assemble left hand-side W for linear system"""    
+    """Equation (I)"""
+    W=1.0*L
+    """Equation (II)"""
+    W[list(A), :]=0.0
+    W[list(A), list(A)]=1.0
+    """Equation (III)"""
+    W[list(B), :]=0.0
+    W[list(B), list(B)]=1.0
 
-    U = K[numpy.ix_(notAB, notAB)]
-    v = numpy.zeros(m)
-    v[:] = v[:] - K[notAB[:], B[:]]
-
-    qI = numpy.linalg.solve(U, v)
-
-    q_forward = numpy.zeros(n)
-    q_forward[set_B] = 1
-    q_forward[notAB[:]] = qI[:]
-
-    return q_forward
+    """Assemble right hand side r for linear system"""
+    """Equation (I+II)"""
+    r=np.zeros(T.shape[0])
+    """Equation (III)"""
+    r[list(B)]=1.0
+    
+    u=solve(W, r)
+    return u       
 
 def backward_committor(T, A, B):
-    """ 
-    calculate backward committor from A to B given transition matrix T.
+    r"""Backward committor between given sets.
+
+    The backward committor u(x) between sets A and B is the
+    probability for the chain starting in x to have come from A last
+    rather than from B.
+
     Parameters
     ----------
-    T : numpy.ndarray shape = (n, n)
+    T : (M, M) ndarray
         Transition matrix
-    A : array like
+    A : array_like
         List of integer state labels for set A
-    B : array like
+    B : array_like
         List of integer state labels for set B
-        
+
     Returns
     -------
-    x : ndarray, shape=(n, )
-    Committor vector.
+    u : (M, ) ndarray
+        Vector of forward committor probabilities    
+
+    Notes
+    -----
+    The forward committor is a solution to the following
+    boundary-value problem
+
+    .. math::
+
+        \sum_j K_{ij} \pi_{j} u_{j}=0    for i in X\(A u B) (I)
+                                  u_{i}=1    for i \in A        (II)
+                                  u_{i}=0    for i \in B        (III)
+
+    with adjoint of the generator matrix K=(D_pi(P-I))'.
+                          
     """
-    from ..api import statdist
-    n = len(T)
-    set_X = numpy.arange(n)
-    set_A = numpy.unique(A)
-    set_B = numpy.unique(B)
-    set_AB = numpy.intersect1d(set_A, set_B, True)
-    notAB = numpy.setdiff1d(set_X, set_AB, True)
-    m = len(notAB)
-    
-    eq = statdist(T)
-        
-    Tback = numpy.transpose(T)
-    Tback = numpy.dot(numpy.diag(1.0 / eq), Tback)
-    Tback = numpy.dot(Tback, numpy.diag(eq))
-        
-    K = Tback - numpy.diag(numpy.ones((n)))
+    X=set(range(T.shape[0]))
+    A=set(A)
+    B=set(B)
+    AB=A.intersection(B)
+    notAB=X.difference(A).difference(B)
+    if len(AB)>0:
+        raise ValueError("Sets A and B have to be disjoint")
+    pi=statdist(T)
+    K=np.transpose(pi[:,np.newaxis]*(T-np.eye(T.shape[0])))
 
-    U = K[numpy.ix_(notAB, notAB)]
-    v = numpy.zeros(m)
-    v[:] = v[:] - K[notAB[:], B[:]]
+    """Assemble left-hand side W for linear system"""
+    """Equation (I)"""
+    W=1.0*K
+    """Equation (II)"""
+    W[list(A), :]=0.0
+    W[list(A), list(A)]=1.0
+    """Equation (III)"""
+    W[list(B), :]=0.0
+    W[list(B), list(B)]=1.0
 
-    qI = numpy.linalg.solve(U, v)
+    """Assemble right-hand side r for linear system"""
+    """Equation (I)+(III)"""
+    r=np.zeros(T.shape[0])
+    """Equation (II)"""
+    r[list(A)]=1.0
 
-    q_backward = numpy.zeros(n)
-    q_backward[set_B] = 1
-    q_backward[notAB[:]] = qI[:]
+    u=solve(W, r)
 
-    return q_backward
+    return u
