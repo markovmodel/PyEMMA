d9975e044abdf20970840af66ee67471d9769399
marscher <m.scherer@fu-berlin.de>
2014-05-27 16:55:57 +0200

    [version] use versioneer for automatization of version numbers (rely on git tags)

--- emma2/__init__.py
+++ emma2/__init__.py
@@ -8,12 +8,6 @@ import msm
 import pmm
 import util
 
-# note that this works only for installed (or egg links created via setup.py develop)
-import pkg_resources
-try:
-    __version__ = pkg_resources.get_distribution('emma2').version
-except pkg_resources.DistributionNotFound:
-    __version__ = 'undefined-cause-not-installed'
-finally:
-    del pkg_resources
-
+from ._version import get_versions
+__version__ = get_versions()['version']
+del get_versions
--- setup.py
+++ setup.py
@@ -6,7 +6,6 @@
 """
 import sys
 import os
-import subprocess
 from glob import glob
 
 
@@ -34,36 +33,11 @@ except: # this should catch pkg_resources.DistributionNotFound, which is not imp
     print getSetuptoolsError()
     sys.exit(24)
 
-
-VERSION = "2.0.0"
-ISRELEASED = False
-
-# taken from numpy setup
-def git_version():
-    def _minimal_ext_cmd(cmd):
-        # construct minimal environment
-        env = {}
-        for k in ['SYSTEMROOT', 'PATH']:
-            v = os.environ.get(k)
-            if v is not None:
-                env[k] = v
-        # LANGUAGE is used on win32
-        env['LANGUAGE'] = 'C'
-        env['LANG'] = 'C'
-        env['LC_ALL'] = 'C'
-        out = subprocess.Popen(cmd, stdout=subprocess.PIPE, env=env).communicate()[0]
-        return out
-    try:
-        out = _minimal_ext_cmd(['git', 'rev-parse', '--short', 'HEAD'])
-        GIT_REVISION = out.strip().decode('ascii')
-    except OSError:
-        GIT_REVISION = "Unknown"
-    return GIT_REVISION
-
-if not ISRELEASED and os.path.exists('.git'):
-    __version__ = VERSION + '-' + git_version()
-else:
-    __version__ = VERSION
+import versioneer
+versioneer.versionfile_source = 'emma2/_version.py'
+versioneer.versionfile_build = 'emma2/_version.py'
+versioneer.tag_prefix = '' # tags are like 1.2.0
+versioneer.parentdir_prefix = 'emma2-' # dirname like 'myproject-1.2.0'
 
 
 cocovar_module = Extension('emma2.coordinates.cocovar',
@@ -121,17 +95,23 @@ if not os.environ.get('JAVA_HOME', None):
 
 metadata = dict(
       name = 'Emma2',
-      version = __version__,
+      version = versioneer.get_version(),
       description = 'EMMA 2',
       url = 'http://compmolbio.biocomputing-berlin.de/index.php',
       author = 'The Emma2 team',
+      author_email = '', # TODO: add this
       # packages are found if their folder contains an __init__.py,
       packages = find_packages(),
       # install default emma.cfg and stallone jar into package.
       package_data = {'emma2' : ['emma2.cfg','stallone-1.0-SNAPSHOT-jar-with-dependencies.jar']},
       scripts = [s for s in glob('scripts/*') if s.find('mm_') != -1],
       cmdclass = dict(build_ext = np_build,
-                      test = DiscoverTest),
+                      test = DiscoverTest,
+                      version = versioneer.cmd_version,
+                      versioneer = versioneer.cmd_update_files,
+                      build = versioneer.cmd_build,
+                      sdist = versioneer.cmd_sdist,
+                      ),
       ext_modules = [cocovar_module],
       setup_requires = ['numpy >= 1.6.0'],
       tests_require = [],
