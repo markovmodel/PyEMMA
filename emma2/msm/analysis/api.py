28d7ca2818e8b8f5c61aed944319762b8986bb4f
Benjamin Trendelkamp-Schroer <benjamin.trendelkamp-schroer@fu-berlin.de>
2014-06-05 14:02:54 +0200

    [msm/analysis] removed is_ergodic from api

--- emma2/msm/analysis/api.py
+++ emma2/msm/analysis/api.py
@@ -154,29 +154,6 @@ def is_connected(T, directed=True):
     else:
         raise _type_not_supported
 
-# DONE: Martin 
-def is_ergodic(T, tol=1e-15):
-    r"""True if T is connected (irreducible) and aperiodic.
-    
-    Parameters
-    ----------
-    T : ndarray or scipy.sparse matrix
-        Transition matrix
-    tol : float
-        tolerance to check with
-    
-    Returns
-    -------
-    Truth value : bool
-        True, if T is ergodic
-        False, otherwise
-    """
-    if issparse(T) or isdense(T):
-        # T has to be sparse, and will be converted in sparse impl
-        return sparse.assessment.is_ergodic(T, tol)
-    else:
-        raise _type_not_supported
-
 # DONE: Martin
 def is_reversible(T, mu=None, tol=1e-15):
     r"""True if T is a transition matrix
