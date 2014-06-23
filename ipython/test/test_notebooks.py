60a21f0085911b5c6e0dded111f24b8fbd9ce03c
marscher <m.scherer@fu-berlin.de>
2014-06-23 17:58:46 +0200

    [ipython] added dynamically created testcases to run each notebook and check it for exceptions

---
+++ ipython/test/test_notebooks.py
@@ -0,0 +1,54 @@
+'''
+Created on Jun 20, 2014
+
+@author: marscher
+'''
+import unittest
+import os
+
+# ipy notebook runner
+from runipy.notebook_runner import NotebookRunner
+from IPython.nbformat.current import read
+from IPython.nbformat.v3.nbbase import new_code_cell
+
+ipy_dir = os.path.abspath(os.path.dirname(__file__) + '/..')
+
+def case_generator(filename):
+    def test(self):
+        # TODO: is it possible to execute new kernel for each nb?
+        payload = open(filename)
+        nb = read(payload, 'json')
+        # turn off colors, so we can properly read exceptions
+        cell = new_code_cell(input='%colors NoColor', prompt_number=0)
+        nb.worksheets[0].cells.insert(0,cell)
+        runner = NotebookRunner(nb, working_dir='..')
+        try:
+            runner.run_notebook(skip_exceptions=False)
+        except Exception as e:
+            self.fail('Notebook %s threw an exception (%s)' % (filename, e))
+    return test
+
+class TestNotebooks(unittest.TestCase):
+    @classmethod
+    def setUpClass(cls):
+        super(TestNotebooks, cls).setUpClass()
+        for p, d, filenames in os.walk(ipy_dir):
+           #break
+            for f in filenames:
+                if f.endswith('.ipynb') and f.rfind('-checkpoint') == -1:
+                    test_name = 'test_%s' % os.path.splitext(f)[0]
+                    test = case_generator(os.path.join(p, f))
+                    setattr(cls, test_name, test)
+                    
+        #setattr(cls, 'test_coor', case_generator('../ipython/coordinates.ipynb'))
+        print dir(cls)
+
+    def testDummy(self):
+        """
+        this is needed for py.test, so the class is considered during test collection
+        Do not remove!
+        """
+        pass
+
+if __name__ == '__main__':
+    unittest.main()
