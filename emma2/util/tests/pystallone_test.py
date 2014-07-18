5b360309cad07051979171f2e1f1ada452973fb9
marscher <m.scherer@fu-berlin.de>
2014-07-18 22:15:10 +0200

    [pystallone] conversion warnings added if type of input type is not compatible or will be casted to be accepted in stallone. Made testcase for int64 an expected failure, since it will be downcasted now to int32. Corrected 'no copy' test, which is now failing, because memory changes on python side are not transparent to java

--- emma2/util/tests/pystallone_test.py
+++ emma2/util/tests/pystallone_test.py
@@ -11,7 +11,7 @@ import numpy as np
 class TestPyStallone(unittest.TestCase):
     def setUp(self):
         # setup a random numpy array for each test case
-        self.n = 100000
+        self.n = 1000
         self.a = np.random.random(self.n)
 
     def convertToNPandCompare(self, stArray, npArray, skipDataTypeCheck=False):
@@ -54,7 +54,7 @@ class TestPyStallone(unittest.TestCase):
         b = st.ndarray_to_stallone_array(a)
         self.convertToNPandCompare(b, a, True)
         
-
+    @unittest.expectedFailure # since stallone only maps int32, input converted.
     def testConversionND_Int64(self):
         a = self.a.astype(np.int64)
         b = st.ndarray_to_stallone_array(a)
@@ -78,7 +78,7 @@ class TestPyStallone(unittest.TestCase):
         self.compareNP(self.a, b)
 
     def testJArr2ND_int1d(self):
-        self.a = np.random.randint(1, size=self.n)
+        self.a = np.random.randint(1, size=self.n).astype(np.int32)
         jarr = st.JArray(st.JInt)(self.a)
          
         a = st.API.intsNew.arrayFrom(jarr)
@@ -87,20 +87,20 @@ class TestPyStallone(unittest.TestCase):
         self.compareNP(self.a, b)
     
     def testJArr2ND_int2d(self):
-        self.a = np.random.randint(1, size=self.n)
-        self.a = self.a.reshape((10, self.n / 10))
-        jarr = st.JArray(st.JInt, 2)(self.a)
+        self.a = np.random.randint(1, size=self.n).reshape((10, self.n/10)).astype(np.int32)
+        #self.a = self.a.reshape((10, self.n / 10))
+        list_of_subarrays = [st.JArray(st.JInt, 1)(r) for r in self.a]
+        jarr = st.JArray(st.JInt, 2)(list_of_subarrays)
+        # directly invoke factory, which acceppts only int32
         a = st.API.intsNew.table(jarr)
         b = st.stallone_array_to_ndarray(a)
         self.compareNP(self.a, b)
-    """    
+
     def testDirectBufferFloat(self):
         stArr = st.ndarray_to_stallone_array(self.a, copy=False)
-        # create a copy from java buffer view
-        copy = np.empty(self.n)
-        for i in xrange(len(self.a)):
-            copy[i] = stArr.get(i)
-        self.compareNP(self.a, copy)
-   """
+        # make change in self.a and see if its reflected in stallone array
+        self.a[0] = 42
+        self.assertEqual(42, stArr.get(0))
+
 if __name__ == "__main__":
     unittest.main()
