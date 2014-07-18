24fcddf8c75e0399dc0fdb0dbfb3346f7f674351
marscher <m.scherer@fu-berlin.de>
2014-07-18 22:09:53 +0200

    [util/exceptions] added ConversionWarning

--- emma2/util/exceptions.py
+++ emma2/util/exceptions.py
@@ -4,11 +4,20 @@ Created on May 26, 2014
 @author: marscher
 '''
 
-__all__ = ['ImaginaryWarning', 'SpectralWarning']
+__all__ = ['ImaginaryWarning', 'SpectralWarning', 'PrecisionWarning']
 
 
 class SpectralWarning(RuntimeWarning):
     pass
 
 class ImaginaryEigenValueWarning(SpectralWarning):
+    pass
+
+class PrecisionWarning(RuntimeWarning):
+    """
+    This warning indicates that some operation in your code leads to a conversion
+    of datatypes, which involves a loss/gain in precision.
+    For instance in the stallone wrapper only 32 bit integer and doubles are 
+    supported. So these types have to be converted with the known implications.
+    """
     pass
