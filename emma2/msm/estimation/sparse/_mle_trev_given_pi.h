f4f309e3da96bcf36d869aefb068bf6c7ec4fe4e
jouno <fab@physik.tu-berlin.de>
2014-07-28 16:21:08 +0200

    Added C/Cython implementaton of reversible Tmatrix estimator for fixed pi.

---
+++ emma2/msm/estimation/sparse/_mle_trev_given_pi.h
@@ -0,0 +1,2 @@
+int _mle_trev_given_pi(double * const T, const long long * const C, const double * const pi, const int n, double maxerr, const int maxiter, const double mu);
+int _mle_trev_given_pi_sparse(double * const T, const long long * const C, const double * const pi, const int n, double maxerr, const int maxiter, const double mu);
