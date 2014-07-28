34ab4d7b4de9b66147e9f606103e257e83a4c1e0
jouno <fab@physik.tu-berlin.de>
2014-07-28 18:30:19 +0200

    [msm/estimation] adapted mle_trev_given_pi to parameter conventions + incorp. ito api + split into dense and sparse + remove OpenMP from sparse

--- emma2/msm/estimation/sparse/_mle_trev_given_pi.h
+++ emma2/msm/estimation/sparse/_mle_trev_given_pi.h
@@ -1,2 +1,1 @@
-int _mle_trev_given_pi(double * const T, const long long * const C, const double * const pi, const int n, double maxerr, const int maxiter, const double mu);
-int _mle_trev_given_pi_sparse(double * const T, const long long * const C, const double * const pi, const int n, double maxerr, const int maxiter, const double mu);
+int _mle_trev_given_pi_sparse(double * const T, const long long * const C, const double * const mu, const int n, double maxerr, const int maxiter, const double eps);
