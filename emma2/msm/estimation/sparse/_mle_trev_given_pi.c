5ace22f395fbdf4ca87a8acbeeafe23afc3ea05d
jouno <fab@physik.tu-berlin.de>
2014-07-29 15:21:25 +0200

    [msm/estimation] changed sparse implementation to scipy coo format + updated uni test

--- emma2/msm/estimation/sparse/_mle_trev_given_pi.c
+++ emma2/msm/estimation/sparse/_mle_trev_given_pi.c
@@ -14,69 +14,36 @@ static double distsq(const int n, const double *const a, const double *const b)
   return d;
 }
 
-#define C(i,j) (C [(i)*n+(j)])
-#define T(i,j) (T[(i)*n+(j)])
-
-struct coo { int i; int j; double d; };
-
-int _mle_trev_given_pi_sparse(double * const T, const long long * const C, const double * const mu, const int n, double maxerr, const int maxiter, const double eps)
+int _mle_trev_given_pi_sparse(double * const T_data, const double * const CCt_data, const long long * const i_indices, const long long * const j_indices, const int len_CCt,  const double * const mu, const int len_mu, double maxerr, const int maxiter)
 {
   double d_sq;
   int i, j, t, err, iteration;
-  int Csparse_len;
   double *lam, *lam_new, *temp;
   double CCt_ij;
-  struct coo *Csparse;
 
-  Csparse = NULL;
   err = 0;
 
-  lam= (double*)malloc(n*sizeof(double));
-  lam_new= (double*)malloc(n*sizeof(double));
+  lam= (double*)malloc(len_mu*sizeof(double));
+  lam_new= (double*)malloc(len_mu*sizeof(double));
   if(!(lam&&lam_new)) { err=1; goto error; }
 
   /* check mu */
-  for(i=0; i<n; i++) {
+  for(i=0; i<len_mu; i++) {
     if(mu[i]==0) { err=4; goto error; }
   }
 
   /* initialise lambdas */
-  for(i=0; i<n; i++) {
-    lam_new[i] = 0.0;
-    for(j=0; j<n; j++) {
-      lam_new[i] += 0.5*(C(i,j)+C(j,i));
-    }
-    if(lam_new[i]==0) { err=3; goto error; }
+  for(i=0; i<len_mu; i++) lam_new[i] = 0.0;
+  for(t=0; t<len_CCt; t++) {
+    i = i_indices[t];
+    j = j_indices[t];
+    if(i<j) continue;
+    lam_new[i] += 0.5*CCt_data[t];
+    lam_new[j] += 0.5*CCt_data[t];
   }
-  
-  /* initalize Csparse */
-  Csparse_len = 0;
-  for(i=0; i<n; i++) {
-    for(j=0; j<=i; j++) {
-      if(i==j || C(i,j)+C(j,i)!=0) Csparse_len++;
-    }
-  }
-  Csparse= (struct coo*)malloc(Csparse_len*sizeof(struct coo));
-  if(!Csparse) { err=1; goto error; }
-  t = 0;
-  for(i=0; i<n; i++) {
-    for(j=0; j<=i; j++) {
-      if(C(i,j)+C(j,i)!=0 || i==j) {
-        Csparse[t].i = i;
-        Csparse[t].j = j;
-        if(C(i,j)+C(j,i)==0.0) { 
-          Csparse[t].d=eps; 
-          if(eps==0) { err=6; goto error; }
-        }
-        else Csparse[t].d = C(i,j)+C(j,i); 
-        assert(Csparse[t].d!=0);
-        t++;
-      }
-    }
-  }
-  assert(t==Csparse_len);
+  for(i=0; i<len_mu; i++) if(lam_new[i]==0) { err=3; goto error; }
 
-  /* iterate lambdas */  
+  /* iterate lambdas */
   iteration = 0;
   do {
     /* swap buffers */
@@ -84,51 +51,44 @@ int _mle_trev_given_pi_sparse(double * const T, const long long * const C, const
     lam = lam_new;
     lam_new = temp;
 
-    for(i=0; i<n; i++) {
+    for(i=0; i<len_mu; i++) {
        lam_new[i] = 0.0;
     }
-    for(t=0; t<Csparse_len; t++) {
-      i = Csparse[t].i;
-      j = Csparse[t].j;
-      CCt_ij = Csparse[t].d;
+    for(t=0; t<len_CCt; t++) {
+      i = i_indices[t];
+      j = j_indices[t];
+      if(i<j) continue;
+      CCt_ij = CCt_data[t];
       assert(CCt_ij!=0);
       lam_new[i] += CCt_ij / ((mu[i]*lam[j])/(mu[j]*lam[i])+1.0);
       if(i!=j)
         lam_new[j] += CCt_ij / ((mu[j]*lam[i])/(mu[i]*lam[j])+1.0);
     }
-    for(i=0; i<n; i++) {
+    for(i=0; i<len_mu; i++) {
        if(lam_new[i]==0) { err=2; goto error; }
        if(isnan(lam_new[i])) { err=2; goto error; }
     }
 
     iteration += 1;
-    d_sq = distsq(n,lam,lam_new);
+    d_sq = distsq(len_mu,lam,lam_new);
   } while(d_sq > maxerr*maxerr && iteration < maxiter);
   
   if(iteration==maxiter) { err=5; goto error; } 
 
   /* calculate T */
-  for(i=0; i<n; i++) {
-    for(j=0; j<n; j++) {
-      T(i,j) = 0.0;
-    }
-  }
-  for(t=0; t<Csparse_len; t++) {
-      i = Csparse[t].i;
-      j = Csparse[t].j;
-      CCt_ij = Csparse[t].d;
-      T(i,j) = CCt_ij / (lam_new[i] + lam_new[j]*mu[i]/mu[j]);
-      T(j,i) = CCt_ij / (lam_new[j] + lam_new[i]*mu[j]/mu[i]);
+  for(t=0; t<len_CCt; t++) {
+      i = i_indices[t];
+      j = j_indices[t];
+      CCt_ij = CCt_data[t];
+      T_data[t] = CCt_ij / (lam_new[i] + lam_new[j]*mu[i]/mu[j]);
   }
 
   if(lam) free(lam);
   if(lam_new) free(lam_new);
-  if(Csparse) free(Csparse);
   return 0;
   
 error:
   if(lam) free(lam);
   if(lam_new) free(lam_new);
-  if(Csparse) free(Csparse);
   return -err;
 }
