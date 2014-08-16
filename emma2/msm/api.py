6040dc9074512f112ba68850c34e34c8bd8f134e
noe <frank.noe@fu-berlin.de>
2014-08-16 16:38:21 +0200

    Renamed pathways to flux

---
+++ emma2/msm/api.py
@@ -0,0 +1,22 @@
+'''
+Created on Aug 15, 2014
+
+__moduleauthor__ = "Frank Noe"
+'''
+
+import flux 
+
+def dtrajs(files):
+    pass
+
+def its(dtrajs, lagtimes, connected=True, reversible=True):
+    pass
+
+def msm(dtrajs, tau, connected=True, reversible=True):
+    pass
+
+def cktest(dtrajs, lagtimes, connected=True, reversible=True):
+    pass
+
+# TPT
+tpt = flux.tpt
