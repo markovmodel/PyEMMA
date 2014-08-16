611387fc38c73ce94df0e702059ca32634bba769
noe <frank.noe@fu-berlin.de>
2014-08-16 16:35:19 +0200

    New pathways module with revised TPT functions and classes

--- emma2/msm/pathways/tests/birth_death_chain.py
+++ emma2/msm/pathways/tests/birth_death_chain.py
@@ -1,14 +1,12 @@
-r"""Unit test for the TPT-module
+r"""Birth death chain for testing
 
 .. moduleauthor:: B.Trendelkamp-Schroer <benjamin DOT trendelkamp-schroer AT fu-berlin DOT de>
 
 """
 
-import unittest
 import numpy as np
 
-import committor
-import tpt
+from scipy.sparse import diags
 
 class BirthDeathChain():
     """Birth and death chain class
@@ -58,6 +56,20 @@ class BirthDeathChain():
         P1=np.diag(self.p[0:-1], k=1)
         P_1=np.diag(self.q[1:], k=-1)
         return P0+P1+P_1
+
+    def transition_matrix_sparse(self):
+        """Tridiagonal transition matrix for birth and death chain
+
+        Returns
+        -------
+        P : (N,N) scipy.sparse matrix
+            Transition matrix for birth and death chain with given
+            birth and death probabilities.
+
+        """        
+        P=diags([self.q[1:], self.r, self.p[0:-1]], [-1, 0, 1])
+        return P
+
 
     def stationary_distribution(self):
         a=np.zeros(self.dim)
@@ -236,51 +248,3 @@ class BirthDeathChain():
         qminus=self.committor_backward(a, b)
         kAB=F/(pi*qminus).sum()
         return kAB
-        
-class TestTPT(unittest.TestCase):
-    def setUp(self):
-        p=np.zeros(10)
-        q=np.zeros(10)
-        p[0:-1]=0.5
-        q[1:]=0.5
-        p[4]=0.01
-        q[6]=0.1
-
-        self.A=[0,1]
-        self.B=[8,9]
-        self.a=1
-        self.b=8
-
-        self.bdc=BirthDeathChain(q, p)
-        self.T=self.bdc.transition_matrix()
-
-        """Use precomputed mu, qminus, qplus"""        
-        self.mu = self.bdc.stationary_distribution()
-        self.qplus = self.bdc.committor_forward(self.a, self.b)
-        self.qminus = self.bdc.committor_backward(self.a, self.b)
-        #self.qminus = committor.backward_committor(self.T, self.A, self.B, mu=self.mu)
-        #self.qplus = committor.forward_committor(self.T, self.A, self.B)
-        self.fluxn = tpt.grossflux(self.T, self.mu, self.qminus, self.qplus)
-        self.netfluxn = tpt.netflux(self.fluxn)
-        self.Fn = tpt.totalflux(self.fluxn, self.A)
-        self.kn = tpt.rate(self.Fn, self.mu, self.qminus)
-
-    def test_flux(self):
-        flux=self.bdc.flux(self.a, self.b)        
-        self.assertTrue(np.allclose(self.fluxn, flux))
-
-    def test_netflux(self):
-        netflux=self.bdc.netflux(self.a, self.b)
-        self.assertTrue(np.allclose(self.netfluxn, netflux))
-
-    def test_totalflux(self):
-        F=self.bdc.totalflux(self.a, self.b)
-        self.assertTrue(np.allclose(self.Fn, F))
-
-    def test_rate(self):
-        k=self.bdc.rate(self.a, self.b)
-        self.assertTrue(np.allclose(self.kn, k))
-
-if __name__ == "__main__":
-    unittest.main()
-
