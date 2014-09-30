cc6a1d1bf89b0fec5d73b0464448157cf202af9c
Benjamin Trendelkamp-Schroer <benjamin.trendelkamp-schroer@fu-berlin.de>
2014-09-30 14:14:03 +0200

    [msm/flux] Include flux package in doc

--- emma2/msm/flux/__init__.py
+++ emma2/msm/flux/__init__.py
@@ -1,1 +1,54 @@
+r"""
+
+===================================================================
+flux - Reactive flux an transition pathways (:mod:`emma2.msm.flux`)
+===================================================================
+
+.. currentmodule:: emma2.msm.flux
+
+This module contains functions to compute reactive flux networks and
+find dominant reaction pathways in such networks.
+
+TPT-object
+==========
+
+.. autosummary::
+   :toctree: generated/
+
+   tpt - ReactiveFlux object
+
+Reactive flux
+=============
+
+.. autosummary::
+   :toctree: generated/
+
+   flux_matrix - TPT flux network
+   to_netflux - Netflux from gross flux
+   flux_production - Net flux-production for all states
+   flux_producers
+   flux_consumers
+   coarsegrain
+
+Reaction rates and fluxes
+=========================
+
+.. autosummary::
+   :toctree: generated/
+
+   total_flux
+   rate
+   mfpt
+   
+
+Pathway decomposition
+=====================
+
+.. autosummary::
+   :toctree: generated/
+
+   pathways
+
+"""
+
 from .api import *
