69398ce1518d57e5549c24fb4c5e38db30ce0fca
Benjamin Trendelkamp-Schroer <benjamin.trendelkamp-schroer@fu-berlin.de>
2014-09-30 14:14:19 +0200

    [msm/flux] Fixed docstrings

--- emma2/msm/flux/api.py
+++ emma2/msm/flux/api.py
@@ -10,6 +10,18 @@ from scipy.sparse.sputils import isdense
 import sparse
 import dense
 
+__all__=['tpt',
+         'flux_matrix',
+         'to_netflux',
+         'flux_production',
+         'flux_producers',
+         'flux_consumers',
+         'coarsegrain',
+         'total_flux',
+         'rate',
+         'mfpt',
+         'pathways']                          
+
 _type_not_supported = \
     TypeError("T is not a numpy.ndarray or a scipy.sparse matrix.")
 
@@ -19,10 +31,7 @@ _type_not_supported = \
 
 # DONE: Ben, Frank
 def tpt(T, A, B, mu=None, qminus=None, qplus=None, rate_matrix=False):
-    r""" Computes the A->B reactive flux using transition path theory (TPT)
-    
-    Uses TPT (originally introduced in [1]) in its discrete form [2]. Here,
-    we use the transition matrix formulation described in [3]. 
+    r""" Computes the A->B reactive flux using transition path theory (TPT)  
     
     Parameters
     ----------
@@ -44,7 +53,7 @@ def tpt(T, A, B, mu=None, qminus=None, qplus=None, rate_matrix=False):
         
     Returns
     -------
-    tpt: emma2.msm.analysis.ReactiveFlux object
+    tpt: emma2.msm.flux.ReactiveFlux object
         A python object containing the reactive A->B flux network
         and several additional quantities, such as stationary probability,
         committors and set definitions.
@@ -52,7 +61,11 @@ def tpt(T, A, B, mu=None, qminus=None, qplus=None, rate_matrix=False):
     Notes
     -----
     The central object used in transition path theory is
-    the forward and backward comittor function.
+    the forward and backward comittor function. 
+
+    TPT (originally introduced in [1]) for continous systems has a
+    discrete version outlined in [2]. Here, we use the transition
+    matrix formulation described in [3].
 
     See also
     --------
@@ -66,10 +79,11 @@ def tpt(T, A, B, mu=None, qminus=None, qplus=None, rate_matrix=False):
     .. [2] P. Metzner, C. Schuette and E. Vanden-Eijnden.
         Transition Path Theory for Markov Jump Processes. 
         Multiscale Model Simul 7: 1192-1219 (2009)
-    .. [3] F. Noe, Ch. Schuette, E. Vanden-Eijnden, L. Reich and T. Weikl: 
-        Constructing the Full Ensemble of Folding Pathways from Short Off-Equilibrium Simulations. 
+    .. [3] F. Noe, Ch. Schuette, E. Vanden-Eijnden, L. Reich and
+        T. Weikl: Constructing the Full Ensemble of Folding Pathways
+        from Short Off-Equilibrium Simulations.
         Proc. Natl. Acad. Sci. USA, 106, 19011-19016 (2009)
-    
+        
     """
     import emma2.msm.analysis as msmana
 
@@ -168,10 +182,11 @@ def flux_matrix(T, pi, qminus, qplus, netflux=True):
     .. [2] P. Metzner, C. Schuette and E. Vanden-Eijnden.
         Transition Path Theory for Markov Jump Processes. 
         Multiscale Model Simul 7: 1192-1219 (2009)
-    .. [3] F. Noe, Ch. Schuette, E. Vanden-Eijnden, L. Reich and T. Weikl: 
-        Constructing the Full Ensemble of Folding Pathways from Short Off-Equilibrium Simulations. 
+    .. [3] F. Noe, Ch. Schuette, E. Vanden-Eijnden, L. Reich and
+        T. Weikl: Constructing the Full Ensemble of Folding Pathways
+        from Short Off-Equilibrium Simulations.
         Proc. Natl. Acad. Sci. USA, 106, 19011-19016 (2009)
-    
+        
     """
     if issparse(T):
         return sparse.tpt.flux_matrix(T, pi, qminus, qplus, netflux=netflux)
@@ -182,10 +197,7 @@ def flux_matrix(T, pi, qminus, qplus, netflux=True):
 
 
 def to_netflux(flux):
-    r"""Compute the netflux from the gross flux.
-    
-        f_ij^{+}=max{0, f_ij-f_ji}
-        for all pairs i,j
+    r"""Compute the netflux from the gross flux.   
     
     Parameters
     ----------
@@ -196,6 +208,15 @@ def to_netflux(flux):
     -------
     netflux : (M, M) ndarray
         Matrix of netflux values between pairs of states.
+        
+    Notes
+    -----
+    The netflux or effective current is defined as
+    
+    .. math:: f_{ij}^{+}=\max \{ f_{ij}-f_{ji}, 0 \}
+    
+    :math:`f_{ij}` is the flux for the transition from :math:`A` to
+    :math:`B`.
     
     References
     ----------
@@ -217,13 +238,15 @@ def flux_production(F):
     
     Parameters
     ----------
-    F : (n, n) ndarray
+    F : (M, M) ndarray
         Matrix of flux values between pairs of states.
     
     Returns
     -------
-    prod : (n) ndarray
-        array with flux production (positive) or consumption (negative) at each state
+    prod : (M,) ndarray
+        Array containing flux production (positive) or consumption
+        (negative) at each state
+        
     """
     return dense.tpt.flux_production(F) # works for dense or sparse
 
@@ -233,7 +256,7 @@ def flux_producers(F, rtol=1e-05, atol=1e-12):
     
     Parameters
     ----------
-    F : (n, n) ndarray
+    F : (M, M) ndarray
         Matrix of flux values between pairs of states.
     rtol : float
         relative tolerance. fulfilled if max(outflux-influx, 0) / max(outflux,influx) < rtol
@@ -242,11 +265,11 @@ def flux_producers(F, rtol=1e-05, atol=1e-12):
     
     Returns
     -------
-    producers : (n) ndarray of int
-        indexes of states that are net flux producers. May include "dirty" producers, i.e.
-        states that have influx but still produce more outflux and thereby violate flux
-        conservation.
-    
+    producers : (M, ) ndarray of int
+        indexes of states that are net flux producers. May include
+        "dirty" producers, i.e.  states that have influx but still
+        produce more outflux and thereby violate flux conservation.
+        
     """
     return dense.tpt.flux_producers(F) # works for dense or sparse
 
@@ -256,7 +279,7 @@ def flux_consumers(F, rtol=1e-05, atol=1e-12):
     
     Parameters
     ----------
-    F : (n, n) ndarray
+    F : (M, M) ndarray
         Matrix of flux values between pairs of states.
     rtol : float
         relative tolerance. fulfilled if max(outflux-influx, 0) / max(outflux,influx) < rtol
@@ -265,22 +288,17 @@ def flux_consumers(F, rtol=1e-05, atol=1e-12):
     
     Returns
     -------
-    producers : (n) ndarray of int
-        indexes of states that are net flux producers. May include "dirty" producers, i.e.
-        states that have influx but still produce more outflux and thereby violate flux
-        conservation.
-    
+    producers : (M, ) ndarray of int
+        indexes of states that are net flux producers. May include
+        "dirty" producers, i.e.  states that have influx but still
+        produce more outflux and thereby violate flux conservation.
+        
     """
     return dense.tpt.flux_consumers(F) # works for dense or sparse
 
 
 def coarsegrain(F, sets):
-    r"""Coarse-grains the flux to the given sets
-    
-    $fc_{i,j} = \sum_{i \in I,j \in J} f_{i,j}$
-    Note that if you coarse-grain a net flux, it does not necessarily have a net
-    flux property anymore. If want to make sure you get a netflux, 
-    use to_netflux(coarsegrain(F,sets)).
+    r"""Coarse-grains the flux to the given sets. 
     
     Parameters
     ----------
@@ -288,13 +306,24 @@ def coarsegrain(F, sets):
         Matrix of flux values between pairs of states.
     sets : list of array-like of ints
         The sets of states onto which the flux is coarse-grained.
+
+    Notes
+    -----
+    The coarse grained flux is defined as
+
+    .. math:: fc_{I,J} = \sum_{i \in I,j \in J} f_{i,j}
+    
+    Note that if you coarse-grain a net flux, it does n ot necessarily
+    have a net flux property anymore. If want to make sure you get a
+    netflux, use to_netflux(coarsegrain(F,sets)).
     
     References
     ----------
-    .. [1] F. Noe, Ch. Schuette, E. Vanden-Eijnden, L. Reich and T. Weikl: 
-        Constructing the Full Ensemble of Folding Pathways from Short Off-Equilibrium Simulations. 
+    .. [1] F. Noe, Ch. Schuette, E. Vanden-Eijnden, L. Reich and
+        T. Weikl: Constructing the Full Ensemble of Folding Pathways
+        from Short Off-Equilibrium Simulations.
         Proc. Natl. Acad. Sci. USA, 106, 19011-19016 (2009)
-    
+        
     """
     if issparse(F):
         return sparse.tpt.coarsegrain(F, sets)
@@ -310,28 +339,28 @@ def coarsegrain(F, sets):
 
 
 def total_flux(F, A = None):
-    r"""Compute the total flux, or turnover flux, that is produced by the
-        flux sources and consumed by the flux sinks
-    
+    r"""Compute the total flux, or turnover flux, that is produced by
+        the flux sources and consumed by the flux sinks.
+        
     Parameters
     ----------
-    F : (n, n) ndarray
+    F : (M, M) ndarray
         Matrix of flux values between pairs of states.
     A : array_like (optional)
         List of integer state labels for set A (reactant)
-    
+        
     Returns
     -------
     F : float
-        The total flux, or turnover flux, that is produced by the
-        flux sources and consumed by the flux sinks
-    
+        The total flux, or turnover flux, that is produced by the flux
+        sources and consumed by the flux sinks
+        
     References
     ----------
     .. [1] P. Metzner, C. Schuette and E. Vanden-Eijnden.
         Transition Path Theory for Markov Jump Processes. 
         Multiscale Model Simul 7: 1192-1219 (2009)
-    
+        
     """
     if issparse(F):
         return sparse.tpt.total_flux(F, A = A)
@@ -369,23 +398,24 @@ def rate(totflux, pi, qminus):
     (TPT). The transition rate, i.e. the total number of reaction events per
     time step, is given in [1] as:
 
-    .. math:: k_{AB}=\frac{1}{F} \sum_i \pi_i q_i^-
+    .. math:: k_{AB}=\frac{1}{F} \sum_i \pi_i q_i^{(-)}
 
     :math:`F` is the total flux for the transition from :math:`A` to
     :math:`B`.
     
     References
     ----------
-    .. [1] F. Noe, Ch. Schuette, E. Vanden-Eijnden, L. Reich and T. Weikl: 
-        Constructing the Full Ensemble of Folding Pathways from Short Off-Equilibrium Simulations. 
+    .. [1] F. Noe, Ch. Schuette, E. Vanden-Eijnden, L. Reich and
+        T. Weikl: Constructing the Full Ensemble of Folding Pathways
+        from Short Off-Equilibrium Simulations.
         Proc. Natl. Acad. Sci. USA, 106, 19011-19016 (2009)
-    
+        
     """
     return dense.tpt.rate(totflux, pi, qminus)
 
 
 def mfpt(totflux, pi, qminus):
-    r"""Mean first passage time for reaction A to B. Equal to the inverse rate in [1]
+    r"""Mean first passage time for reaction A to B.
     
     Parameters
     ----------
@@ -406,12 +436,17 @@ def mfpt(totflux, pi, qminus):
     --------
     rate
     
+    Notes
+    -----
+    Equal to the inverse rate, see [1].
+    
     References
     ----------
-    .. [1] F. Noe, Ch. Schuette, E. Vanden-Eijnden, L. Reich and T. Weikl: 
-        Constructing the Full Ensemble of Folding Pathways from Short Off-Equilibrium Simulations. 
+    .. [1] F. Noe, Ch. Schuette, E. Vanden-Eijnden, L. Reich and
+        T. Weikl: Constructing the Full Ensemble of Folding Pathways
+        from Short Off-Equilibrium Simulations.
         Proc. Natl. Acad. Sci. USA, 106, 19011-19016 (2009)
-    
+        
     """
     return dense.tpt.mfpt(totflux, pi, qminus)
 
@@ -422,31 +457,31 @@ def mfpt(totflux, pi, qminus):
 # ======================================================================
 
 def pathways(F, A, B, qplus, fraction = 1.0, totalflux = None):
-    r"""
-    Performs a pathway decomposition of the net flux.
+    r"""Pathway decomposition of the net flux.
     
-    Parameters:
-    -----------
-    F : (n, n) ndarray
+    Parameters
+    ----------
+    F : (M, M) ndarray
         Matrix of flux values between pairs of states.
     A : array-like of ints
         A states (source, educt states)
     B : array-like of ints
         B states (sinks, product states)
-    qplus : (n) ndarray
+    qplus : (M,) ndarray
         Forward committor
-    fraction = 1.0 : float
-        The fraction of the total flux for which pathways will be computed.
-        When set larger than 1.0, will use 1.0. When set <= 0.0, no
-        pathways will be computed and two empty lists will be returned.
-        For example, when set to fraction = 0.9, the pathway decomposition 
-        will stop when 90% of the flux have been accumulated. This is very
-        useful for large flux networks which often contain a few major and
-        a lot of minor paths. In such networks, the algorithm would spend a
-        very long time in the last few percent of pathways
+    fraction = float (optional)
+        The fraction of the total flux for which pathways will be
+        computed.  When set larger than 1.0, will use 1.0. When set <=
+        0.0, no pathways will be computed and two empty lists will be
+        returned.  For example, when set to fraction = 0.9, the
+        pathway decomposition will stop when 90% of the flux have been
+        accumulated. This is very useful for large flux networks which
+        often contain a few major and a lot of minor paths. In such
+        networks, the algorithm would spend a very long time in the
+        last few percent of pathways
     
-    Returns:
-    --------
+    Returns
+    -------
     (paths,pathfluxes) : (list of int-arrays, double-array)
         paths in the order of decreasing flux. Each path is given as an 
         int-array of state indexes, ordered by increasing forward committor 
