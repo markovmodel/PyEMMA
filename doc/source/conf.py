0440496edc6b08a9072fc2be2e3051f7a9e9895b
marscher <m.scherer@fu-berlin.de>
2014-10-07 17:33:15 +0200

    [sphinx] if rtd_theme is not found, use default (which may look ugly, but works at least)

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -111,9 +111,12 @@ pygments_style = 'sphinx'
 on_rtd = os.environ.get('READTHEDOCS', None) == 'True'
 
 if not on_rtd:  # only import and set the theme if we're building docs locally
-    import sphinx_rtd_theme
-    html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
-    html_theme = 'sphinx_rtd_theme'
+    try:
+        import sphinx_rtd_theme
+        html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
+        html_theme = 'sphinx_rtd_theme'
+    except ImportError:
+        html_theme = 'default'
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
