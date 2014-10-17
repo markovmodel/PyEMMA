8aaadaf0e65fdb6e04542da312478860f50b1ec5
marscher <m.scherer@fu-berlin.de>
2014-10-17 16:59:04 +0200

    [mle_trev_tests] skip some matrices sizes in randomized test (reduced input size by factor 5)

--- pyemma/msm/estimation/tests/test_mle_trev_given_pi.py
+++ pyemma/msm/estimation/tests/test_mle_trev_given_pi.py
@@ -8,21 +8,20 @@ from pyemma.msm.estimation.dense.transition_matrix import transition_matrix_reve
 from pyemma.msm.estimation import tmatrix as apicall
 
 class Test_mle_trev_given_pi(unittest.TestCase):
-    
+
     def setUp(self):
         self.eps = 1.0E-6
-    
+
     def test_mle_trev_given_pi(self):
-        for n in xrange(3,100):
-            C  = (1000*np.random.rand(n,n)).astype(int)
-            #print C
+        for n in xrange(3, 100, 5):
+            C = (1000*np.random.rand(n,n)).astype(int)
             pi = np.random.rand(n)
-            pi /= sum(pi)   
+            pi /= sum(pi)
 
             T_cython_dense = mtrgpd(C,pi,eps=self.eps)
             T_cython_sparse = mtrgps(scipy.sparse.csr_matrix(C),pi,eps=self.eps).toarray()
 
-            Cf  = C.astype(float)
+            Cf = C.astype(float)
             for i in xrange(n):
                 if Cf[i,i] == 0:
                     Cf[i,i] = self.eps
@@ -40,4 +39,4 @@ class Test_mle_trev_given_pi(unittest.TestCase):
 
 if __name__ == '__main__':
     unittest.main()
-    
+
