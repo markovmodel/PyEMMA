273eb01f362ce584d4c2b50f3d1c4fc90e3faf56
marscher <m.scherer@fu-berlin.de>
2015-01-19 21:50:20 +0100

    use tica instead of pca

--- pyemma/coordinates/coordinate_transformation/discretizer.py
+++ pyemma/coordinates/coordinate_transformation/discretizer.py
@@ -1,3 +1,4 @@
+from coordinates.coordinate_transformation.tica import TICA
 __author__ = 'noe'
 
 from featurizer import Featurizer
@@ -18,40 +19,51 @@ class Discretizer(object):
         """
         # create featurizer
         featurizer = Featurizer(topfile)
-        # this should be an input!
-        sel = featurizer.selCa()
+        # TODO: move this to a testing function
+        # TODO: where to build chain? User has to provide list of T objects?
+        #sel = featurizer.selCa()
+        sel= [0, 320, 1500]
         pairs = featurizer.pairs(sel)
         featurizer.distances(pairs)
         # feature reader
         reader = FeatureReader(trajfiles, topfile, featurizer)
+
+        self.transformers = []
+
         self.transformers.append(reader)
         # pca output dimension and transformation type should be an input!
-        pca = PCA(reader,2)
+        pca = TICA(reader, lag=10, output_dimension=2)#PCA(reader, 2)
         self.transformers.append(pca)
         # number of states and clustering type should be an input
-        utc = UniformTimeClustering(pca,100)
+        utc = UniformTimeClustering(pca, 100)
         self.transformers.append(utc)
 
+        # ---------------- 
+
         # determine memory requirements
-        M = psutil.virtual_memory()[1] # available RAM
-        print "available RAM: ",M
+        M = psutil.virtual_memory()[1]  # available RAM
+        print "available RAM: ", M
         const_mem = long(0)
         mem_per_frame = long(0)
         for trans in self.transformers:
             mem_per_frame += trans.get_memory_per_frame()
             const_mem += trans.get_constant_memory()
-        print "per-frame memory requirements: ",mem_per_frame
-        chunksize = (M-const_mem) / mem_per_frame # maximum allowed chunk size
+        print "per-frame memory requirements: ", mem_per_frame
+        # maximum allowed chunk size
+        print "const mem: ", const_mem
+        chunksize = (M - const_mem) / mem_per_frame
+        if chunksize < 0:
+            raise MemoryError('Not enough memory for desired transformation chain!')
         # is this chunksize sufficient to store full trajectories?
         chunksize = min(chunksize, np.max(reader.trajectory_lengths()))
-        print "resulting chunk size: ",chunksize
+        print "resulting chunk size: ", chunksize
         # set chunksize
         for trans in self.transformers:
             trans.set_chunksize(chunksize)
 
         # any memory unused? if yes, we can store results
         Mfree = M - const_mem - chunksize * mem_per_frame
-        print "free memory: ",Mfree
+        print "free memory: ", Mfree
 
         # starting from the back of the pipeline, store outputs if possible
         for trans in reversed(self.transformers):
