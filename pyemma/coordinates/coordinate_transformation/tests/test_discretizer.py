0b9d530f02b50799d5b17bf35cafd4201f7703ce
marscher <m.scherer@fu-berlin.de>
2015-01-21 21:41:29 +0100

    refactor to reuse Discretizer

--- pyemma/coordinates/coordinate_transformation/tests/test_discretizer.py
+++ pyemma/coordinates/coordinate_transformation/tests/test_discretizer.py
@@ -7,39 +7,54 @@ import unittest
 from coordinates.coordinate_transformation.discretizer import Discretizer
 from coordinates.coordinate_transformation.tica import TICA
 
+from pyemma.coordinates.transform.tica_amuse import Amuse
+
 import numpy as np
+
+# TODO: clean up
+D = None
+runned_discretizer = False
+files = ['/home/marscher/kchan/traj01_sliced.xtc']
+topo = '/home/marscher/kchan/Traj_Structure.pdb'
+
+
+def run_discretizer():
+    global D
+    if not runned_discretizer:
+        D = Discretizer(files, topo)
+        D.run()
 
 
 class TestDiscretizer(unittest.TestCase):
 
     def testChunksizeResultsTica(self):
-        chunk = 50
-        eps = 1e-4
-
-        d = Discretizer(['/home/marscher/kchan/traj01_sliced.xtc'],
-                        # '/home/marscher/kchan/Traj01.xtc'],
-                        #  '/home/marscher/kchan/Traj_link.xtc'],
-                        '/home/marscher/kchan/Traj_Structure.pdb')
-        # run once with calculated chunk size
-        d.run()
+        chunk = 3
+        eps = 1e-3
+        run_discretizer()
 
         # store mean and cov
-        tica = d.transformers[-1]
+        tica = D.transformers[-1]
         assert isinstance(tica, TICA)
         cov = tica.cov.copy()
         mean = tica.mu.copy()
+        # ------- run again -------
         # reset norming factor
         tica.N = 0
 
-        for t in d.transformers:
+        for t in D.transformers:
             t.parameterized = False
             t.set_chunksize(chunk)
 
-        d.run()
+        D.run()
 
         np.testing.assert_allclose(tica.mu, mean, atol=eps)
         np.testing.assert_allclose(tica.cov, cov, atol=eps)
 
+    @unittest.skip("not impled")
+    def compareWithAmuse(self):
+        # TODO: compare
+        amuse = Amuse.compute(self.files, lag=10)
+        print amuse.mean
 
 if __name__ == "__main__":
     unittest.main()
