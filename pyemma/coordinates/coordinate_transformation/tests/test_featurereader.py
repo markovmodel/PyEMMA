c3886567279b8f6264dda17380c435084ca76db4
marscher <m.scherer@fu-berlin.de>
2015-01-26 22:21:11 +0100

    added test for feature reader (test time lagged access)

---
+++ pyemma/coordinates/coordinate_transformation/tests/test_featurereader.py
@@ -0,0 +1,100 @@
+'''
+Created on 23.01.2015
+
+@author: marscher
+'''
+import tempfile
+import unittest
+
+import mdtraj
+import pkg_resources
+
+from pyemma.coordinates.coordinate_transformation.feature_reader import FeatureReader
+from pyemma.coordinates.coordinate_transformation.featurizer import Featurizer
+from pyemma.coordinates.coordinate_transformation.transformer import Transformer
+import numpy as np
+
+
+def map_return_input(traj):
+    return traj.xyz
+
+
+class MemoryStorage(Transformer):
+
+    def __init__(self, source, chunksize=100, lag=0):
+        Transformer.__init__(self)
+        self.lagged_chunks = []
+        self.final = None
+        self.data_producer = source
+        self._param_finished = False
+
+    def add_chunk(self, X, itraj, t, first_chunk, last_chunk_in_traj, last_chunk, ipass, Y=None):
+        self.lagged_chunks.append(Y)
+        # print t
+        if last_chunk:
+            # TODO: fix shape
+            new_shape = ()
+            # .reshape((2, 1000, 2, 3))
+            self.final = np.array(self.lagged_chunks)
+            self._param_finished = True
+
+
+class TestFeatureReader(unittest.TestCase):
+
+    @classmethod
+    def setUpClass(cls):
+        c = super(TestFeatureReader, cls).setUpClass()
+        # create a fake trajectory which has 2 atoms and coordinates are just a range
+        # over all frames.
+        cls.trajfile = tempfile.mktemp('.xtc')
+        xyz = np.arange(1000 * 2 * 3).reshape((1000, 2, 3))
+        cls.topfile = pkg_resources.resource_filename(
+            'test_featurereader', 'data/test.pdb')
+        t = mdtraj.load(cls.topfile)
+        t.xyz = xyz
+        t.time = np.arange(1000)
+        t.save(cls.trajfile)
+        return c
+
+    def testTimeLaggedAccess(self):
+        # each frame has 2 atoms with 3 coords = 6 coords per frame.
+        # coords are sequential through all frames and start with 0.
+        trajfiles = [self.trajfile]
+
+        # so first lagged frame is 18, since 3 frames a 6 coords are skipped
+        lags = [1, 3, 5, 10, 29, 100]
+
+        chunksizes = [2, 3, 7, 11, 10, 101]
+
+        for lag in lags:
+            for chunksize in chunksizes:
+                if chunksize <= lag:
+                    continue
+                print "lag: %s; chunksize: %s " % (lag, chunksize)
+                f = Featurizer(self.topfile)
+                f.map = map_return_input
+
+                reader = FeatureReader(trajfiles, self.topfile, f)
+
+                m = MemoryStorage(reader, lag=lag)
+
+                for t in [f, reader, m]:
+                    t.chunksize = chunksize
+
+                m.parametrize()
+
+                frist_lagged_chunk = m.lagged_chunks[0]
+                self.assertEqual(frist_lagged_chunk.shape[0], chunksize)
+
+                n_coords = 3 * 2 * 10 * chunksize
+                coords = np.arange(n_coords).reshape((n_coords / 6, 2, 3))
+
+                # first lagged chunk should start at lag and stop at chunksize +
+                # lag
+                np.testing.assert_allclose(
+                    frist_lagged_chunk, coords[lag:chunksize + lag])
+                # TODO: check last lagged frame
+
+
+if __name__ == "__main__":
+    unittest.main()
