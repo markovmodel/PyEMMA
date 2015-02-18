89cabd928749dc2d775f27111da3ed754fad4c85
marscher <m.scherer@fu-berlin.de>
2015-02-18 15:24:22 +0100

    added ChunkedReader interface

---
+++ pyemma/coordinates/io/reader.py
@@ -0,0 +1,17 @@
+'''
+Created on 18.02.2015
+
+@author: marscher
+'''
+
+class ChunkedReader(object):
+    '''
+    classdocs
+    '''
+
+
+    def __init__(self, chunksize=0):
+        '''
+        Constructor
+        '''
+        self.chunksize=chunksize
