b31a855fab092f608107e2f0f0caf1a923b8cb08
marscher <m.scherer@fu-berlin.de>
2015-02-20 22:39:34 +0100

    [test-featurereader] rewrote test with iterators.

--- pyemma/coordinates/tests/test_featurereader.py
+++ pyemma/coordinates/tests/test_featurereader.py
@@ -3,35 +3,22 @@ Created on 23.01.2015
 
 @author: marscher
 '''
-import pkg_resources
+import mdtraj
+import os
 import tempfile
 import unittest
 
-from pyemma.util.log import getLogger
-import mdtraj
-
 from pyemma.coordinates.io.feature_reader import FeatureReader
-from pyemma.coordinates.io.featurizer import MDFeaturizer
 from pyemma.coordinates.transform.transformer import Transformer
+from pyemma.util.log import getLogger
+import pkg_resources
+
+from pyemma.coordinates.util.chaining import build_chain, run_chain
 import numpy as np
-import os
+import cProfile
 
 
 log = getLogger('TestFeatureReader')
-
-
-class MemoryStorage(Transformer):
-    """stores added data in memory (list of ndarrays) """
-
-    def __init__(self, chunksize=100, lag=0):
-        Transformer.__init__(self, chunksize, lag)
-
-        self.lagged_chunks = []
-
-    def param_add_data(self, X, itraj, t, first_chunk, last_chunk_in_traj, last_chunk, ipass, Y=None):
-        self.lagged_chunks.append(Y.xyz)
-        if last_chunk:
-            return True
 
 
 class TestFeatureReader(unittest.TestCase):
@@ -44,6 +31,7 @@ class TestFeatureReader(unittest.TestCase):
         cls.trajfile = tempfile.mktemp('.xtc')
         cls.n_frames = 1000
         cls.xyz = np.arange(cls.n_frames * 2 * 3).reshape((cls.n_frames, 2, 3))
+        log.debug("shape traj: %s" % str(cls.xyz.shape))
         cls.topfile = pkg_resources.resource_filename(
             'pyemma.coordinates.tests.test_featurereader', 'data/test.pdb')
         t = mdtraj.load(cls.topfile)
@@ -57,40 +45,99 @@ class TestFeatureReader(unittest.TestCase):
         os.unlink(cls.trajfile)
         super(TestFeatureReader, cls).tearDownClass()
 
+    def setUp(self):
+        self.pr = cProfile.Profile()
+        self.pr.enable()
+
+    def tearDown(self):
+        from pstats import Stats
+        p = Stats(self.pr)
+        p.sort_stats('cumtime')
+        # p.print_stats()
+        p.print_callers('heavy_function')
+
+    def testIteratorAccess(self):
+        reader = FeatureReader(self.trajfile, self.topfile)
+
+        frames = 0
+        data = []
+        for i, X in reader:
+            frames += X.xyz.shape[0]
+            data.append(X.xyz)
+
+        # restore shape of input
+        data = np.array(data).reshape(self.xyz.shape)
+
+        self.assertEqual(frames, reader.trajectory_lengths()[0])
+        np.testing.assert_equal(data, self.xyz)
+
+    def testIteratorAccess2(self):
+        reader = FeatureReader([self.trajfile, self.trajfile], self.topfile)
+        reader.chunksize = 100
+
+        frames = 0
+        data = []
+        for i, X in reader:
+            frames += X.xyz.shape[0]
+            data.append(X.xyz)
+        self.assertEqual(frames, reader.trajectory_lengths()[0] * 2)
+        # restore shape of input
+        data = np.array(
+            data[0:reader.trajectory_lengths()[0] / reader.chunksize]).reshape(self.xyz.shape)
+
+        np.testing.assert_equal(data, self.xyz)
+
+    def testTimeLaggedIterator(self):
+        lag = 10
+        reader = FeatureReader(self.trajfile, self.topfile)
+        reader.lag = lag
+        frames = 0
+        data = []
+        lagged = []
+        for _, X, Y in reader:
+            frames += X.xyz.shape[0]
+            data.append(X.xyz)
+            lagged.append(Y.xyz)
+
+        merged_lagged = np.array(lagged).reshape(self.xyz.shape)
+
+        # reproduce outcome
+        fake_lagged = np.empty_like(self.xyz)
+        fake_lagged[:-lag] = self.xyz[lag:]
+        fake_lagged[-lag:] = 0
+
+        np.testing.assert_equal(merged_lagged, fake_lagged)
+
+        # restore shape of input
+        data = np.array(data).reshape(self.xyz.shape)
+
+        self.assertEqual(frames, reader.trajectory_lengths()[0])
+        np.testing.assert_equal(data, self.xyz)
+
+    @unittest.skip("")
     def testTimeLaggedAccess(self):
         # each frame has 2 atoms with 3 coords = 6 coords per frame.
         # coords are sequential through all frames and start with 0.
-        trajfiles = [self.trajfile]
 
-        # so first lagged frame is 18, since 3 frames a 6 coords are skipped
-        lags = [1, 3, 5, 10, 29, 100]
+        lags = [2, 200]
 
-        #chunksizes = [2, 3, 7, 11, 10, 101, 512]
-        chunksizes = [2, 4, 16, 24, 48, 64, 127]
+        chunksizes = [1, 100]
 
         for lag in lags:
             for chunksize in chunksizes:
-                if chunksize <= lag:
-                    continue
                 log.info("chunksize=%i\tlag=%i" % (chunksize, lag))
-                f = MDFeaturizer(self.topfile)
-                #f.map = map_return_input
 
-                reader = FeatureReader(trajfiles, self.topfile)
-                reader.feature = f
-
-                m = MemoryStorage(lag=lag)
-                m.data_producer = reader
-                chain = [f, reader, m]
-                for t in chain:
-                    t.chunksize = chunksize
-                m.parametrize()
+                lagged_chunks = []
+                reader = FeatureReader(self.trajfile, self.topfile)
+                reader.chunksize = chunksize
+                reader.lag = lag
+                for _, _, y in reader:
+                    lagged_chunks.append(y.xyz)
 
                 coords = self.xyz
 
-                # all despite last chunk shall have chunksize
-                for ii, c in enumerate(m.lagged_chunks[:-1]):
-                    #log.debug("ind: %i" % ii)
+                for ii, c in enumerate(lagged_chunks[:-1]):
+                    # all despite last chunk shall have chunksize
                     self.assertEqual(c.shape[0], chunksize)
                     # first lagged chunk should start at lag and stop at chunksize +
                     # lag
@@ -104,10 +151,10 @@ class TestFeatureReader(unittest.TestCase):
                 # last lagged chunk should miss "lag" frames of input! e.g
                 # padded to maintain chunksize
 
-                last_chunk = m.lagged_chunks[-1]
-                print last_chunk
+                last_chunk = lagged_chunks[-1]
+                # print last_chunk
                 # when is last_chunk padded?
-                # if 
+                # if
                 # how many zeros are going to be used?
 
 
