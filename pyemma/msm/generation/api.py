8540d50b805920de617de375986ced227212fc24
noe <frank.noe@fu-berlin.de>
2015-03-03 14:51:39 +0100

    implemented trajectory generation in python. 1 Java dependency less.

--- pyemma/msm/generation/api.py
+++ pyemma/msm/generation/api.py
@@ -6,10 +6,173 @@ Created on Jan 8, 2014
 
 import math
 import numpy as np
-import pyemma.util.pystallone as stallone
+import scipy.stats
 
 __all__=['transition_matrix_metropolis_1d',
          'generate_traj']
+
+
+class MarkovChainSampler:
+    """
+    Class for generation of trajectories from a transition matrix P.
+    If many trajectories will be sampled from P, using this class is much more efficient than individual calls to
+    generate_traj because that avoid costly multiple construction of random variable objects.
+
+    """
+
+    def __init__(self, P, dt = 1):
+        """
+        Constructs a sampling object with transition matrix P. The results will be produced every dt'th time step
+
+        Parameters
+        ----------
+        P : (n, n) ndarray
+            transition matrix
+        dt : int
+            trajectory will be saved every dt time steps.
+            Internally, the dt'th power of P is taken to ensure a more efficient simulation.
+
+        """
+        # process input
+        if dt > 1:
+            # take a power of P if requested
+            self.P = np.linalg.matrix_power(P, dt)
+        else:
+            # create local copy and transform to ndarray if in a different format
+            self.P = np.array(P)
+        self.n = self.P.shape[0]
+
+        # initialize mu
+        self.mudist = None
+
+        # generate discrete random value generators for each line
+        self.rgs = np.ndarray( (self.n), dtype=object)
+        for i in range(self.n):
+            nz = np.nonzero(self.P[i])
+            self.rgs[i] = scipy.stats.rv_discrete(values=(nz,self.P[i,nz]))
+
+    def trajectory(self, N, start = None, stop = None):
+        """
+        Generates a trajectory realization of length N, starting from state s
+
+        Parameters
+        ----------
+        N : int
+            trajectory length
+        start : int, optional, default = None
+            starting state. If not given, will sample from the stationary distribution of P
+        stop : int or int-array-like, optional, default = None
+            stopping set. If given, the trajectory will be stopped before N steps
+            once a state of the stop set is reached
+
+        """
+        if start is None:
+            if self.mudist is None:
+                # compute mu, the stationary distribution of P
+                import pyemma.msm.analysis as msmana
+                mu = msmana.statdist(self.P)
+                self.mudist = scipy.stats.rv_discrete( values=( range(self.n), mu ) )
+            # sample starting point from mu
+            start = self.mudist.rvs()
+
+        # evaluate stopping set
+        stopat = np.ndarray((self.n), dtype=bool)
+        stopat[:] = False
+        if (stop != None):
+            for s in np.array(stop):
+                stopat[s] = True
+
+        # result
+        traj = np.zeros( N, dtype=int )
+        traj[0] = start
+        for t in range(1,N):
+            traj[t] = self.rgs[traj[t-1]].rvs()
+            if stopat[traj[t]]:
+                break
+        # return
+        return traj
+
+    def trajectories(self, M, N, start = None, stop = None):
+        """
+        Generates M trajectories, each of length N, starting from state s
+
+        Parameters
+        ----------
+        M : int
+            number of trajectories
+        N : int
+            trajectory length
+        start : int, optional, default = None
+            starting state. If not given, will sample from the stationary distribution of P
+        stop : int or int-array-like, optional, default = None
+            stopping set. If given, the trajectory will be stopped before N steps
+            once a state of the stop set is reached
+
+        """
+        trajs = [self.trajectory(N, start = start, stop = stop) for i in range(M)]
+        return trajs
+
+
+
+def generate_traj(P, N, start = None, stop = None, dt = 1):
+    """
+    Generates a realization of the Markov chain with transition matrix P.
+
+    Parameters
+    ----------
+    P : (n, n) ndarray
+        transition matrix
+    N : int
+        trajectory length
+    start : int, optional, default = None
+        starting state. If not given, will sample from the stationary distribution of P
+    stop : int or int-array-like, optional, default = None
+        stopping set. If given, the trajectory will be stopped before N steps
+        once a state of the stop set is reached
+    dt : int
+        trajectory will be saved every dt time steps.
+        Internally, the dt'th power of P is taken to ensure a more efficient simulation.
+
+    Returns
+    -------
+    traj_sliced : (N/dt, ) ndarray
+        A discrete trajectory with length N/dt
+
+    """
+    sampler = MarkovChainSampler(P, dt = dt)
+    return sampler.trajectory(N, start = start, stop = stop)
+
+
+def generate_trajs(P, M, N, start = None, stop = None, dt = 1):
+    """
+    Generates multiple realizations of the Markov chain with transition matrix P.
+
+    Parameters
+    ----------
+    P : (n, n) ndarray
+        transition matrix
+    M : int
+        number of trajectories
+    N : int
+        trajectory length
+    start : int, optional, default = None
+        starting state. If not given, will sample from the stationary distribution of P
+    stop : int or int-array-like, optional, default = None
+        stopping set. If given, the trajectory will be stopped before N steps
+        once a state of the stop set is reached
+    dt : int
+        trajectory will be saved every dt time steps.
+        Internally, the dt'th power of P is taken to ensure a more efficient simulation.
+
+    Returns
+    -------
+    traj_sliced : (N/dt, ) ndarray
+        A discrete trajectory with length N/dt
+
+    """
+    sampler = MarkovChainSampler(P, dt = dt)
+    return sampler.trajectories(M, N, start = start, stop = stop)
+
 
 def transition_matrix_metropolis_1d(E,d=1.0):
     r"""Transition matrix describing the Metropolis chain jumping
@@ -49,114 +212,3 @@ def transition_matrix_metropolis_1d(E,d=1.0):
     P += np.diag(1.0-np.sum(P,axis=1))
     # done
     return P
-
-
-def generate_traj(T, s, N, dt=1):
-    r"""Generates a realization of the Markov chain with transition
-    matrix T.
-    
-    Parameters
-    ----------
-    T : (M, M) ndarray
-        transition matrix
-    s : int
-        starting state
-    N : int
-        trajectory length
-    dt : int
-        number of steps before saving a state. 
-        
-    Returns
-    -------
-    traj_sliced : (N/dt, ) ndarray
-        A discrete trajectory of length will be N/dt
-        
-    """
-    Tstall = stallone.ndarray_to_stallone_array(T)
-    mc = stallone.API.msmNew.markovChain(Tstall)
-    mc.setStartingState(s)
-    traj_full = mc.randomTrajectory(N).getArray()
-    traj_sliced = (np.array(traj_full))[0:len(traj_full):dt]
-    return traj_sliced
-
-# TODO: Implement in python or call to stallone (FRANK)
-# def trajectory_generator(T, start=None, stop=None):
-#     r"""Generates a markov chain object whose purpose is to generate
-#     trajectories
-#     
-#     The returned object will is able to efficiently generate
-#     trajectories from the Markov chain with transition matrix T. Its
-#     main function is the following:
-# 
-#     trajectory(length=1000, start=None, skip=1, out=None) which
-#     generates an integer sequence of length length/skip from the
-#     specified starting state (when not given will be drawn from the
-#     starting_distribution). The trajectory is of the specified length,
-#     but when skip > 1, only every skip frames will be returned. When
-#     out is specified, the output trajectory will be written there
-#     
-#     Parameters
-#     ----------
-#     start : int or array
-#         distribution from which the initial state of generated
-#         trajectories will be drawn. A single int specifies a
-#         deterministic starting state.  When set to None (default), the
-#         uniform distribution will be used. This parameter is
-#         overridden by specifying a specific start state in the call to
-#         trajectory(...)
-#     stop : int or sequence of ints
-#         trajectory will stop before reaching length when it hits one
-#         of the specified states. If it is desired that trajectories
-#         should always run until the stop state(s) are hit, then set
-#         the trajectory length to infinite. Note that this might
-#         produce a memory error though, when trajectories become too
-#         long to be stored.
-#     
-#     """
-#     raise NotImplementedError("This is not implemented yet.")
-# 
-# # TODO: Implement in python or call to stallone (FRANK)
-# def trajectory(T, length=1000, start=None, stop=None, skip=1, out=None):
-#     r"""Generates a Markov chain trajectory using transition matrix T
-#     
-#     The starting state is either deterministic (fixed by setting the
-#     start- value to an integer < n, where n is the row/column-size of
-#     T, e.g. 'start=0') or drawn from a user-specified distribution of
-#     length n (e.g. 'start=numpy.array([0.5, 0.4, 0.1])'.  The
-#     trajectory will be run until it has reached the specified length,
-#     or until it has hit a state from the set of stop states specified.
-#     
-#     Parameters
-#     ----------
-#     T : array
-#         sparse or dense quadratic array (size n x n)
-#     start: int or array
-#         when set to None, the starting state will be chosen uniformly
-#         random when set to an int, the trajectory will start with this
-#         state.  when set to a n-sized array, the starting state will
-#         be randomly generated from this distribution.
-#     stop : int or sequence of ints
-#         trajectory will stop before reaching length when it hits one
-#         of the specified states. If it is desired that trajectories
-#         should always run until the stop state(s) are hit, then set
-#         the trajectory length to infinite. Note that this might
-#         produce a memory error though, when trajectories become too
-#         long to be stored.
-#     
-#     Returns
-#     -------
-#     traj : array
-#         an array of size length/skip. When stop is set, then the
-#         returned trajectory is shorter than length/skip when one of
-#         the stopping states has been hit
-#     
-#     Raises
-#     -------
-#     ValueError
-#         If T is nonquadratic, if starting distribution has the wrong
-#         length, if start or stop states have been specified outside
-#         [0,n-1].
-#         
-#     """
-#     raise NotImplementedError("This is not implemented yet.")
-#     
