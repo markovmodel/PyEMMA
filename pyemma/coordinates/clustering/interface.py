9f76b6338fbbf2baf8122d78d2431db193831dfc
marscher <m.scherer@fu-berlin.de>
2015-03-12 14:54:26 +0100

    [discretizer] add output_dir parameter to save_dtrajs

--- pyemma/coordinates/clustering/interface.py
+++ pyemma/coordinates/clustering/interface.py
@@ -41,14 +41,11 @@ class AbstractClustering(Transformer):
             names of input trajectory files, will be used generate output files.
         prefix : str
             prepend prefix to filenames.
-
         output_dir : str
             save files to this directory.
-
         output_format : str
             if format is 'ascii' dtrajs will be written as csv files, otherwise
             they will be written as NumPy .npy files.
-
         extension : str
             file extension to append (eg. '.itraj')
         """
