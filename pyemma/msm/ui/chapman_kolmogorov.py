99477224d611f331f5e6a9a40b66f97b01d6afcb
Benjamin Trendelkamp-Schroer <benjamin.trendelkamp-schroer@fu-berlin.de>
2015-03-25 11:32:43 +0100

    [msm/ui] New implementation of Chapman-Kolmogorov test. The test for the MSM at k*tau is now carried out on time-dependent meta-stable sets A(k*\tau). This is necessary to take into account that the largest connected set at k*\tau and the largest connected set at \tau will be different in general.

--- pyemma/msm/ui/chapman_kolmogorov.py
+++ pyemma/msm/ui/chapman_kolmogorov.py
@@ -1,42 +1,20 @@
-r"""Chapman-Kolmogorov test"""
-import numpy as np
+r"""Chapman-Kolmogorov-Test
 
-from warnings import warn
+.. moduleauthor:: B.Trendelkamp-Schroer <benjamin DOT trendelkamp-schroer AT fu-berlin DOT de>
+
+"""
+import warnings
+
+import numpy as np
+from scipy.sparse import issparse
 
 from pyemma.msm.estimation import cmatrix, connected_cmatrix, largest_connected_set, tmatrix
 from pyemma.msm.analysis import statdist
 from pyemma.msm.analysis import pcca
 
+from mapping import MapToConnectedStateLabels
+
 __all__=['cktest']
-
-class MapToConnectedStateLabels():
-    def __init__(self, lcc):
-        self.lcc=lcc
-        self.new_labels=np.arange(len(self.lcc))
-        self.dictmap=dict(zip(self.lcc, self.new_labels))
-    
-    def map(self, A):
-        """Map subset of microstates to subset of connected
-        microstates.
-
-        Parameters
-        ----------
-        A : list of int
-            Subset of microstate labels
-
-        Returns
-        -------
-        A_cc : list of int
-            Corresponding subset of mircrostate labels
-            in largest connected set lcc. 
-
-        """
-        if not set(A).issubset(set(self.lcc)):
-            raise ValueError("A is not a subset of the set of "+\
-                                 "completely connected states.")
-        else:
-            return [self.dictmap[i] for i in A]
-
 
 def pcca_sets(P, n, lcc):
     r"""Compute partition into Perron clusters.
@@ -62,11 +40,49 @@ def pcca_sets(P, n, lcc):
         sets.append(lcc[pcca_ind==i])
     return sets
 
-def cktest(dtrajs, lag, K, nsets=2, sets=None):
+def propagate(W, P, n=1):
+    r"""Propagate probabilities.
+
+    Parameters
+    ----------
+    W : (K, M) ndarray
+        Matrix of vectors
+    P : (M, M) ndarray or scipy.sparse matrix
+        Transition matrix
+    n : int (optional)
+        Number of steps
+
+    Returns
+    -------
+    W_n : (K, M) ndarray
+        Matrix of propagated vectors
+        
+    """
+    if issparse(P):
+        """Ensure csr format"""
+        P = P.tocsr()        
+        """Transpose W and P"""
+        WT = W.T
+        PT = P.T
+        for i in range(n):
+            WT = PT.dot(WT)
+        """Transpose propgated WT"""    
+        W_n = WT.T
+    else:
+        W_n = 1.0*W
+        for i in range(n):
+            W_n = np.dot(W_n, P)
+    return W_n                        
+
+def cktest(T_MSM, lcc_MSM, dtrajs, lag, K, nsets=2, sets=None, full_output=False):
     r"""Perform Chapman-Kolmogorov tests for given data.
 
     Parameters
     ----------
+    T_MSM : (M, M) ndarray or scipy.sparse matrix
+        Transition matrix of estimated MSM
+    lcc_MSM : array-like
+        Largest connected set of the estimated MSM
     dtrajs : list
         discrete trajectories
     lag : int
@@ -77,17 +93,21 @@ def cktest(dtrajs, lag, K, nsets=2, sets=None):
         number of PCCA sets on which to perform the test
     sets : list, optional
         List of user defined sets for the test
+    full_output : bool, optional
+        Return additional information about set_factors
 
     Returns
     -------
-    p_MSM : (K, n_sets) ndarray
+    p_MSM : (K, nsets) ndarray
         p_MSM[k, l] is the probability of making a transition from
         set l to set l after k*lag steps for the MSM computed at 1*lag
-    p_MD : (K, n_sets) ndarray
+    p_MD : (K, nsets) ndarray
         p_MD[k, l] is the probability of making a transition from
         set l to set l after k*lag steps as estimated from the given data
-    eps_MD : (K, n_sets)
-        eps_MD[k, l] is an estimate for the statistical error of p_MD[k, l]   
+    eps_MD : (K, nsets)
+        eps_MD[k, l] is an estimate for the statistical error of p_MD[k, l]    
+    set_factors : (K, nsets) ndarray, optional
+        set_factor[k, i] is the quotient of the MD and the MSM set probabilities
 
     References
     ----------
@@ -96,72 +116,102 @@ def cktest(dtrajs, lag, K, nsets=2, sets=None):
         molecular kinetics: Generation and validation. J Chem Phys
         134: 174105
         
-    """
-    C_1=cmatrix(dtrajs, lag, sliding=True)
-    lcc_1=largest_connected_set(C_1)
-    
-    """Compute PCCA-sets from MSM at lagtime 1*\tau"""    
-    if sets is None:
-        Ccc_1=connected_cmatrix(C_1, lcc=lcc_1)
-        T_1=tmatrix(Ccc_1)
-        sets=pcca_sets(T_1.toarray(), nsets, lcc_1)
-
+    """    
     p_MD = np.zeros((K, nsets))
     p_MSM = np.zeros((K, nsets))
-    eps_MD = np.zeros((K, nsets))
+    eps_MD = np.zeros((K, nsets))    
+    set_factors = np.zeros((K, nsets))
+
+    if sets is None:
+        """Compute PCCA-sets from MSM at lagtime 1*\tau"""    
+        if issparse(T_MSM):
+            msg = ("Converting sparse transition matrix to dense\n"
+                   "since PCCA is currently only implemented for dense matrices.\n"
+                   "You can avoid automatic conversion to dense arrays by\n"
+                   "giving sets for the Chapman-Kolmogorov test explicitly")
+            warnings.warn(msg, UserWarning)
+            sets=pcca_sets(T_MSM.toarray(), nsets, lcc_MSM)
+        else:
+            sets=pcca_sets(T_MSM, nsets, lcc_MSM)
+    nsets = len(sets)
+
+    """Stationary distribution at 1*tau"""
+    mu_MSM = statdist(T_MSM)  
     
-    for k in range(1, K, 10):  
-        print k
-        C_k = cmatrix(dtrajs, (k+1)*lag, sliding=True)
-        lcc_k = largest_connected_set(C_k)
+    """Mapping to lcc at lagtime 1*tau"""
+    lccmap_MSM = MapToConnectedStateLabels(lcc_MSM)
 
-        lcc = np.intersect1d(lcc_1, lcc_k)
+    """Compute stationary distribution on sets"""
+    w_MSM_1 = np.zeros((nsets, mu_MSM.shape[0]))
+    for l in range(nsets):
+        A = sets[l]
+        A_MSM = lccmap_MSM.map(A)
+        w_MSM_1[l, A_MSM] = mu_MSM[A_MSM]/mu_MSM[A_MSM].sum()    
 
-        Ccc_1 = connected_cmatrix(C_1, lcc=lcc)
-        Ccc_k = connected_cmatrix(C_k, lcc=lcc)
+    w_MSM_k = 1.0*w_MSM_1    
 
-        T_1 = tmatrix(Ccc_1).toarray()
-        T_k = tmatrix(Ccc_k).toarray()
+    p_MSM[0, :] = 1.0
+    p_MD[0, :] = 1.0
+    eps_MD[0, :] = 0.0
+    set_factors[0, :] = 1.0
 
-        mu = statdist(T_1)
+    for k in range(1, K): 
+        """Propagate probability vectors for MSM"""
+        w_MSM_k = propagate(w_MSM_k, T_MSM)        
 
-        T_1_k = np.linalg.matrix_power(T_1, k+1)
+        """Estimate model at k*tau"""
+        C_MD = cmatrix(dtrajs, (k+1)*lag, sliding=True)
+        lcc_MD = largest_connected_set(C_MD)
+        Ccc_MD = connected_cmatrix(C_MD, lcc=lcc_MD)
+        """State counts for MD"""
+        c_MD = Ccc_MD.sum(axis=1)
+        """Transition matrix at k*tau"""
+        T_MD = tmatrix(Ccc_MD)
 
-        lccmap=MapToConnectedStateLabels(lcc)
-        C=Ccc_k.toarray()
-        for l in range(len(sets)):            
-            w=np.zeros(len(lcc))
-            """There is a problem here - the initial sets will probably be bigger than the lcc at time k*\tau"""
+        """Mapping to lcc at lagtime k*tau"""
+        lccmap_MD = MapToConnectedStateLabels(lcc_MD)
+
+        """Intersection of lcc_1 and lcc_k. lcc_k is not necessarily contained within lcc_1"""
+        lcc = np.intersect1d(lcc_MSM, lcc_MD)           
+
+        """Stationary distribution restricted to lcc at lagtime k*tau"""
+        mu_MD = np.zeros(T_MD.shape[0])
+        """Extract stationary values in 'joint' lcc and assining them to their respective position"""
+        mu_MD[lccmap_MD.map(lcc)] = mu_MSM[lccmap_MSM.map(lcc)]        
+
+        """Obtain sets and distribution at k*tau"""
+        w_MD_1 = np.zeros((nsets, mu_MD.shape[0]))
+        for l in range(len(sets)):
+            A = sets[l]
+            """Intersect the set with the lcc at lagtime k*tau"""
+            A_new = np.intersect1d(A, lcc)
+            if A_new.size > 0:           
+                A_MD = lccmap_MD.map(A_new)
+                w_MD_1[l, A_MD] = mu_MD[A_MD]/mu_MD[A_MD].sum()
+
+        """Propagate vector by the MD model"""
+        w_MD_k = propagate(w_MD_1, T_MD)
+
+        """Compute values"""
+        
+        for l in range(len(sets)):
+            A = sets[l]
+            """MSM values"""
+            A_MSM = lccmap_MSM.map(A)
+            p_MSM[k, l] = w_MSM_k[l, A_MSM].sum()
             
-            """Intersect the set with the lcc to avoid having sets 'bigger' than the lcc"""
-            set_l = sets[l]
-            set_l_new = np.intersect1d(lcc, set_l)
-            
-            # inds = lccmap.map(sets[l])
-            inds = np.asarray(lccmap.map(set_l_new))
-            if inds.size==0:
-                warn("""Set %d has empty intersection with
-                               the largest connected set at the
-                               current lagtime %d tau.  The
-                               probability to stay in the set is set
-                               to zero""" %(l, k), RuntimeWarning)                
-                prob_MD = 0.0
-                prob_MSM = 0.0
-            nu = mu[inds]
-            nu /= nu.sum()
-            w[inds] = nu
+            """MD values"""
+            A_new = np.intersect1d(A, lcc)
+            if A_new.size > 0:           
+                A_MD = lccmap_MD.map(A_new)
+                prob_MD = w_MD_k[l, A_MD].sum()
+                p_MD[k, l] = prob_MD
+                """Statistical errors"""
+                c = c_MD[A_MD].sum()
+                eps_MD[k, l]=np.sqrt((k + 1) * (prob_MD - prob_MD**2) / c)   
+                set_factors[k, l] = mu_MSM[lccmap_MSM.map(A_new)].sum()/mu_MSM[A_MSM].sum()
 
-            w_1_k = np.dot(w, T_1_k)
-            w_k = np.dot(w, T_k)
-
-            prob_MD=np.sum(w_k[inds])
-            prob_MSM=np.sum(w_1_k[inds])
-
-            p_MD[k, l] = prob_MD
-            p_MSM[k, l] = prob_MSM
-
-            """Statistical errors"""
-            c=C[inds, :].sum()
-            eps_MD[k, l]=np.sqrt((k + 1) * (prob_MD - prob_MD**2) / c)   
-
-    return p_MSM, p_MD, eps_MD
+    if full_output:
+        return p_MSM, p_MD, eps_MD, set_factors
+    else:
+        return p_MSM, p_MD, eps_MD
---
+++ pyemma/msm/ui/chapman_kolmogorov_test.py
@@ -0,0 +1,126 @@
+r"""Unit test for Chapman-Kolmogorov-Test module
+
+.. moduleauthor:: B.Trendelkamp-Schroer <benjamin DOT trendelkamp-schroer AT fu-berlin DOT de>
+
+"""
+import unittest
+
+import numpy as np
+
+from pyemma.msm.generation import generate_traj
+from pyemma.msm.estimation import cmatrix, largest_connected_set, connected_cmatrix, tmatrix
+from pyemma.msm.analysis import statdist
+
+from pyemma.util.types import ensure_dtraj
+from pyemma.util.numeric import assert_allclose
+
+from birth_death_chain import BirthDeathChain
+from chapman_kolmogorov import cktest
+
+class TestCkTest(unittest.TestCase):
+    
+    def setUp(self):
+        """Store state of the rng"""
+        self.state = np.random.mtrand.get_state()
+
+        """Reseed the rng to enforce 'deterministic' behavior"""
+        np.random.mtrand.seed(42)
+
+        b = 2
+        q = np.zeros(7)
+        p = np.zeros(7)
+        q[1:] = 0.5
+        p[0:-1] = 0.5
+        q[2] = 1.0-10**(-b)
+        q[4] = 10**(-b)
+        p[2] = 10**(-b)
+        p[4] = 1.0-10**(-b)
+
+        bdc = BirthDeathChain(q, p)
+        P = bdc.transition_matrix()
+        dtraj = generate_traj(P, 10000, start=0)
+        tau = 1
+
+        """Estimate MSM"""
+        C_MSM = cmatrix(dtraj, tau)
+        lcc_MSM = largest_connected_set(C_MSM)
+        Ccc_MSM = connected_cmatrix(C_MSM, lcc=lcc_MSM)
+        P_MSM = tmatrix(Ccc_MSM)
+        mu_MSM = statdist(P_MSM)
+
+        """Meta-stable sets"""
+        A = [0, 1, 2]
+        B = [4, 5, 6]
+
+        w_MSM = np.zeros((2, mu_MSM.shape[0]))
+        w_MSM[0, A] = mu_MSM[A]/mu_MSM[A].sum()
+        w_MSM[1, B] = mu_MSM[B]/mu_MSM[B].sum()
+
+        K = 10
+        P_MSM_dense = P_MSM.toarray()
+        
+        p_MSM = np.zeros((K, 2))
+        w_MSM_k = 1.0*w_MSM
+        for k in range(1, K):
+            w_MSM_k = np.dot(w_MSM_k, P_MSM_dense)
+            p_MSM[k, 0]=w_MSM_k[0, A].sum()
+            p_MSM[k, 1]=w_MSM_k[1, B].sum()
+
+        """Assume that sets are equal, A(\tau)=A(k \tau) for all k"""
+        w_MD = 1.0*w_MSM
+        p_MD = np.zeros((K, 2))
+        eps_MD = np.zeros((K, 2))
+        p_MSM[0, :] = 1.0
+        p_MD[0, :] = 1.0
+        eps_MD[0, :] = 0.0
+        for k in range(1, K):
+            """Build MSM at lagtime k*tau"""
+            C_MD = cmatrix(dtraj, (k+1)*tau, sliding=True)
+            lcc_MD = largest_connected_set(C_MD)
+            Ccc_MD = connected_cmatrix(C_MD, lcc=lcc_MD)
+            c_MD = Ccc_MD.sum(axis=1)
+            P_MD = tmatrix(Ccc_MD).toarray()
+            w_MD_k = np.dot(w_MD, P_MD)
+
+            """Set A"""
+            prob_MD = w_MD_k[0, A].sum()            
+            c = c_MD[A].sum()
+            p_MD[k, 0] = prob_MD
+            eps_MD[k, 0] = np.sqrt((k + 1) * (prob_MD - prob_MD**2) / c)
+
+            """Set B"""
+            prob_MD = w_MD_k[1, B].sum()
+            c = c_MD[B].sum()
+            p_MD[k, 1] = prob_MD
+            eps_MD[k, 1] = np.sqrt((k + 1) * (prob_MD - prob_MD**2) / c)          
+
+        """Input"""
+        self.P_MSM = P_MSM
+        self.lcc_MSM = lcc_MSM
+        self.dtraj = dtraj
+        self.tau = tau
+        self.K = K
+        self.A = A
+        self.B = B
+
+        """Expected results"""
+        self.p_MSM = p_MSM
+        self.p_MD = p_MD
+        self.eps_MD = eps_MD
+            
+
+    def tearDown(self):
+        """Revert the state of the rng"""
+        np.random.mtrand.set_state(self.state)
+
+
+    def test_cktest(self):
+        p_MSM, p_MD, eps_MD = cktest(self.P_MSM, self.lcc_MSM, self.dtraj, 
+                                     self.tau, self.K, sets=[self.A, self.B])
+        assert_allclose(p_MSM, self.p_MSM)
+        assert_allclose(p_MD, self.p_MD)
+        assert_allclose(eps_MD, self.eps_MD)
+        
+
+if __name__=="__main__":
+    unittest.main()
