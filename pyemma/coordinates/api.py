0295bae5e473df782287bcb797460f513ba2a5ac
Moritz Hoffmann <clonker@gmail.com>
2015-04-01 15:04:56 +0200

    Moved the implementation detail of creating a file reader into a separate file, as suggested under io/util/reader_utils.py.

--- pyemma/coordinates/api.py
+++ pyemma/coordinates/api.py
@@ -2,6 +2,7 @@ r"""User-API for the pyemma.coordinates package
 
 .. currentmodule:: pyemma.coordinates.api
 """
+from coordinates.io.util.reader_utils import get_file_reader
 
 __docformat__ = "restructuredtext en"
 
@@ -218,7 +219,7 @@ def input(input, featurizer=None, topology=None):
     if isinstance(input, basestring) or (
         isinstance(input, (list, tuple)) and (any(isinstance(item, basestring) for item in input) or len(input) is 0)
     ):
-        reader = _get_file_reader(input, topology, featurizer)
+        reader = get_file_reader(input, topology, featurizer)
     # CASE 2: input is a (T, N, 3) array or list of (T_i, N, 3) arrays
         # check: if single array, create a one-element list
         # check: do all arrays have compatible dimensions (*, N, 3)? If not: raise ValueError.
@@ -230,55 +231,6 @@ def input(input, featurizer=None, topology=None):
         # create MemoryReader
     return reader
 
-
-def _get_file_reader(input, topology, featurizer):
-    if isinstance(input, basestring) or (
-                isinstance(input, (list, tuple)) and (any(isinstance(item, basestring) for item in input) or len(input) is 0)
-    ):
-        reader = None
-        # check: if single string create a one-element list
-        if isinstance(input, basestring):
-            input_list = [input]
-        elif len(input) > 0 and all(isinstance(item, basestring) for item in input):
-            input_list = input
-        else:
-            if len(input) is 0:
-                raise ValueError("The passed input list should not be empty.")
-            else:
-                raise ValueError("The passed list did not exclusively contain strings.")
-
-        try:
-            idx = input_list[0].rindex(".")
-            suffix = input_list[0][idx:]
-        except ValueError as ex:
-            raise ValueError("The specified files %s had no file extension!" % input_list, ex)
-
-        # check: do all files have the same file type? If not: raise ValueError.
-        if all(item.endswith(suffix) for item in input_list):
-            from mdtraj.formats.registry import _FormatRegistry
-
-            # CASE 1.1: file types are MD files
-            if suffix in _FormatRegistry.loaders.keys():
-                # check: do we either have a featurizer or a topology file name? If not: raise ValueError.
-                # create a MD reader with file names and topology
-                if not featurizer and not topology:
-                    raise ValueError("The input files were MD files which makes it mandatory to have either a "
-                                     "featurizer or a topology file.")
-                if not topology:
-                    # we have a featurizer
-                    reader = _FeatureReader.init_from_featurizer(input_list, featurizer)
-                else:
-                    # we have a topology file
-                    reader = _FeatureReader(input_list, topology)
-            else:
-                # TODO: CASE 1.2: file types are raw data files
-                # TODO: create raw data reader from file names
-                pass
-        else:
-            raise ValueError("Not all elements in the input list were of the type %s!" % suffix)
-    else:
-        raise ValueError("Input \"%s\" was no string or list of strings." % input)
-    return reader
 
 # TODO: Alternative names: chain, stream, datastream... probably pipeline is the best name though.
 def pipeline(stages, run=True, param_stride=1):
