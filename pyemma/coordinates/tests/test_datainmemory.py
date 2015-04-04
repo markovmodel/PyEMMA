4b5840c888584c537a9314fca5d0c22415d2758e
marscher <m.scherer@fu-berlin.de>
2015-04-04 11:22:17 +0200

    [datainmemory] support npz; unified how to add (reshape) vectors

--- pyemma/coordinates/tests/test_datainmemory.py
+++ pyemma/coordinates/tests/test_datainmemory.py
@@ -8,7 +8,6 @@ import unittest
 from pyemma.coordinates.io.data_in_memory import DataInMemory
 from pyemma.util.log import getLogger
 import numpy as np
-from pyemma.coordinates.api import kmeans
 
 import tempfile
 import os
@@ -28,24 +27,44 @@ class TestDataInMemory(unittest.TestCase):
         f3 = tempfile.mktemp()
         f4 = tempfile.mktemp(suffix='.npy')
 
+        npz = tempfile.mktemp(suffix='.npz')
+
         np.savetxt(f1, d)
         np.save(f2, d)
 
         np.savetxt(f3, d_1d)
         np.save(f4, d_1d)
 
+        np.savez(npz, d, d)
+
         cls.files2d = [f1, f2]
         cls.files1d = [f3, f4]
         cls.d = d
         cls.d_1d = d_1d
+
+        cls.npz = npz
         return cls
 
     @classmethod
     def tearDownClass(cls):
-        for f in cls.files1d:
-            os.remove(f)
-        for f in cls.files2d:
-            os.remove(f)
+        # try to clean temporary files
+        try:
+            for f in cls.files1d:
+                os.remove(f)
+            for f in cls.files2d:
+                os.remove(f)
+
+            os.remove(cls.npz)
+        except:
+            pass
+
+    def testSingleFile(self):
+        reader = DataInMemory(self.files2d[0])
+
+        self.assertEqual(reader.n_frames_total(), self.d.shape[0])
+
+    def test_npz(self):
+        reader = DataInMemory(self.npz)
 
     def testListOfArrays(self):
 
@@ -58,7 +77,7 @@ class TestDataInMemory(unittest.TestCase):
         self.assertEqual(d.dimension(), dim)
 
         self.assertEqual(
-            d.trajectory_lengths(), [frames_per_traj for i in xrange(3)])
+            d.trajectory_lengths(), [frames_per_traj for _ in xrange(3)])
 
     def testDataArray(self):
         frames_per_traj = 100
@@ -68,22 +87,21 @@ class TestDataInMemory(unittest.TestCase):
         d = DataInMemory(data)
 
         self.assertEqual(
-            d.trajectory_lengths(), [frames_per_traj for i in xrange(1)])
+            d.trajectory_lengths(), [frames_per_traj for _ in xrange(1)])
 
     def test1dData(self):
         n = 3
         data = np.arange(n)
         reader = DataInMemory(data)
 
-        self.assertEqual(reader.trajectory_lengths(), [n])
-        self.assertEqual(reader.dimension(), 3)
-        self.assertEqual(reader.ntraj, 1)
-        self.assertEqual(reader.n_frames_total(), 3)
-
-        k = kmeans(data, k=2)
-        print k.dtrajs
+        self.assertEqual(reader.trajectory_lengths(), [1])
+        self.assertEqual(reader.dimension(), n)
+        self.assertEqual(reader.number_of_trajectories(), 1)
+        self.assertEqual(reader.n_frames_total(), 1)
 
     def test1dDataList_diff_dim(self):
+        # TODO: discuss it shall be possible to use 1d data of different
+        # length?
         n = 3
         data = [np.arange(n), np.arange(n + 1)]
         with self.assertRaises(ValueError):
@@ -96,7 +114,7 @@ class TestDataInMemory(unittest.TestCase):
 
         self.assertEqual(reader.trajectory_lengths(), [1, 1])
         self.assertEqual(reader.dimension(), n)
-        self.assertEqual(reader.ntraj, 2)
+        self.assertEqual(reader.number_of_trajectories(), 2)
         self.assertEqual(reader.n_frames_total(), 2)
 
     def test_file_1d(self):
@@ -111,6 +129,17 @@ class TestDataInMemory(unittest.TestCase):
 
         with self.assertRaises(ValueError):
             DataInMemory(data)
+
+    def test_ndim_input(self):
+        data = np.empty((4, 2, 2, 2))
+
+        reader = DataInMemory(data)
+
+        self.assertEqual(reader.dimension(), 2 * 2 * 2)
+        self.assertEqual(reader.number_of_trajectories(), 1)
+        self.assertEqual(reader.n_frames_total(), 4)
+        self.assertEqual(
+            reader.trajectory_lengths(), [reader.n_frames_total()])
 
     def test_time_lagged_chunked_access(self):
         n = 100
@@ -150,55 +179,44 @@ class TestDataInMemory(unittest.TestCase):
             # increment trajectory
             itraj += 1
 
-    def test_time_lagged_chunked_access_big_lag(self):
+    def test_lagged_iterator(self):
         n = 100
+        chunksize = 10
+        lag = 1
+
         data = [np.random.random((n, 3)), np.zeros((29, 3)),
                 np.random.random((n - 50, 3))]
+        input_lens = [x.shape[0] for x in data]
         reader = DataInMemory(data)
-        reader.chunksize = 27
+        reader.chunksize = chunksize
 
-        self.assertEqual(reader.n_frames_total(), n + n - 50 + 29)
-
-        self.assertEqual(reader.trajectory_length(0), n)
-        self.assertEqual(reader.trajectory_length(1), 29)
-        self.assertEqual(reader.trajectory_length(2), n-50)
+        self.assertEqual(reader.n_frames_total(), sum(input_lens))
 
         # iterate over data
-        lag = 7
-        t = 0
-        itraj = 0
-        last_chunk = False
-        while not last_chunk:
-            last_chunk_in_traj = False
-            t = 0
-            while not last_chunk_in_traj:
-                X, Y = reader._next_chunk(lag=lag)
-                print "X", X.shape
-                print "Y", Y.shape
-                print "-"*80
-                #assert Y.shape[0] > 0
-#                 if itraj == 0:
-#                     self.assertEqual(X.shape, (100, 3))
-#                     self.assertEqual(Y.shape, (70, 3))
-#                 elif itraj == 1:
-#                     # the time lagged chunk can not be built due to lag time
-#                     self.assertEqual(X.shape, (29, 3))
-#                     self.assertEqual(Y.shape, (0, 3))
-#                 elif itraj == 2:
-#                     self.assertEqual(X.shape, (50, 3))
-#                     self.assertEqual(Y.shape, (20, 3))
-                L = np.shape(X)[0]
-                print "L", L
-                print "itraj", itraj
-                # last chunk in traj?
-                last_chunk_in_traj = (
-                    t + L >= reader.trajectory_length(itraj))
-                # last chunk?
-                last_chunk = (
-                    last_chunk_in_traj and itraj >= reader.number_of_trajectories() - 1)
-                t += L
-                print "t", t
-            # increment trajectory
-            itraj += 1
+        chunks = {0: [], 1: [], 2: []}
+        lagged_chunks = {0: [], 1: [], 2: []}
+
+        for itraj, X, Y in reader.iterator(lag=lag):
+            chunks[itraj].append(X)
+            lagged_chunks[itraj].append(Y)
+
+        # check results
+        merged_chunks = [np.array(c) for c in chunks.values()]
+        merged_lagged_chunks = [np.array(c) for c in lagged_chunks.values()]
+
+        logger.debug("shapes: %s" % [x.shape for x in merged_chunks])
+
+        np.testing.assert_equal(
+            merged_chunks[0].reshape(data[0].shape), data[0])
+        logger.debug("from %s to %s" %
+                     (str(merged_chunks[1].shape), str(data[1].shape)))
+        np.testing.assert_equal(
+            merged_chunks[1].reshape(data[1].shape), data[1])
+        np.testing.assert_equal(
+            merged_chunks[2].reshape(data[2].shape), data[2])
+
+        logger.debug(merged_lagged_chunks[0].shape)
+
+
 if __name__ == "__main__":
     unittest.main()
