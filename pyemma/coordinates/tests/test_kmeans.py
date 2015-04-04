4caaad8e297f4d4628b5f8b6f1417045fad5025b
marscher <m.scherer@fu-berlin.de>
2015-04-04 11:27:44 +0200

    [kmeans/test] added 1d case (fails) and cleanup code a bit

--- pyemma/coordinates/tests/test_kmeans.py
+++ pyemma/coordinates/tests/test_kmeans.py
@@ -4,50 +4,51 @@ Created on 28.01.2015
 @author: marscher
 '''
 import unittest
-from pyemma.coordinates.clustering.kmeans import KmeansClustering
-from test_regspace import RandomDataSource
 import tempfile
 import os
+import numpy as np
+from pyemma.coordinates.api import cluster_kmeans
+import shutil
 
 
 class TestKmeans(unittest.TestCase):
 
+    @classmethod
+    def setUpClass(cls):
+        super(TestKmeans, cls).setUpClass()
+        cls.dtraj_dir = tempfile.mkdtemp()
+
     def setUp(self):
         self.k = 5
-        self.kmeans = KmeansClustering(n_clusters=self.k, max_iter=100)
-
-        self.kmeans.data_producer = RandomDataSource()
+        self.dim = 100
+        self.data = [np.random.random((30, self.dim)),
+                     np.random.random((37, self.dim))]
+        self.kmeans = cluster_kmeans(data=self.data, k=self.k, max_iter=100)
 
     def tearDown(self):
-        try:
-            #os.rmdir(self.outdir)
-            pass
-        except:
-            pass
+        shutil.rmtree(self.dtraj_dir, ignore_errors=True)
 
-    def testName(self):
-        self.kmeans.parametrize()
-
+    def testDtraj(self):
         assert self.kmeans.dtrajs[0].dtype == int
 
+    def test_1d_data(self):
+        # check for exception
+        data = np.arange(10)
+
+        kmeans = cluster_kmeans(data, k=7)
+
     def testSaveDtrajs(self):
-        self.kmeans.parametrize()
         prefix = "test"
         extension = ".dtraj"
-        outdir = tempfile.mkdtemp()
-        self.outdir = outdir
-        try:
-            self.kmeans.save_dtrajs(trajfiles=None, prefix=prefix,
+        outdir = self.dtraj_dir
+        self.kmeans.save_dtrajs(trajfiles=None, prefix=prefix,
                                     output_dir=outdir, extension=extension)
-        except:
-            os.rmdir(outdir)
-            raise
 
         names = ["%s_%i%s" % (prefix, i, extension)
-                 for i in xrange(self.kmeans.data_producer.n_samples)]
+                 for i in xrange(self.kmeans.data_producer.number_of_trajectories())]
         names = [os.path.join(outdir, n) for n in names]
 
-        # check files are there
+        # check files with given patterns are there
         for f in names:
             os.stat(f)
 
