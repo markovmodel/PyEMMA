5511cb82eae35ff76570cdc1e3d4b1c1519d463c
noe <frank.noe@fu-berlin.de>
2015-04-07 21:47:13 +0200

    [msm.ui.msm_test]: dragging along refactor changes

--- pyemma/msm/ui/msm_test.py
+++ pyemma/msm/ui/msm_test.py
@@ -14,7 +14,7 @@ from pyemma.util.numeric import assert_allclose, allclose_sparse
 
 from birth_death_chain import BirthDeathChain
 
-from msm import MSM
+from msm import EstimatedMSM
 
 class TestMSM(unittest.TestCase):
 
@@ -38,30 +38,30 @@ class TestMSM(unittest.TestCase):
 
         bdc = BirthDeathChain(q, p)
         P = bdc.transition_matrix()
-        self.dtrajs = generate_traj(P, 10000, start=0)
+        self.dtrajs = [generate_traj(P, 10000, start=0)]
         self.tau = 1
 
         """Estimate MSM"""
-        self.C_MSM = cmatrix(self.dtrajs, self.tau, sliding=True)
+        self.C_MSM = cmatrix(self.dtrajs, self.tau, sliding=True).toarray()
         self.lcc_MSM = largest_connected_set(self.C_MSM)
         self.Ccc_MSM = connected_cmatrix(self.C_MSM, lcc=self.lcc_MSM)
         self.P_MSM = tmatrix(self.Ccc_MSM, reversible=True)
         self.mu_MSM = statdist(self.P_MSM)
         self.k = 3
-        self.ts = timescales(self.P_MSM, k=self.k, tau=self.tau)
+        self.ts = timescales(self.P_MSM, k=self.k+1, tau=self.tau)[1:]
 
     def tearDown(self):
         """Revert the state of the rng"""
         np.random.mtrand.set_state(self.state)
 
     def test_MSM(self):
-        msm = MSM(self.dtrajs, self.tau)
-        assert_allclose(self.dtrajs, msm.discrete_trajectories)
+        msm = EstimatedMSM(self.dtrajs, self.tau)
+        assert_allclose(self.dtrajs, msm.discrete_trajectories_full)
         self.assertEqual(self.tau, msm.lagtime)
         assert_allclose(self.lcc_MSM, msm.largest_connected_set)
-        self.assertTrue(allclose_sparse(self.Ccc_MSM, msm.count_matrix_active))
-        self.assertTrue(allclose_sparse(self.C_MSM, msm.count_matrix_full))
-        self.assertTrue(allclose_sparse(self.P_MSM, msm.transition_matrix))
+        assert_allclose(self.Ccc_MSM, msm.count_matrix_active)
+        assert_allclose(self.C_MSM, msm.count_matrix_full)
+        assert_allclose(self.P_MSM, msm.transition_matrix)
         assert_allclose(self.mu_MSM, msm.stationary_distribution)
         assert_allclose(self.ts, msm.timescales(self.k))
 
