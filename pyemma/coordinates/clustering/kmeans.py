6d20982afa2bb51b7b6dcc91b76b69b0083dc08d
marscher <m.scherer@fu-berlin.de>
2015-04-11 15:17:08 +0200

    [kmeans] cleanup

--- pyemma/coordinates/clustering/kmeans.py
+++ pyemma/coordinates/clustering/kmeans.py
@@ -4,7 +4,6 @@ Created on 22.01.2015
 @author: marscher, noe
 '''
 import numpy as np
-import warnings
 from sklearn.cluster import KMeans as _sklearn_kmeans
 
 from pyemma.util.annotators import doc_inherit
@@ -43,11 +42,6 @@ class KmeansClustering(AbstractClustering):
     def _get_constant_memory(self):
         return 1
 
-    @staticmethod
-    def _ensure2d(X):
-        X = X.reshape((-1, 1))
-        return X
-
     def _map_to_memory(self):
         # resutls mapped to memory during parameterize
         pass
@@ -59,13 +53,16 @@ class KmeansClustering(AbstractClustering):
 #             raise ValueError("Input dimension lower than number of clusters!")
 
     def _param_add_data(self, X, itraj, t, first_chunk, last_chunk_in_traj,
-                       last_chunk, ipass, Y=None, stride=1):
+                        last_chunk, ipass, Y=None, stride=1):
         # first pass: gather data and run k-means
         if ipass == 0:
             # beginning - compute
             if first_chunk:
                 memreq = 1e-6 * 2 * X[0,:].nbytes * self.n_frames_total(stride=stride)
-                self._logger.warn('K-means implementation is currently memory inefficient. This calculation needs '+str(memreq)+' megabytes of main memory. If you get a memory error, try using a larger stride.')
+                self._logger.warn('K-means implementation is currently memory inefficient.'
+                                  ' This calculation needs %i megabytes of main memory.'
+                                  ' If you get a memory error, try using a larger stride.'
+                                  % memreq)
 
             # appends a true copy
             self._chunks.append(X[:,:])
