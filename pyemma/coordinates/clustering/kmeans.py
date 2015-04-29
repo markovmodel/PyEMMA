48fbd44c8d6f0ac8823ee387ec5ac2b72c9f524b
Moritz Hoffmann <clonker@gmail.com>
2015-04-29 15:41:17 +0200

    [kmeans] c extension

--- pyemma/coordinates/clustering/kmeans.py
+++ pyemma/coordinates/clustering/kmeans.py
@@ -1,4 +1,3 @@
-
 # Copyright (c) 2015, 2014 Computational Molecular Biology Group, Free University
 # Berlin, 14195 Berlin, Germany.
 # All rights reserved.
@@ -6,9 +5,9 @@
 # Redistribution and use in source and binary forms, with or without modification,
 # are permitted provided that the following conditions are met:
 #
-#  * Redistributions of source code must retain the above copyright notice, this
+# * Redistributions of source code must retain the above copyright notice, this
 # list of conditions and the following disclaimer.
-#  * Redistributions in binary form must reproduce the above copyright notice,
+# * Redistributions in binary form must reproduce the above copyright notice,
 # this list of conditions and the following disclaimer in the documentation and/or
 # other materials provided with the distribution.
 #
@@ -23,13 +22,15 @@
 # (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
 # SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
-'''
+"""
 Created on 22.01.2015
 
 @author: marscher, noe
-'''
+"""
+
+import kmeans_clustering
+import warnings
 import numpy as np
-from sklearn.cluster import KMeans as _sklearn_kmeans
 
 from pyemma.util.annotators import doc_inherit
 from pyemma.coordinates.clustering.interface import AbstractClustering
@@ -56,8 +57,8 @@ class KmeansClustering(AbstractClustering):
         super(KmeansClustering, self).__init__(metric=metric)
         self.n_clusters = n_clusters
         self.max_iter = max_iter
-        self._algo = _sklearn_kmeans(n_clusters=self.n_clusters, max_iter=self.max_iter)
-        self._chunks = []
+        self._clustercenters = []
+        self._centers_iter_list = []
 
     @doc_inherit
     def describe(self):
@@ -76,37 +77,50 @@ class KmeansClustering(AbstractClustering):
     def _param_add_data(self, X, itraj, t, first_chunk, last_chunk_in_traj,
                         last_chunk, ipass, Y=None, stride=1):
         # first pass: gather data and run k-means
-        if ipass == 0:
-            # beginning - compute
-            if first_chunk:
-                memreq = 1e-6 * 2 * X[0, :].nbytes * self.n_frames_total(stride=stride)
-                self._logger.warn('K-means implementation is currently memory inefficient.'
-                                  ' This calculation needs %i megabytes of main memory.'
-                                  ' If you get a memory error, try using a larger stride.'
-                                  % memreq)
+        if ipass == 0 and first_chunk:
+            # initialize cluster centers
+            self._clustercenters = [
+                np.array(X[np.random.randint(0, len(X))], dtype=np.float32) for _ in xrange(self.n_clusters)
+            ]
 
-            # appends a true copy
-            self._chunks.append(X[:, :])
+        # allocate assigns
+        data_assigns = [0] * len(X)
 
-            # run k-means in the end
-            if last_chunk:
-                # concatenate all data
-                alldata = np.vstack(self._chunks)
-                # free part of the memory
-                del self._chunks
-                # run k-means with all the data
-                self._logger.info("Accumulated all data, running kmeans on "+str(alldata.shape))
-                self._algo.fit(alldata)
+        try:
+            #print "cctype=%s, assignstype=%s, metrictype=%s" % (type(self._clustercenters), type(data_assigns), type(self.metric))
+            chunk = X.astype(np.float32, order='C', copy=False)
+            #print "Feeding chunk with len=%s, type=%s, chunk=%s"%(len(chunk), type(chunk), self._clustercenters)
+            new_centers = kmeans_clustering.cluster(chunk,
+                                                    self._clustercenters, data_assigns,
+                                                    self.metric)
+            self._centers_iter_list.append(new_centers)
+        except RuntimeError:
+            msg = 'Maximum number of cluster centers reached.' \
+                  ' Consider increasing max_clusters or choose' \
+                  ' a larger minimum distance, dmin.'
+            self._logger.warning(msg)
+            warnings.warn(msg)
+            return True
 
-            # done
-            if last_chunk:
-                return True
+        # run k-means in the end
+        done = ipass + 1 >= self.max_iter
+        if last_chunk:
+            #center_sum = np.zeros([self.n_clusters, len(X[0])])
+            center_sum = [np.zeros([len(X[0])], dtype=np.float32) for _ in range(0, self.n_clusters)]
+            for chunk_center in self._centers_iter_list:
+                #print "cc=%s"%chunk_center
+                for idx, chunk_center_element in enumerate(chunk_center):
+                    center_sum[idx] += chunk_center_element
+
+            old_centers = self._clustercenters
+            n_centers_in_iter_list = len(self._centers_iter_list)
+            self._clustercenters = [center / n_centers_in_iter_list for center in center_sum]
+            self._centers_iter_list = []
+            if np.allclose(old_centers, self._clustercenters, rtol=1e-5):
+                done = True
+        return done
 
     def _param_finish(self):
-        self.clustercenters = self._algo.cluster_centers_
-
-    def _map_array(self, X):
-        d = self._algo.predict(X)
-        if d.dtype != self.output_type():
-            d = d.astype(self.output_type())  # convert type if necessary
-        return d[:,None]  # always return a column vector in this function
+        self.clustercenters = np.array(self._clustercenters)
+        del self._clustercenters
+        del self._centers_iter_list
