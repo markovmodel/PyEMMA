681f5f15ceb51a2b6cc8520f45f22a0a6e09f80a
Moritz Hoffmann <clonker@gmail.com>
2015-04-29 17:20:11 +0200

    [kmeans/c extension] Added legal header, some cleanup.

--- pyemma/coordinates/clustering/src/kmeans.c
+++ pyemma/coordinates/clustering/src/kmeans.c
@@ -1,10 +1,35 @@
+/* * Copyright (c) 2015, 2014 Computational Molecular Biology Group, Free University
+ * Berlin, 14195 Berlin, Germany.
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without modification,
+ * are permitted provided that the following conditions are met:
+ *
+ *  * Redistributions of source code must retain the above copyright notice, this
+ * list of conditions and the following disclaimer.
+ *  * Redistributions in binary form must reproduce the above copyright notice,
+ * this list of conditions and the following disclaimer in the documentation and/or
+ * other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS ``AS IS''
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR
+ * ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON
+ * ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
 #include <clustering.h>
 
 static PyObject *cluster(PyObject *self, PyObject *args) {
     int debug;
     debug = 0;
-    if(debug == 1) printf("KMEANS: \n----------- cluster called ----------\n");
-    if(debug == 1) printf("KMEANS: declaring variables...");
+    if(debug) printf("KMEANS: \n----------- cluster called ----------\n");
+    if(debug) printf("KMEANS: declaring variables...");
     PyObject *py_centers, *py_item, *py_res;
     PyArrayObject *np_chunk, *np_item;
     PyObject *py_data_assigns;
@@ -21,25 +46,25 @@ static PyObject *cluster(PyObject *self, PyObject *args) {
     int closest_center_index;
     float (*distance)(float*, float*, size_t, float*, float*);
     PyObject* return_new_centers;
-    if(debug == 1) printf("done.\n");
+    if(debug) printf("done.\n");
 
-    if(debug == 1) printf("KMEANS: initializing some of them...");
+    if(debug) printf("KMEANS: initializing some of them...");
     py_centers = NULL; py_item = NULL; py_res = NULL;
     np_chunk = NULL; np_item = NULL;
     centers = NULL; metric=""; chunk = NULL;
     centers_counter = NULL; new_centers = NULL;
     buffer_a = NULL; buffer_b = NULL;
     return_new_centers = Py_BuildValue("");
-    if(debug == 1) printf("done\n");
+    if(debug) printf("done\n");
 
-    if(debug == 1) printf("KMEANS: attempting to parse args...");
+    if(debug) printf("KMEANS: attempting to parse args...");
     if (!PyArg_ParseTuple(args, "O!O!O!s", &PyArray_Type, &np_chunk, &PyList_Type, &py_centers, &PyList_Type, &py_data_assigns, &metric)) {
         goto error;
     }
-    if(debug == 1) printf("done\n");
+    if(debug) printf("done\n");
 
     /* import chunk */
-    if(debug == 1) printf("KMEANS: importing chunk...");
+    if(debug) printf("KMEANS: importing chunk...");
     if(PyArray_TYPE(np_chunk)!=NPY_FLOAT32) { PyErr_SetString(PyExc_ValueError, "dtype of \"chunk\" isn\'t float (32)."); goto error; };
     if(!PyArray_ISCARRAY_RO(np_chunk) ) { PyErr_SetString(PyExc_ValueError, "\"chunk\" isn\'t C-style contiguous or isn\'t behaved."); goto error; };
     if(PyArray_NDIM(np_chunk)!=2) { PyErr_SetString(PyExc_ValueError, "Number of dimensions of \"chunk\" isn\'t 2."); goto error;  };
@@ -50,9 +75,9 @@ static PyObject *cluster(PyObject *self, PyObject *args) {
         goto error;
     }
     chunk = PyArray_DATA(np_chunk);
-    if(debug == 1) printf("done with N_frames=%d, dim=%d\n", N_frames, dim);
+    if(debug) printf("done with N_frames=%zd, dim=%zd\n", N_frames, dim);
 
-    if(debug == 1) printf("KMEANS: creating metric function...");
+    if(debug) printf("KMEANS: creating metric function...");
     if(strcmp(metric,"euclidean")==0) {
         distance = euclidean_distance;
     } else if(strcmp(metric,"minRMSD")==0) {
@@ -64,10 +89,10 @@ static PyObject *cluster(PyObject *self, PyObject *args) {
         PyErr_SetString(PyExc_ValueError, "metric must be one of \"euclidean\" or \"minRMSD\".");
         goto error;
     }
-    if(debug == 1) printf("done\n");
+    if(debug) printf("done\n");
 
     /* import list of cluster centers */
-    if(debug == 1) printf("KMEANS: importing list of cluster centers...");
+    if(debug) printf("KMEANS: importing list of cluster centers...");
     N_centers = PyList_Size(py_centers);
     if(!(centers = malloc(N_centers*sizeof(float*)))) {
         PyErr_NoMemory(); goto error;
@@ -75,40 +100,40 @@ static PyObject *cluster(PyObject *self, PyObject *args) {
     int l;
     for(i = 0; i < N_centers; ++i) {
         l = 0;
-        if(debug == 1) printf("%d", l++); // 0
+        if(debug) printf("%d", l++); // 0
         py_item = PyList_GetItem(py_centers,i); /* ref:borr. */
-        if(debug == 1) printf("%d", l++); // 1
+        if(debug) printf("%d", l++); // 1
         if(!py_item) goto error;
-        if(debug == 1) printf("%d", l++); // 2
+        if(debug) printf("%d", l++); // 2
         if(!PyArray_Check(py_item)) { PyErr_SetString(PyExc_ValueError, "Elements of centers must be numpy arrays."); goto error; }
-        if(debug == 1) printf("%d", l++); // 3
+        if(debug) printf("%d", l++); // 3
         np_item = (PyArrayObject*)py_item;
-        if(debug == 1) printf("%d", l++); // 4
+        if(debug) printf("%d", l++); // 4
         if(PyArray_TYPE(np_item)!=NPY_FLOAT32) { PyErr_SetString(PyExc_ValueError, "dtype of cluster center isn\'t float (32)."); goto error; };
-        if(debug == 1) printf("%d", l++); // 5
+        if(debug) printf("%d", l++); // 5
         if(!PyArray_ISBEHAVED_RO(np_item) ) { PyErr_SetString(PyExc_ValueError, "cluster center isn\'t behaved."); goto error; };
-        if(debug == 1) printf("%d", l++); // 6
+        if(debug) printf("%d", l++); // 6
         if(PyArray_NDIM(np_item)!=1) { PyErr_SetString(PyExc_ValueError, "Number of dimensions of cluster centers must be 1."); goto error;  };
-        if(debug == 1) printf("%d", l++); // 7
+        if(debug) printf("%d", l++); // 7
         if(np_item->dimensions[0]!=dim) {
           PyErr_SetString(PyExc_ValueError, "Dimension of cluster centers doesn\'t match dimension of frames.");
           goto error;
         }
-        if(debug == 1) printf("%d", l++); // 8
+        if(debug) printf("%d", l++); // 8
         centers[i] = (float*)PyArray_DATA(np_item);
-        if(debug == 1) printf("%d", l++); // 9
+        if(debug) printf("%d", l++); // 9
     }
-    if(debug == 1) printf("done, k=%d\n", N_centers);
+    if(debug) printf("done, k=%zd\n", N_centers);
 
     /* initialize centers_counter and new_centers with zeros */
-    if(debug == 1) printf("KMEANS: initializing calloc centers counter and new_centers stuff...");
+    if(debug) printf("KMEANS: initializing calloc centers counter and new_centers stuff...");
     centers_counter = (int*) calloc(N_centers, sizeof(int));
     new_centers = (float*) calloc(N_centers * dim, sizeof(float));
     if(!centers_counter || !new_centers) { PyErr_NoMemory(); goto error; }
-    if(debug == 1) printf("done\n");
+    if(debug) printf("done\n");
 
     /* do the clustering */
-    if(debug == 1) printf("KMEANS: performing the clustering...");
+    if(debug) printf("KMEANS: performing the clustering...");
     for (i = 0; i < N_frames; i++) {
         mindist = FLT_MAX;
         for(j = 0; j < N_centers; ++j) {
@@ -139,9 +164,9 @@ static PyObject *cluster(PyObject *self, PyObject *args) {
             }
         }
     }
-    if(debug == 1) printf("done\n");
+    if(debug) printf("done\n");
 
-    if(debug == 1) printf("KMEANS: creating return_new_centers...");
+    if(debug) printf("KMEANS: creating return_new_centers...");
     npy_intp dims[2] = {N_centers, dim};
     return_new_centers = PyArray_SimpleNew(2, dims, NPY_FLOAT32);
     if (return_new_centers == NULL){
@@ -149,15 +174,15 @@ static PyObject *cluster(PyObject *self, PyObject *args) {
         goto error;
     }
     void *arr_data = PyArray_DATA((PyArrayObject*)return_new_centers);
-    if(debug == 1) printf("done\n");
+    if(debug) printf("done\n");
     /* Need to copy the data of the malloced buffer to the PyObject
        since the malloced buffer will disappear after the C extension is called. */
-    if(debug == 1) printf("KMEANS: attempting memcopy...");
+    if(debug) printf("KMEANS: attempting memcopy...");
     memcpy(arr_data, new_centers, PyArray_ITEMSIZE((PyArrayObject*) return_new_centers) * N_centers * dim);
-    if(debug == 1) printf("done\n");
-    if(debug == 1) printf("KMEANS: increasting ref to return_new_centers thingy...");
+    if(debug) printf("done\n");
+    if(debug) printf("KMEANS: increasting ref to return_new_centers thingy...");
     Py_INCREF(return_new_centers);  /* The returned list should still exist after calling the C extension */
-    if(debug == 1) printf("done\n");
+    if(debug) printf("done\n");
     /* fall through */
 error:
     free(centers_counter);
