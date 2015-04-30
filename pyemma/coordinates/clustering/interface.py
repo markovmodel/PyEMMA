b7e7367f95c2a0af94957b2f49426448022fcff5
jouno <fab@physik.tu-berlin.de>
2015-04-30 18:44:52 +0200

    [coordinates/clustering] changed dtype of dtrajs from int64 to int32

--- pyemma/coordinates/clustering/interface.py
+++ pyemma/coordinates/clustering/interface.py
@@ -81,7 +81,7 @@ class AbstractClustering(Transformer):
         return 1
 
     def output_type(self):
-        return np.int64
+        return np.int32
 
     def assign(self, X=None, stride=1):
         """
