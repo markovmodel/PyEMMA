2d611e6a1f8478d03119f80480f04536215a7a57
noe <frank.noe@fu-berlin.de>
2015-06-03 13:14:05 +0200

    changed ui to estimators in import

--- pyemma/msm/api.py
+++ pyemma/msm/api.py
@@ -29,9 +29,9 @@ r"""User API for the pyemma.msm package
 
 __docformat__ = "restructuredtext en"
 
-from pyemma.msm.ui.hmsm_estimator import HMSMEstimator as _HMSMEstimator
-from pyemma.msm.ui.msm_bayesian_estimator import BayesianMSMEstimator as _BayesianMSMEstimator
-from pyemma.msm.ui.hmsm_bayesian_estimator import BayesianHMSMEstimator as _BayesianHMSMEstimator
+from pyemma.msm.estimators.hmsm_estimator import HMSMEstimator as _HMSMEstimator
+from pyemma.msm.estimators.msm_bayesian_estimator import BayesianMSMEstimator as _BayesianMSMEstimator
+from pyemma.msm.estimators.hmsm_bayesian_estimator import BayesianHMSMEstimator as _BayesianHMSMEstimator
 
 from flux import tpt as tpt_factory
 from pyemma.msm.estimators.msm_estimator import MSMEstimator as _MSMEstimator
