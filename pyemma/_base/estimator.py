0803e838cb90eb636a72467089153ba178fa8d8e
noe <frank.noe@fu-berlin.de>
2015-06-03 13:43:51 +0200

    [_base.estimator]: extended Base Estimator

--- pyemma/_base/estimator.py
+++ pyemma/_base/estimator.py
@@ -7,5 +7,39 @@ class Estimator(SklearnEstimator):
 
     """
 
+    def set_data(self, X):
+        """ Sets the data on which the estimation will be run.
+
+        Do any necessary updated upon setting or changing the data here.
+
+        Parameters
+        ----------
+        X : object
+            A reference to the data set.
+
+        """
+        raise NotImplementedError('The set_data() method is not implemented for this Estimator.')
+
+    def get_data(self, X):
+        """ Gets a reference to the data on which the estimation is run.
+
+        Do any necessary updated upon setting or changing the data here.
+
+        Returns
+        -------
+        X : object
+            A reference to the data set.
+
+        """
+        raise NotImplementedError('The get_data() method is not implemented for this Estimator.')
+
     def estimate(self, **kwargs):
         raise NotImplementedError('The estimate() method is not implemented for this Estimator.')
+
+    def fit(self, X):
+        """ For compatibility with sklearn. Requires that X has been announced in the constructor
+        :param X:
+        :return:
+        """
+        self.set_data(X)
+        self.estimate()
