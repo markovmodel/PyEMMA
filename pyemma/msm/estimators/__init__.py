60af4c38c43687e170183b8dbb62a4f84d4adc81
noe <frank.noe@fu-berlin.de>
2015-06-06 16:07:59 +0200

    [_base.estimator]: basic tools for parameter scans

--- pyemma/msm/estimators/__init__.py
+++ pyemma/msm/estimators/__init__.py
@@ -1,1 +1,6 @@
 __author__ = 'noe'
+
+from maximum_likelihood_msm import MaximumLikelihoodMSM
+from bayesian_msm import BayesianMSM
+from maximum_likelihood_hmsm import MaximumLikelihoodHMSM
+from bayesian_hmsm import BayesianHMSM
