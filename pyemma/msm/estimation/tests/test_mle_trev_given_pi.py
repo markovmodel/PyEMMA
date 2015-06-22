bc3fde40ea38235addd73812763330c6d120e7fd
jouno <fab@physik.tu-berlin.de>
2015-06-22 16:22:28 +0200

    [msm/estimation] updated unit test (reversible estimation with fixed pi)

--- pyemma/msm/estimation/tests/test_mle_trev_given_pi.py
+++ pyemma/msm/estimation/tests/test_mle_trev_given_pi.py
@@ -34,9 +34,9 @@ import pyemma.util.exceptions
 from os.path import abspath, join
 from os import pardir
 
-from pyemma.msm.estimation.dense.mle_trev_given_pi import mle_trev_given_pi as mtrgpd
-from pyemma.msm.estimation.sparse.mle_trev_given_pi import mle_trev_given_pi as mtrgps
-from pyemma.msm.estimation.dense.transition_matrix import transition_matrix_reversible_fixpi as tmrfp
+from pyemma.msm.estimation.dense.mle_trev_given_pi import mle_trev_given_pi as impl_dense
+from pyemma.msm.estimation.sparse.mle_trev_given_pi import mle_trev_given_pi as impl_sparse
+from pyemma.msm.estimation.dense.transition_matrix import transition_matrix_reversible_fixpi as impl_dense_Frank
 from pyemma.msm.estimation import tmatrix as apicall
 from pyemma.msm.analysis import statdist, is_transition_matrix
 
@@ -45,29 +45,50 @@ testpath = abspath(join(abspath(__file__), pardir)) + '/testfiles/'
 
 class Test_mle_trev_given_pi(unittest.TestCase):
     def setUp(self):
-        self.eps = 0
+        pass
 
     def test_mle_trev_given_pi(self):
         C = np.loadtxt(testpath + 'C_1_lag.dat')
         pi = np.loadtxt(testpath + 'pi.dat')
 
-        T_cython_dense = mtrgpd(C, pi, eps=self.eps)
-        T_cython_sparse = mtrgps(scipy.sparse.csr_matrix(C), pi, eps=self.eps).toarray()
-        T_python = tmrfp(C, pi)
-        T_api_dense = apicall(C, reversible=True, mu=pi, eps=self.eps)
-        T_api_sparse = apicall(scipy.sparse.csr_matrix(C), reversible=True, mu=pi, eps=self.eps).toarray()
+        T_impl_algo_dense_type_dense = impl_dense(C, pi)
+        T_impl_algo_sparse_type_sparse = impl_sparse(scipy.sparse.csr_matrix(C), pi).toarray()
+        T_Frank = impl_dense_Frank(C, pi)
+        T_api_algo_dense_type_dense = apicall(C, reversible=True, mu=pi, method='dense')
+        T_api_algo_sparse_type_dense = apicall(C, reversible=True, mu=pi, method='sparse')
+        T_api_algo_dense_type_sparse = apicall(scipy.sparse.csr_matrix(C), reversible=True, mu=pi, method='dense').toarray()
+        T_api_algo_sparse_type_sparse = apicall(scipy.sparse.csr_matrix(C), reversible=True, mu=pi, method='sparse').toarray()
+        T_api_algo_auto_type_dense = apicall(C, reversible=True, mu=pi, method='auto')
+        T_api_algo_auto_type_sparse = apicall(scipy.sparse.csr_matrix(C), reversible=True, mu=pi, method='auto').toarray()
 
-        assert_allclose(T_cython_dense, T_python)
-        assert_allclose(T_cython_sparse, T_python)
-        assert_allclose(T_api_sparse, T_python)
-        assert_allclose(T_api_dense, T_python)
+        assert_allclose(T_impl_algo_dense_type_dense, T_Frank)
+        assert_allclose(T_impl_algo_sparse_type_sparse, T_Frank)
+        assert_allclose(T_api_algo_dense_type_dense, T_Frank)
+        assert_allclose(T_api_algo_sparse_type_dense, T_Frank)
+        assert_allclose(T_api_algo_dense_type_sparse, T_Frank)
+        assert_allclose(T_api_algo_sparse_type_sparse, T_Frank)
+        assert_allclose(T_api_algo_auto_type_dense, T_Frank)
+        assert_allclose(T_api_algo_auto_type_sparse, T_Frank)
 
-        assert is_transition_matrix(T_cython_dense)
-        assert is_transition_matrix(T_cython_sparse)
-        assert is_transition_matrix(T_python)
-        assert_allclose(statdist(T_cython_dense), pi)
-        assert_allclose(statdist(T_cython_sparse), pi)
-        assert_allclose(statdist(T_python), pi)
+        assert is_transition_matrix(T_Frank)
+        assert is_transition_matrix(T_impl_algo_dense_type_dense)
+        assert is_transition_matrix(T_impl_algo_sparse_type_sparse)
+        assert is_transition_matrix(T_api_algo_dense_type_dense)
+        assert is_transition_matrix(T_api_algo_sparse_type_dense)
+        assert is_transition_matrix(T_api_algo_dense_type_sparse)
+        assert is_transition_matrix(T_api_algo_sparse_type_sparse)
+        assert is_transition_matrix(T_api_algo_auto_type_dense)
+        assert is_transition_matrix(T_api_algo_auto_type_sparse)
+
+        assert_allclose(statdist(T_Frank), pi)
+        assert_allclose(statdist(T_impl_algo_dense_type_dense), pi)
+        assert_allclose(statdist(T_impl_algo_sparse_type_sparse), pi)
+        assert_allclose(statdist(T_api_algo_dense_type_dense), pi)
+        assert_allclose(statdist(T_api_algo_sparse_type_dense), pi)
+        assert_allclose(statdist(T_api_algo_dense_type_sparse), pi)
+        assert_allclose(statdist(T_api_algo_sparse_type_sparse), pi)
+        assert_allclose(statdist(T_api_algo_auto_type_dense), pi)
+        assert_allclose(statdist(T_api_algo_auto_type_sparse), pi)
 
     def test_warnings(self):
         C = np.loadtxt(testpath + 'C_1_lag.dat')
@@ -75,13 +96,13 @@ class Test_mle_trev_given_pi(unittest.TestCase):
         
         with warnings.catch_warnings(record=True) as w:
             warnings.simplefilter("always")
-            mtrgps(scipy.sparse.csr_matrix(C), pi, eps=self.eps, maxiter=1)
+            impl_sparse(scipy.sparse.csr_matrix(C), pi, maxiter=1)
             assert len(w) == 1
             assert issubclass(w[-1].category, pyemma.util.exceptions.NotConvergedWarning)
             
         with warnings.catch_warnings(record=True) as w:
             warnings.simplefilter("always")
-            mtrgpd(C, pi, eps=self.eps, maxiter=1)
+            impl_dense(C, pi, maxiter=1)
             assert len(w) == 1
             assert issubclass(w[-1].category, pyemma.util.exceptions.NotConvergedWarning)        
 
