bb8061f3e5862e9831a54f3f61132a7063069637
marscher <m.scherer@fu-berlin.de>
2015-06-26 16:03:06 +0200

    [coor/test_featurereader] added test for two files.

--- pyemma/coordinates/tests/test_featurereader.py
+++ pyemma/coordinates/tests/test_featurereader.py
@@ -42,6 +42,20 @@ from pyemma.coordinates.api import feature_reader, discretizer, tica
 log = getLogger('TestFeatureReader')
 
 
+def create_traj(top):
+    trajfile = tempfile.mktemp('.dcd')
+    n_frames = np.random.randint(500, 1500)
+    log.debug("create traj with %i frames" % n_frames)
+    xyz = np.arange(n_frames * 3 * 3).reshape((n_frames, 3, 3))
+
+    t = mdtraj.load(top)
+    t.xyz = xyz
+    t.time = np.arange(n_frames)
+    t.save(trajfile)
+
+    return trajfile, xyz, n_frames
+
+
 class TestFeatureReader(unittest.TestCase):
 
     @classmethod
@@ -49,17 +63,11 @@ class TestFeatureReader(unittest.TestCase):
         c = super(TestFeatureReader, cls).setUpClass()
         # create a fake trajectory which has 3 atoms and coordinates are just a range
         # over all frames.
-        cls.trajfile = tempfile.mktemp('.xtc')
-        cls.n_frames = 1000
-        cls.xyz = np.random.random(
-            cls.n_frames * 3 * 3).reshape((cls.n_frames, 3, 3))
-        log.debug("shape traj: %s" % str(cls.xyz.shape))
         cls.topfile = pkg_resources.resource_filename(
             'pyemma.coordinates.tests.test_featurereader', 'data/test.pdb')
-        t = mdtraj.load(cls.topfile)
-        t.xyz = cls.xyz
-        t.time = np.arange(cls.n_frames)
-        t.save(cls.trajfile)
+        cls.trajfile, cls.xyz, cls.n_frames = create_traj(cls.topfile)
+        cls.trajfile2, cls.xyz2, cls.n_frames2 = create_traj(cls.topfile)
+
         return c
 
     @classmethod
@@ -75,30 +83,32 @@ class TestFeatureReader(unittest.TestCase):
         frames = 0
         data = []
         for i, X in reader:
+            assert isinstance(X, np.ndarray)
             frames += X.shape[0]
             data.append(X)
-
-        # restore shape of input
-        data = np.array(data).reshape(self.xyz.shape)
 
         self.assertEqual(frames, reader.trajectory_lengths()[0])
-        self.assertTrue(np.allclose(data, self.xyz))
+        data = np.vstack(data)
+        # restore shape of input
+        data.reshape(self.xyz.shape)
+
+        self.assertTrue(np.allclose(data, self.xyz.reshape(-1, 9)))
 
     def testIteratorAccess2(self):
-        reader = FeatureReader([self.trajfile, self.trajfile], self.topfile)
+        reader = FeatureReader([self.trajfile, self.trajfile2], self.topfile)
         reader.chunksize = 100
 
-        frames = 0
-        data = []
-        for i, X in reader:
-            frames += X.shape[0]
-            data.append(X)
-        self.assertEqual(frames, reader.trajectory_lengths()[0] * 2)
-        # restore shape of input
-        data = np.array(
-            data[0:reader.trajectory_lengths()[0] / reader.chunksize]).reshape(self.xyz.shape)
+        data = {itraj: [] for itraj in xrange(reader.number_of_trajectories())}
 
-        self.assertTrue(np.allclose(data, self.xyz))
+        for i, X in reader:
+            data[i].append(X)
+
+        # restore shape of input
+        data[0] = np.vstack(data[0]).reshape(-1, 9)
+        data[1] = np.vstack(data[1]).reshape(-1, 9)
+
+        np.testing.assert_equal(data[0], self.xyz.reshape(-1, 9))
+        np.testing.assert_equal(data[1], self.xyz2.reshape(-1, 9))
 
     def testTimeLaggedIterator(self):
         lag = 10
@@ -117,20 +127,18 @@ class TestFeatureReader(unittest.TestCase):
 
         # reproduce outcome
         xyz_s = self.xyz.shape
-        fake_lagged = np.empty((xyz_s[0] - lag, xyz_s[1] * xyz_s[2]))
         fake_lagged = self.xyz.reshape((xyz_s[0], -1))[lag:]
 
         self.assertTrue(np.allclose(merged_lagged, fake_lagged))
 
         # restore shape of input
-        data = np.array(data).reshape(self.xyz.shape)
+        data = np.vstack(data).reshape(self.xyz.shape)
 
         self.assertEqual(frames, reader.trajectory_lengths()[0])
         self.assertTrue(np.allclose(data, self.xyz))
 
     def test_with_pipeline_time_lagged(self):
         reader = feature_reader(self.trajfile, self.topfile)
-        #reader.featurizer.distances([[0, 1], [0, 2]])
         t = tica(dim=2, lag=1)
         d = discretizer(reader, t)
         d.parametrize()
@@ -140,9 +148,6 @@ class TestFeatureReader(unittest.TestCase):
         out1 = reader.get_output()
         # now map stuff to memory
         reader.in_memory = True
-
-        print len(reader._Y)
-        print reader._Y[0].shape
 
         reader2 = api.source(self.trajfile, top=self.topfile)
         out = reader2.get_output()
@@ -170,66 +175,36 @@ class TestFeatureReader(unittest.TestCase):
 
     def test_in_memory_switch_stride_dim(self):
         reader = api.source(self.trajfile, top=self.topfile)
+        reader.chunksize = 100
         reader.in_memory = True
 
         # now get output with different strides
-        strides = [1, 2, 3, 4, 5]
+        strides = [1, 2, 3, 4, 5, 10, 20]
         for s in strides:
             out = reader.get_output(stride=s)
             shape = (reader.trajectory_length(0, stride=s), reader.dimension())
-            self.assertEqual(out[0].shape, shape, "not equal for stride=%i" % s)
+            self.assertEqual(
+                out[0].shape, shape, "not equal for stride=%i" % s)
 
-    def testTimeLaggedAccess(self):
-        # each frame has 2 atoms with 3 coords = 6 coords per frame.
-        # coords are sequential through all frames and start with 0.
+    def test_lagged_stridden_access(self):
+        reader = api.source([self.trajfile, self.trajfile2], top=self.topfile)
+        reader.chunksize = 210
+        strides = [2, 3, 5, 7, 15]
+        lags = [1, 3, 7, 10, 30]
+        err_msg = "not equal for stride=%i, lag=%i"
+        for stride in strides:
+            for lag in lags:
+                chunks = {itraj: []
+                          for itraj in xrange(reader.number_of_trajectories())}
+                for itraj, _, Y in reader.iterator(stride, lag):
+                    chunks[itraj].append(Y)
+                chunks[0] = np.vstack(chunks[0])
+                np.testing.assert_almost_equal(
+                    chunks[0], self.xyz.reshape(-1, 9)[lag::stride], err_msg=err_msg % (stride, lag))
 
-        lags = [2, 200]
-
-        chunksizes = [1, 100]
-
-        for lag in lags:
-            for chunksize in chunksizes:
-                log.info("chunksize=%i\tlag=%i" % (chunksize, lag))
-
-                lagged_chunks = []
-                reader = api.source(self.trajfile, top=self.topfile)
-                reader.chunksize = chunksize
-                for _, _, y in reader.iterator(lag=lag):
-                    lagged_chunks.append(y)
-
-                coords = self.xyz.reshape((self.xyz.shape[0], -1))
-
-                for ii, c in enumerate(lagged_chunks[:-1]):
-                    # all despite last chunk shall have chunksize
-                    self.assertTrue(c.shape[0] <= chunksize)
-                    # first lagged chunk should start at lag and stop at chunksize +
-                    # lag
-                    ind1 = ii * chunksize + lag
-                    ind2 = ind1 + chunksize
-                    #log.debug("coor slice[%i: %i]" % (ind1, ind2))
-                    np.testing.assert_allclose(c, coords[ind1:ind2])
-
-                # TODO: check last lagged frame
-
-                # last lagged chunk should miss "lag" frames of input! e.g
-                # padded to maintain chunksize
-
-                last_chunk = lagged_chunks[-1]
-                # print last_chunk
-                # when is last_chunk padded?
-                # if
-                # how many zeros are going to be used?
-
-
-#                 expected = np.empty((chunksize, 2, 3))
-#                 for ii, c in enumerate(xrange(chunksize)):
-#                     c += 1
-#                     expected[ii] = coords[-c]
-# expected  = np.array((coords[-2], coords[-1]))
-#                 print last_chunk
-#                 print "-"*10
-#                 print expected
-                # np.testing.assert_allclose(last_chunk, expected)
+                chunks[1] = np.vstack(chunks[1])
+                np.testing.assert_almost_equal(
+                    chunks[1], self.xyz2.reshape(-1, 9)[lag::stride], err_msg=err_msg % (stride, lag))
 
 if __name__ == "__main__":
     unittest.main()
