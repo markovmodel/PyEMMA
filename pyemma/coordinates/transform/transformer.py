0d8b05e9ec010c4257aec23b8f364dbb7d832e96
Moritz Hoffmann <clonker@gmail.com>
2015-07-10 16:24:05 +0200

    First couple changes into the direction of supporting a random access variant of stride, accessible in param_add_data by a third element in the returned tuple (and a second element in the returned tuple of param_init)

--- pyemma/coordinates/transform/transformer.py
+++ pyemma/coordinates/transform/transformer.py
@@ -246,7 +246,7 @@ class Transformer(ProgressReporter):
 
         # if stride is not equal to one and does not match to a previous call
         # retrigger parametrization (but not for readers).
-        if stride != self._param_with_stride and not self._data_producer is self:
+        if stride != self._param_with_stride and self._data_producer is not self:
             self._parametrized = False
 
         self._param_with_stride = stride
@@ -257,7 +257,10 @@ class Transformer(ProgressReporter):
         # init
         return_value = self._param_init()
         if return_value is not None:
-            lag = return_value
+            if isinstance(return_value, tuple):
+                lag, stride = return_value
+            else:
+                lag = return_value
         else:
             lag = 0
         # feed data, until finished
@@ -276,6 +279,7 @@ class Transformer(ProgressReporter):
                 # iterate over trajectories
                 last_chunk = False
                 itraj = 0
+
                 while not last_chunk:
                     last_chunk_in_traj = False
                     t = 0
@@ -287,6 +291,7 @@ class Transformer(ProgressReporter):
                         else:
                             X, Y = self.data_producer._next_chunk(lag=lag, stride=stride)
                         L = np.shape(X)[0]
+
                         # last chunk in traj?
                         last_chunk_in_traj = (
                             t + L >= self.trajectory_length(itraj, stride=stride))
@@ -312,7 +317,10 @@ class Transformer(ProgressReporter):
                             self._progress_update(1, 0)
 
                         if isinstance(return_value, tuple):
-                            add_data_finished, lag = return_value
+                            if len(return_value) == 2:
+                                add_data_finished, lag = return_value
+                            else:
+                                add_data_finished, lag, stride = return_value
                         else:
                             add_data_finished = return_value
 
@@ -453,24 +461,47 @@ class Transformer(ProgressReporter):
             # operate in memory, implement iterator here
             traj_len = self.trajectory_length(self._itraj)
             if lag == 0:
-                Y = self._Y[self._itraj][
-                    self._t:min(self._t + self.chunksize*stride, traj_len):stride]
-                # increment counters
-                self._t += self.chunksize*stride
+                if isinstance(stride, dict):
+                    # TODO: respect chunksize (does this work?)
+                    while self._itraj not in stride.keys() and self._itraj < self.number_of_trajectories():
+                        self._itraj += 1
+                        self._t = 0
+                    Y = [self._Y[self._itraj][k] for k in stride[self._itraj][
+                                                          self._t:min(self._t + self.chunksize, traj_len)]
+                         ]
+                    self._t += self.chunksize
+                else:
+                    Y = self._Y[self._itraj][self._t:min(self._t + self.chunksize * stride, traj_len):stride]
+                    # increment counters
+                    self._t += self.chunksize * stride
                 if self._t >= traj_len:
                     self._itraj += 1
                     self._t = 0
                 return Y
             else:
-                Y0 = self._Y[self._itraj][
-                    self._t:min(self._t + self.chunksize*stride, traj_len):stride]
-                Ytau = self._Y[self._itraj][
-                    self._t + lag*stride:min(self._t + (self.chunksize + lag)*stride, traj_len):stride]
-                # increment counters
-                self._t += self.chunksize*stride
+                if isinstance(stride, dict):
+                    # TODO: respect chunksize (does this work?)
+                    while self._itraj not in stride.keys() and self._itraj < self.number_of_trajectories():
+                        self._itraj += 1
+                        self._t = 0
+                    Y0 = [self._Y[self._itraj][k] for k in stride[self._itraj][
+                                                           self._t:min(self._t + self.chunksize, traj_len)]
+                          ]
+                    Ytau = [
+                        self._Y[self._itraj][k] for k in stride[self._itraj][
+                                                         lag + self._t:min(self._t + self.chunksize, traj_len)]
+                        ]
+                    self._itraj += 1
+                else:
+                    Y0 = self._Y[self._itraj][self._t:min(self._t + self.chunksize * stride, traj_len):stride]
+                    Ytau = self._Y[self._itraj][
+                           self._t + lag * stride:min(self._t + (self.chunksize + lag) * stride, traj_len):stride]
+                    # increment counters
+                    self._t += self.chunksize * stride
                 if self._t >= traj_len:
                     self._itraj += 1
-                return (Y0, Ytau)
+                    self._t = 0
+                return Y0, Ytau
         else:
             # operate in pipeline
             if lag == 0:
@@ -487,7 +518,7 @@ class Transformer(ProgressReporter):
                 if self._t >= self.trajectory_length(self._itraj, stride=stride):
                     self._itraj += 1
                     self._t = 0
-                return (self.map(X0), self.map(Xtau))
+                return self.map(X0), self.map(Xtau)
 
     def __iter__(self):
         r"""
@@ -579,7 +610,7 @@ class Transformer(ProgressReporter):
 
         if isinstance(dimensions, int):
             ndim = 1
-            dimensions = slice(dimensions, dimensions+1)
+            dimensions = slice(dimensions, dimensions + 1)
         elif isinstance(dimensions, list):
             ndim = len(np.zeros(self.dimension())[dimensions])
         elif isinstance(dimensions, np.ndarray):
