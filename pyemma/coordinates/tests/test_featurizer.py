ba54f5c5e0883d43f707d0a74ced0eaec06b664a
gph82 <guille.perez@fu-berlin.de>
2015-07-20 17:22:55 +0200

    [featurizer.py] two new distance features (group and residue)

--- pyemma/coordinates/tests/test_featurizer.py
+++ pyemma/coordinates/tests/test_featurizer.py
@@ -327,6 +327,70 @@ class TestFeaturizer(unittest.TestCase):
         ref_Y = mdtraj.rmsd(self.traj, self.traj[self.ref_frame], atom_indices=self.atom_indices, precentered=True)
         verbose_assertion_minrmsd(ref_Y, test_Y, self)
 
+    def test_Residue_Mindist_Ca_all(self):
+        self.feat.add_residue_mindist(scheme='ca')
+        D = self.feat.map(self.traj)
+        Dref = mdtraj.compute_contacts(self.traj, scheme='ca')[0]
+        assert np.allclose(D, Dref)
+
+
+    def test_Residue_Mindist_Ca_array(self):
+        contacts=np.array([[20,10,], [10,0]])
+        self.feat.add_residue_mindist(scheme='ca', residue_pairs=contacts)
+        D = self.feat.map(self.traj)
+        Dref = mdtraj.compute_contacts(self.traj, scheme='ca', contacts=contacts)[0]
+        assert np.allclose(D, Dref)
+
+    def test_Group_Mindist_One_Group(self):
+        group0= [0,20,30,0]
+        self.feat.add_group_mindist(group_definitions=[group0]) # Even with duplicates
+        D = self.feat.map(self.traj)
+        dist_list = list(combinations(np.unique(group0),2))
+        Dref = mdtraj.compute_distances(self.traj, dist_list)
+        assert np.allclose(D.squeeze(), Dref.min(1))
+
+    def test_Group_Mindist_All_Three_Groups(self):
+        group0 = [0,20,30,0]
+        group1 = [1,21,31,1]
+        group2 = [2,22,32,2]
+        self.feat.add_group_mindist(group_definitions=[group0, group1, group2])
+        D = self.feat.map(self.traj)
+
+        # Now the references, computed separately for each combination of groups
+        dist_list_01 = np.array(list(product(np.unique(group0),np.unique(group1))))
+        dist_list_02 = np.array(list(product(np.unique(group0),np.unique(group2))))
+        dist_list_12 = np.array(list(product(np.unique(group1),np.unique(group2))))
+        Dref_01 = mdtraj.compute_distances(self.traj, dist_list_01).min(1)
+        Dref_02 = mdtraj.compute_distances(self.traj, dist_list_02).min(1)
+        Dref_12 = mdtraj.compute_distances(self.traj, dist_list_12).min(1)
+        Dref = np.vstack((Dref_01,Dref_02,Dref_12)).T
+
+        assert np.allclose(D.squeeze(), Dref)
+
+    def test_Group_Mindist_Some_Three_Groups(self):
+        group0 = [0,20,30,0]
+        group1 = [1,21,31,1]
+        group2 = [2,22,32,2]
+
+        group_pairs=np.array([[0,1],
+                              [2,2],
+                              [0,2]])
+
+        self.feat.add_group_mindist(group_definitions=[group0, group1, group2], group_pairs=group_pairs)
+        D = self.feat.map(self.traj)
+
+        # Now the references, computed separately for each combination of groups
+        dist_list_01 = np.array(list(product(np.unique(group0),np.unique(group1))))
+        dist_list_02 = np.array(list(product(np.unique(group0),np.unique(group2))))
+        dist_list_22 = np.array(list(combinations(np.unique(group2),2)))
+        Dref_01 = mdtraj.compute_distances(self.traj, dist_list_01).min(1)
+        Dref_02 = mdtraj.compute_distances(self.traj, dist_list_02).min(1)
+        Dref_22 = mdtraj.compute_distances(self.traj, dist_list_22).min(1)
+        Dref = np.vstack((Dref_01,Dref_22,Dref_02)).T
+
+        assert np.allclose(D.squeeze(), Dref)
+
+
 class TestFeaturizerNoDubs(unittest.TestCase):
 
     def testAddFeaturesWithDuplicates(self):
@@ -393,6 +457,14 @@ class TestFeaturizerNoDubs(unittest.TestCase):
         featurizer.add_minrmsd_to_ref(pdbfile)
         self.assertEquals(len(featurizer.active_features), 10)
 
+        featurizer.add_residue_mindist()
+        featurizer.add_residue_mindist()
+        self.assertEquals(len(featurizer.active_features), 11)
+
+        featurizer.add_group_mindist([[0,1],[0,2]])
+        featurizer.add_group_mindist([[0,1],[0,2]])
+        self.assertEquals(len(featurizer.active_features), 12)
+
     def test_labels(self):
         """ just checks for exceptions """
         featurizer = MDFeaturizer(pdbfile)
@@ -405,6 +477,8 @@ class TestFeaturizerNoDubs(unittest.TestCase):
         cs.dimension = lambda: 3
         featurizer.add_custom_feature(cs)
         featurizer.add_minrmsd_to_ref(pdbfile)
+        featurizer.add_residue_mindist()
+        featurizer.add_group_mindist([[0,1],[0,2]])
 
         featurizer.describe()
 
