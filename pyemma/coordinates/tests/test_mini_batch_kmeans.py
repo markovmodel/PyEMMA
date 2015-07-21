f8b552e787208188b1479909b25ca4c4be49e24e
Moritz Hoffmann <clonker@gmail.com>
2015-07-21 17:58:26 +0200

    [coordinates/kmeans] adjusted test cases

--- pyemma/coordinates/tests/test_mini_batch_kmeans.py
+++ pyemma/coordinates/tests/test_mini_batch_kmeans.py
@@ -6,34 +6,33 @@ from pyemma.coordinates.api import source
 
 
 class TestMiniBatchKmeans(TestCase):
-    def setUp(self):
-        self.k = 20
-        self.dim = 5
-        self.data = [np.random.random((600, self.dim)),
-                     np.random.random((100, self.dim)),
-                     np.random.random((2, self.dim))]
 
-    def test_cluster_centers(self):
-        # data in memory: complete trajectory mode
-        kmeans = cluster_mini_batch_kmeans(source(self.data, chunk_size=0), k=self.k, batch_size=0.5)
-        # data in memory: chunked access
-        #kmeans = cluster_mini_batch_kmeans(source(self.data, chunk_size=3000), k=self.k, batch_size=0.5)
-
+    def test_3gaussian_1d_singletraj(self):
+        # generate 1D data from three gaussians
+        X = [np.random.randn(200) - 2.0,
+             np.random.randn(300),
+             np.random.randn(400) + 2.0]
+        X = np.hstack(X)
+        kmeans = cluster_mini_batch_kmeans(X, batch_size=0.5, k=10)
         cc = kmeans.clustercenters
-        print cc
+        assert (np.any(cc < 1.0))
+        assert (np.any((cc > -1.0) * (cc < 1.0)))
+        assert (np.any(cc > -1.0))
 
-    def test_cluster_centers_in_memory(self):
-        pass
-        # kmeans = cluster_mini_batch_kmeans(self.data, k=self.k, batch_size=0.5)
-        # kmeans.in_memory = True
-        # cc = kmeans.clustercenters
-        # print cc
-        # for c in range(1, 8):
-        #     kmeans.chunksize=c
-        #     for x in kmeans.iterator(stride={1: [1,2,3,4,5,6], 2: [1]}):
-        #         print x
-        #     print "***"*15
-
+    def test_3gaussian_2d_multitraj(self):
+        # generate 1D data from three gaussians
+        X1 = np.zeros((200, 2))
+        X1[:, 0] = np.random.randn(200) - 2.0
+        X2 = np.zeros((300, 2))
+        X2[:, 0] = np.random.randn(300)
+        X3 = np.zeros((400, 2))
+        X3[:, 0] = np.random.randn(400) + 2.0
+        X = [X1, X2, X3]
+        kmeans = cluster_mini_batch_kmeans(X, batch_size=0.5, k=10)
+        cc = kmeans.clustercenters
+        assert (np.any(cc < 1.0))
+        assert (np.any((cc > -1.0) * (cc < 1.0)))
+        assert (np.any(cc > -1.0))
 
 if __name__ == '__main__':
     unittest.main()
