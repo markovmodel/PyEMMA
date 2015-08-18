f8bf83b0668338793e7ecdf9f6fa255ac6a605ec
marscher <m.scherer@fu-berlin.de>
2015-08-18 16:09:47 +0200

    try to move .coverage to home

--- tools/conda-recipe/run_test.py
+++ tools/conda-recipe/run_test.py
@@ -7,4 +7,6 @@ src_dir = os.getenv('SRC_DIR')
 nose_run = "nosetests pyemma -vv --with-coverage --cover-inclusive --cover-package=pyemma" \
            " --with-doctest --doctest-options=+NORMALIZE_WHITESPACE,+ELLIPSIS".split(' ')
 res = subprocess.call(nose_run, cwd=src_dir)
+# move .coverage file to home
+os.rename(os.path.join(src_dir, '.coverage'), os.path.expanduser('~'))
 sys.exit(res)
