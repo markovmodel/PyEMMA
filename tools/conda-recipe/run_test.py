360bccf0b50c4a19cf71faac2aead6b2f80dba2d
marscher <m.scherer@fu-berlin.de>
2015-08-18 16:33:14 +0200

    [conda-recipe] do not switch to src_dir

--- tools/conda-recipe/run_test.py
+++ tools/conda-recipe/run_test.py
@@ -6,7 +6,8 @@ src_dir = os.getenv('SRC_DIR')
 
 nose_run = "nosetests pyemma -vv --with-coverage --cover-inclusive --cover-package=pyemma" \
            " --with-doctest --doctest-options=+NORMALIZE_WHITESPACE,+ELLIPSIS".split(' ')
-res = subprocess.call(nose_run, cwd=src_dir)
+res = subprocess.call(nose_run)#, cwd=src_dir)
 # move .coverage file to home
-os.rename(os.path.join(src_dir, '.coverage'), os.path.expanduser('~'))
+fn = '.coverage'
+os.rename(fn, os.path.expanduser('~')+os.path.sep+fn)
 sys.exit(res)
