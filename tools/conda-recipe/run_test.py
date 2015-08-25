11b13703457a4e4c07a1ae09073d22416336dd2b
marscher <m.scherer@fu-berlin.de>
2015-08-25 16:13:11 +0200

    [conda-recipe|travis] move coverage report to $HOME/$git_clone

--- tools/conda-recipe/run_test.py
+++ tools/conda-recipe/run_test.py
@@ -1,14 +1,21 @@
 import subprocess
 import os
 import sys
+import shutil
+
 
 src_dir = os.getenv('SRC_DIR')
 
 nose_run = "nosetests pyemma -vv --with-coverage --cover-inclusive --cover-package=pyemma" \
            " --with-doctest --doctest-options=+NORMALIZE_WHITESPACE,+ELLIPSIS".split(' ')
-res = subprocess.call(nose_run)#, cwd=src_dir)
-# move .coverage file to clone
-# note: this is specific to travis-ci
-fn = '.coverage'
-os.rename(fn, os.path.join(os.path.expanduser('~'), 'markovmodel', 'PyEMMA', fn))
+res = subprocess.call(nose_run)
+# move .coverage file to git clone on Travis CI 
+if os.getenv('TRAVIS', False):
+    fn = '.coverage'
+    assert os.path.exists(fn)
+    dest = os.path.join(os.getenv('TRAVIS_BUILD_DIR'), fn)
+    print( "copying coverage report to", dest)
+    #os.rename(fn, dest)
+    shutil.copy(fn, dest)
+    assert os.path.exists(dest) 
 sys.exit(res)
