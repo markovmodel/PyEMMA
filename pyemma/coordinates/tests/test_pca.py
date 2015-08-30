[coordinates.api, coordinates.transform.pca]: Fixed PCA with given means. Covariance calculation with given means was broken because the number of frames was not computed in this case (thus dividing the covariance matrix by -1). Previous test was ineffective because API function did not pass the given means on to the PCA object

pyemma/coordinates/tests/test_pca.py
@@ -139,7 +139,7 @@ class TestPCAExtensive(unittest.TestCase):
             assert chunk.shape[1] == self.pca_obj.dimension()
 
     def test_map(self):
-        Y = self.pca_obj.map(self.X)
+        Y = self.pca_obj.transform(self.X)
         assert Y.shape[0] == self.T
         assert Y.shape[1] == 1
         # test if consistent with get_output
@@ -182,11 +182,13 @@ class TestPCAExtensive(unittest.TestCase):
     def test_provided_means(self):
         data = np.random.random((300, 3))
         mean = data.mean(axis=0)
-        tica_obj = pca(data, mean=mean)
+        pca_spec_mean = pca(data, mean=mean)
+        pca_calc_mean = pca(data)
 
-        tica_calc_mean = pca(data)
-        np.testing.assert_allclose(tica_obj.mean, tica_calc_mean.mean)
-        np.testing.assert_allclose(tica_obj.cov, tica_calc_mean.cov)
+        np.testing.assert_allclose(mean, pca_calc_mean.mean)
+        np.testing.assert_allclose(mean, pca_spec_mean.mean)
+
+        np.testing.assert_allclose(pca_spec_mean.cov, pca_calc_mean.cov)
 
 if __name__ == "__main__":
     unittest.main()

pyemma/coordinates/transform/pca.py
@@ -82,7 +82,8 @@ class PCA(Transformer):
             raise ValueError('Trying to set both the number of dimension and the subspace variance. Use either or.')
         self._dot_prod_tmp = None
         self.Y = None
-        self._N = 0
+        self._N_mean = 0
+        self._N_cov = 0
 
         self.mu = mean
 
@@ -124,7 +125,8 @@ class PCA(Transformer):
         return self.cov
 
     def _param_init(self):
-        self._N = 0
+        self._N_mean = 0
+        self._N_cov = 0
         # create mean array and covariance matrix
         indim = self.data_producer.dimension()
         self._logger.info("Running PCA on %i dimensional input" % indim)
@@ -173,17 +175,17 @@ class PCA(Transformer):
         if ipass == 0:
             if t == 0:
                 if self._given_mean:
-                    raise SkipPassException(stride=stride)
+                    raise SkipPassException(next_pass_stride=stride)
                 self._sum_tmp = np.empty(X.shape[1])
             np.sum(X, axis=0, out=self._sum_tmp)
             self.mu += self._sum_tmp
-            self._N += np.shape(X)[0]
+            self._N_mean += np.shape(X)[0]
 
             # counting chunks and log of eta
             self._progress_update(1, 0)
 
             if last_chunk:
-                self.mu /= self._N
+                self.mu /= self._N_mean
 
         # pass 2: covariances
         if ipass == 1:
@@ -193,11 +195,12 @@ class PCA(Transformer):
             Xm = X - self.mu
             np.dot(Xm.T, Xm, self._dot_prod_tmp)
             self.cov += self._dot_prod_tmp
+            self._N_cov += np.shape(X)[0]
 
             self._progress_update(1, stage=1)
 
             if last_chunk:
-                self.cov /= self._N - 1
+                self.cov /= self._N_cov - 1
                 return True  # finished!
 
         # by default, continue
@@ -214,7 +217,7 @@ class PCA(Transformer):
         self.cumvar = np.cumsum(self.eigenvalues)
         self.cumvar /= self.cumvar[-1]
 
-    def _map_array(self, X):
+    def _transform_array(self, X):
         r"""
         Projects the data onto the dominant principal components.
 