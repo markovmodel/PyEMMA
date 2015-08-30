b1b7fccf61d2362b9f4c5e0c664c4f3f0230db41
marscher <m.scherer@fu-berlin.de>
2015-08-30 23:12:46 +0200

    missing hdrs

--- setup.py
+++ setup.py
@@ -1,5 +1,21 @@
-
 #!/usr/bin/env python
+
+# This file is part of MSMTools.
+#
+# Copyright (c) 2015, 2014 Computational Molecular Biology Group, Freie Universitaet Berlin (GER)
+#
+# MSMTools is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Lesser General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 """EMMA: Emma's Markov Model Algorithms
 
@@ -252,14 +268,6 @@ else:
 
     # only require numpy and extensions in case of building/installing
     metadata['ext_modules'] = lazy_cythonize(extensions)
-
-    # add argparse to runtime deps if python version is 2.6
-    if sys.version_info[:2] == (2, 6):
-        metadata['install_requires'] += ['argparse']
-
-    # include ipython notebooks. Will be installed directly in site-packages
-    #metadata['packages'] += ['pyemma-ipython']
-    #metadata['include_package_data'] = True
 
 try:
     setup(**metadata)
