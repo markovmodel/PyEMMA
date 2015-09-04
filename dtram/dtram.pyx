83d44ef5de9597e854cd7f9555684d4429a6492f
Christoph Wehmeyer <christoph.wehmeyer@fu-berlin.de>
2015-09-04 23:04:11 +0200

    fixed docstring typo

--- dtram/dtram.pyx
+++ dtram/dtram.pyx
@@ -16,7 +16,7 @@
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 r"""
-Python interface to the WHAM estimator's lowlevel functions.
+Python interface to the dTRAM estimator's lowlevel functions.
 """
 
 import numpy as np
