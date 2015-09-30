bb8e1585af78b1af56b313b0b1cfde96d89387c0
Christoph Wehmeyer <christoph.wehmeyer@fu-berlin.de>
2015-09-30 14:14:26 +0200

    [UTIL] adding count_matrices function and unit tests

--- util/util.pyx
+++ util/util.pyx
@@ -21,8 +21,10 @@ Python interface to utility functions
 
 import numpy as _np
 cimport numpy as _np
+from scipy.sparse import csr_matrix as _csr
+from msmtools.estimation import count_matrix as _cm
 
-__all__ = []
+__all__ = ['count_matrices']
 
 cdef extern from "_util.h":
     int _get_therm_state_break_points(int *T_x, int seq_length, int *break_points)
@@ -49,4 +51,40 @@ def get_therm_state_break_points(
         <int*> _np.PyArray_DATA(T_B))
     return _np.ascontiguousarray(T_B[:nb])
 
+def count_matrices(dtraj, lag, sliding=True, sparse_return=True, nstates=None):
+    r"""
+    dtraj : list of numpy.ndarray(shape=(X, 2), dtype=np.intc)
+        list of discretized trajectories
+    lag : int
+        lagtime in trajectory steps
+    sliding : bool, optional
+        if true the sliding window approach is used for transition counting
+    sparse_return : bool (optional)
+        whether to return a 3D dense matrix or a list 2D sparse matrices
+    nstates : int, optional
+        enforce count-matrices with shape=(nstates, nstates) for all thermodynamic states
 
+    Returns
+    -------
+    C_K : [scipy.sparse.coo_matrix] or numpy.ndarray(shape=(T, M, M))
+        count matrices at given lag in coordinate list format
+    """
+    kmax = _np.max([d[:, 0].max() for d in dtraj])
+    nmax = _np.max([d[:, 1].max() for d in dtraj])
+    if nstates is None:
+        nstates = nmax + 1
+    elif nstates < nmax + 1:
+        raise ValueError("nstates is smaller than the number of observed microstates")
+    C_K = [_csr((nstates, nstates), dtype=_np.intc)] * (kmax + 1)
+    for d in dtraj:
+        bp = get_therm_state_break_points(_np.ascontiguousarray(d[:, 0]))
+        for b in range(1, bp.shape[0]):
+            C_K[d[bp[b - 1], 0]] += _cm(
+                _np.ascontiguousarray(d[bp[b - 1]:bp[b], 1]), lag,
+                sliding=sliding, sparse_return=True, nstates=nstates)
+        C_K[d[bp[-1], 0]] += _cm(
+            _np.ascontiguousarray(d[bp[-1]:, 1]), lag,
+            sliding=sliding, sparse_return=True, nstates=nstates)
+    if sparse_return:
+        return C_K
+    return _np.array([C.todense() for C in C_K], dtype=_np.intc)
