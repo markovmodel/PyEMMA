3594e541af887b83cae451b46a318ddd35e767e1
Christoph Wehmeyer <christoph.wehmeyer@fu-berlin.de>
2015-10-01 14:37:34 +0200

    adding ntherm key to util functions

--- util/util.pyx
+++ util/util.pyx
@@ -51,7 +51,7 @@ def get_therm_state_break_points(
         <int*> _np.PyArray_DATA(T_B))
     return _np.ascontiguousarray(T_B[:nb])
 
-def count_matrices(dtraj, lag, sliding=True, sparse_return=True, nstates=None):
+def count_matrices(dtraj, lag, sliding=True, sparse_return=True, ntherm=None, nstates=None):
     r"""
     Count transitions at given lagtime
 
@@ -65,6 +65,8 @@ def count_matrices(dtraj, lag, sliding=True, sparse_return=True, nstates=None):
         if true the sliding window approach is used for transition counting
     sparse_return : bool (optional)
         whether to return a 3D dense matrix or a list 2D sparse matrices
+    ntherm : int, optional
+        enforce ntherm count-matrices with shape=(nstates, nstates)
     nstates : int, optional
         enforce count-matrices with shape=(nstates, nstates) for all thermodynamic states
 
@@ -75,11 +77,15 @@ def count_matrices(dtraj, lag, sliding=True, sparse_return=True, nstates=None):
     """
     kmax = _np.max([d[:, 0].max() for d in dtraj])
     nmax = _np.max([d[:, 1].max() for d in dtraj])
+    if ntherm is None:
+        ntherm = kmax + 1
+    elif ntherm < kmax + 1:
+        raise ValueError("ntherm is smaller than the number of observed thermodynamic states")
     if nstates is None:
         nstates = nmax + 1
     elif nstates < nmax + 1:
         raise ValueError("nstates is smaller than the number of observed microstates")
-    C_K = [_csr((nstates, nstates), dtype=_np.intc)] * (kmax + 1)
+    C_K = [_csr((nstates, nstates), dtype=_np.intc)] * ntherm
     for d in dtraj:
         bp = get_therm_state_break_points(_np.ascontiguousarray(d[:, 0]))
         for b in range(1, bp.shape[0]):
@@ -93,7 +99,7 @@ def count_matrices(dtraj, lag, sliding=True, sparse_return=True, nstates=None):
         return C_K
     return _np.array([C.todense() for C in C_K], dtype=_np.intc)
 
-def state_counts(dtraj):
+def state_counts(dtraj, nstates=None, ntherm=None):
     r"""
     Count discrete states in all thermodynamic states
 
@@ -101,6 +107,10 @@ def state_counts(dtraj):
     ----------
     dtraj : list of numpy.ndarray(shape=(X, 2), dtype=np.intc)
         list of discretized trajectories
+    nstates : int, optional
+        enforce state count matrix with shape=(ntherm, nstates)
+    ntherm : int, optional
+        enforce state count matrix with shape=(ntherm, nstates)
 
     Returns
     -------
@@ -109,9 +119,17 @@ def state_counts(dtraj):
     """
     kmax = _np.max([d[:, 0].max() for d in dtraj])
     nmax = _np.max([d[:, 1].max() for d in dtraj])
-    N = _np.zeros(shape=(kmax + 1, nmax + 1), dtype=_np.intc)
+    if ntherm is None:
+        ntherm = kmax + 1
+    elif ntherm < kmax + 1:
+        raise ValueError("ntherm is smaller than the number of observed thermodynamic states")
+    if nstates is None:
+        nstates = nmax + 1
+    elif nstates < nmax + 1:
+        raise ValueError("nstates is smaller than the number of observed microstates")
+    N = _np.zeros(shape=(ntherm, nstates), dtype=_np.intc)
     for d in dtraj:
-        for K in range(kmax + 1):
-            for i in range(nmax + 1):
+        for K in range(ntherm):
+            for i in range(nstates):
                 N[K, i] += ((d[:, 0] == K) * (d[:, 1] == i)).sum()
     return N
