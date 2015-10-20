d346406504268f65d1e00017210708f440563059
Christoph Wehmeyer <christoph.wehmeyer@fu-berlin.de>
2015-10-20 14:39:02 +0200

    [MBAR] adding get_biased_conf_energies()

--- mbar/mbar.pyx
+++ mbar/mbar.pyx
@@ -22,7 +22,7 @@ Python interface to the MBAR estimator's lowlevel functions.
 import numpy as _np
 cimport numpy as _np
 
-__all__ = ['update_therm_energies', 'normalize', 'get_conf_energies', 'estimate']
+__all__ = ['update_therm_energies', 'normalize', 'get_conf_energies', 'get_biased_conf_energies', 'estimate']
 
 cdef extern from "_mbar.h":
     void _update_therm_energies(
@@ -37,6 +37,11 @@ cdef extern from "_mbar.h":
         double *bias_energy_sequence, int * conf_state_sequence,
         int n_therm_states, int n_conf_states, int seq_length,
         double *scratch_M, double *scratch_T, double *conf_energies)
+    void _get_biased_conf_energies(
+        double *log_therm_state_counts, double *therm_energies,
+        double *bias_energy_sequence, int *conf_state_sequence,
+        int n_therm_states, int n_conf_states, int seq_length,
+        double *scratch_M, double *scratch_T, double *biased_conf_energies)
 
 def update_therm_energies(
     _np.ndarray[double, ndim=1, mode="c"] log_therm_state_counts not None,
@@ -104,7 +109,7 @@ def get_conf_energies(
     _np.ndarray[int, ndim=1, mode="c"] conf_state_sequence not None,
     _np.ndarray[double, ndim=1, mode="c"] scratch_M not None,
     _np.ndarray[double, ndim=1, mode="c"] scratch_T not None,
-    n_discrete_states):
+    n_conf_states):
     r"""
     Calculate the reduced unbiased free energies conf_energies
         
@@ -122,7 +127,7 @@ def get_conf_energies(
         scratch array
     scratch_T : numpy.ndarray(shape=(T), dtype=numpy.float64)
         scratch array
-    n_discrete_states : int
+    n_conf_states : int
         number of discrete states (M)
 
     Returns
@@ -130,19 +135,66 @@ def get_conf_energies(
     conf_energies : numpy.ndarray(shape=(M), dtype=numpy.float64)
         reduced unbiased free energies
     """
-    conf_energies = _np.zeros(shape=(n_discrete_states,), dtype=_np.float64)
+    conf_energies = _np.zeros(shape=(n_conf_states,), dtype=_np.float64)
     _get_conf_energies(
         <double*> _np.PyArray_DATA(log_therm_state_counts),
         <double*> _np.PyArray_DATA(therm_energies),
         <double*> _np.PyArray_DATA(bias_energy_sequence),
         <int*> _np.PyArray_DATA(conf_state_sequence),
         bias_energy_sequence.shape[0],
-        n_discrete_states,
+        n_conf_states,
         bias_energy_sequence.shape[1],
         <double*> _np.PyArray_DATA(scratch_M),
         <double*> _np.PyArray_DATA(scratch_T),
         <double*> _np.PyArray_DATA(conf_energies))
     return conf_energies
+
+def get_biased_conf_energies(
+    _np.ndarray[double, ndim=1, mode="c"] log_therm_state_counts not None,
+    _np.ndarray[double, ndim=1, mode="c"] therm_energies not None,
+    _np.ndarray[double, ndim=2, mode="c"] bias_energy_sequence not None,
+    _np.ndarray[int, ndim=1, mode="c"] conf_state_sequence not None,
+    _np.ndarray[double, ndim=1, mode="c"] scratch_M not None,
+    _np.ndarray[double, ndim=1, mode="c"] scratch_T not None,
+    n_conf_states):
+    r"""
+    Calculate the reduced biased free energies biased_conf_energies
+        
+    Parameters
+    ----------
+    log_therm_state_counts : numpy.ndarray(shape=(T), dtype=numpy.float64)
+        log of the state counts in each of the T thermodynamic states
+    therm_energies : numpy.ndarray(shape=(T), dtype=numpy.float64)
+        reduced free energies of the T thermodynamic states
+    bias_energy_sequence : numpy.ndarray(shape=(T, X), dtype=numpy.float64)
+        bias energies in the T thermodynamic states for all X samples
+    conf_state_sequence : numpy.ndarray(shape=(X), dtype=numpy.intc)
+        discrete states indices for all X samples
+    scratch_M : numpy.ndarray(shape=(M), dtype=numpy.float64)
+        scratch array
+    scratch_T : numpy.ndarray(shape=(T), dtype=numpy.float64)
+        scratch array
+    n_conf_states : int
+        number of discrete states (M)
+
+    Returns
+    -------
+    biased_conf_energies : numpy.ndarray(shape=(T, M), dtype=numpy.float64)
+        reduced biased free energies
+    """
+    biased_conf_energies = _np.zeros(shape=(bias_energy_sequence.shape[0], n_conf_states), dtype=_np.float64)
+    _get_biased_conf_energies(
+        <double*> _np.PyArray_DATA(log_therm_state_counts),
+        <double*> _np.PyArray_DATA(therm_energies),
+        <double*> _np.PyArray_DATA(bias_energy_sequence),
+        <int*> _np.PyArray_DATA(conf_state_sequence),
+        bias_energy_sequence.shape[0],
+        n_conf_states,
+        bias_energy_sequence.shape[1],
+        <double*> _np.PyArray_DATA(scratch_M),
+        <double*> _np.PyArray_DATA(scratch_T),
+        <double*> _np.PyArray_DATA(biased_conf_energies))
+    return biased_conf_energies
 
 def estimate(therm_state_counts, bias_energy_sequence, maxiter=1000, maxerr=1.0E-8, therm_energies=None):
     r"""
