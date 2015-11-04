a656f0fa9b39edc778fb26431f296783c9458a7e
Moritz Hoffmann <clonker@gmail.com>
2015-11-04 17:55:32 +0100

    [coordinates/data/fragment_reader] added support for lagged data and multiple trajs

--- pyemma/coordinates/tests/test_fragmented_trajectory.py
+++ pyemma/coordinates/tests/test_fragmented_trajectory.py
@@ -14,11 +14,15 @@
 #
 # You should have received a copy of the GNU Lesser General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
-
+import os
 import unittest
-import numpy as np
 
+import pkg_resources
+import mdtraj
+import numpy as np
+import pyemma.coordinates as coor
 from pyemma.coordinates.data.fragmented_trajectory_reader import FragmentedTrajectoryReader
+from six.moves import range
 
 
 class TestFragmentedTrajectory(unittest.TestCase):
@@ -36,6 +40,7 @@ class TestFragmentedTrajectory(unittest.TestCase):
 
     def test_full_trajectory_stridden(self):
         for stride in [1, 3, 5, 7, 13, 20]:
+            print "Stride=%s" % stride
             reader = FragmentedTrajectoryReader([self.d, self.d])
             reader.chunksize = 0
             expected = np.vstack((self.d, self.d))[::stride]
@@ -43,10 +48,10 @@ class TestFragmentedTrajectory(unittest.TestCase):
             np.testing.assert_array_almost_equal(expected, out)
 
     def test_full_trajectory_stridden_with_lag(self):
+        reader = FragmentedTrajectoryReader([self.d, self.d])
         data = np.vstack((self.d, self.d))
         for lag in [1, 5, 7]:
             for stride in [1, 3, 5, 7, 13, 20]:
-                reader = FragmentedTrajectoryReader([self.d, self.d])
                 reader.chunksize = 0
 
                 X, Y = None, None
@@ -57,45 +62,78 @@ class TestFragmentedTrajectory(unittest.TestCase):
                 np.testing.assert_array_almost_equal(data[::stride], X)
                 np.testing.assert_array_almost_equal(data[lag::stride], Y)
 
-    def test_chunked_trajectory(self):
+    def test_fragmented_xtc(self):
+        from pyemma.coordinates.tests.test_featurereader import create_traj
+
+        top_file = pkg_resources.resource_filename(__name__, 'data/test.pdb')
+        trajfiles = []
+        for _ in range(3):
+            f, _, _ = create_traj(top_file)
+            trajfiles.append(f)
+        try:
+            # three trajectories: one consisting of all three, one consisting of the first,
+            # one consisting of the first and the last
+            source = coor.source([trajfiles, [trajfiles[0]], [trajfiles[0], trajfiles[2]]], top=top_file)
+            source.chunksize = 1000
+
+            out = source.get_output(stride=1)
+            trajs = [mdtraj.load(trajfiles[i], top=top_file).xyz.reshape(-1,9) for i in range(0,3)]
+
+            np.testing.assert_equal(out[0], np.vstack(trajs))
+            np.testing.assert_equal(out[1], trajs[0])
+            np.testing.assert_equal(out[2], np.vstack((trajs[0], trajs[2])))
+        finally:
+            for t in trajfiles:
+                try:
+                    os.unlink(t)
+                except EnvironmentError:
+                    pass
+
+    def test_multiple_input_trajectories(self):
+        reader = FragmentedTrajectoryReader([[self.d, self.d], self.d, [self.d, self.d]])
+        reader.chunksize = 37
+        out = reader.get_output()
+        reader.chunksize = 0
+        out2 = reader.get_output()
+        expected0_2 = np.vstack((self.d, self.d))
+        for itraj in range(0, 3):
+            np.testing.assert_array_almost_equal(out[itraj], out2[itraj])
+        np.testing.assert_array_almost_equal(out[0], expected0_2)
+        np.testing.assert_array_almost_equal(out[1], self.d)
+        np.testing.assert_array_almost_equal(out[2], expected0_2)
+
+    def test_chunked_trajectory_with_lag(self):
         data = np.vstack((self.d, self.d))
-        lag = 1
-        stride = 1
-        chunksize = 1
-        # for lag in [0, 1, 3]:
-        #     for stride in [1, 3, 5]:
-        #         for chunksize in [1, 17]:
-        #             print "lag=%s, stride=%s, cs=%s" % (lag, stride, chunksize)
         reader = FragmentedTrajectoryReader([self.d, self.d])
-        reader.chunksize = chunksize
+        for lag in [0, 1, 3]:
+            for stride in [1, 3, 5]:
+                for chunksize in [1, 34, 53, 72]:
+                    reader.chunksize = chunksize
 
-        if lag > 0:
-            collected = None
-            collected_lagged = None
-            for itraj, X, Y in reader.iterator(stride=stride, lag=lag):
-                collected = X if collected is None else np.vstack((collected, X))
-                collected_lagged = Y if collected_lagged is None else np.vstack((collected_lagged, X))
-            np.testing.assert_array_almost_equal(data[::stride], collected)
-            np.testing.assert_array_almost_equal(data[lag::stride], collected)
-        else:
-            collected = None
-            for itraj, X in reader.iterator(stride=stride):
-                collected = X if collected is None else np.vstack((collected, X))
-            np.testing.assert_array_almost_equal(data[::stride], collected)
-
-
-
+                    if lag > 0:
+                        collected = None
+                        collected_lagged = None
+                        for itraj, X, Y in reader.iterator(stride=stride, lag=lag):
+                            collected = X if collected is None else np.vstack((collected, X))
+                            collected_lagged = Y if collected_lagged is None else np.vstack((collected_lagged, Y))
+                        np.testing.assert_array_almost_equal(data[::stride], collected)
+                        np.testing.assert_array_almost_equal(data[lag::stride], collected_lagged)
+                    else:
+                        collected = None
+                        for itraj, X in reader.iterator(stride=stride):
+                            collected = X if collected is None else np.vstack((collected, X))
+                        np.testing.assert_array_almost_equal(data[::stride], collected)
 
     def test_index_to_reader_index(self):
         reader = FragmentedTrajectoryReader([self.d, self.d])
-        assert (0, 0) == reader._index_to_reader_index(0), "first frame is first frame of first reader"
-        assert (0, 1) == reader._index_to_reader_index(1), "second frame is second frame of first reader"
-        assert (1, 0) == reader._index_to_reader_index(100), "101'st frame is first frame of second reader"
-        assert (1, 1) == reader._index_to_reader_index(101), "102'nd frame is second frame of second reader"
+        assert (0, 0) == reader._index_to_reader_index(0, 0), "first frame is first frame of first reader"
+        assert (0, 1) == reader._index_to_reader_index(1, 0), "second frame is second frame of first reader"
+        assert (1, 0) == reader._index_to_reader_index(100, 0), "101'st frame is first frame of second reader"
+        assert (1, 1) == reader._index_to_reader_index(101, 0), "102'nd frame is second frame of second reader"
         with self.assertRaises(ValueError):
-            reader._index_to_reader_index(-1)
+            reader._index_to_reader_index(-1, 0)
         with self.assertRaises(ValueError):
-            reader._index_to_reader_index(200)
+            reader._index_to_reader_index(200, 0)
 
     def test_simple(self):
         pass
