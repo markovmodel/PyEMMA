cc5e35e2b3a3e6584a215ac4ed39273b7e29f0aa
Martin Scherer <m.scherer@fu-berlin.de>
2015-11-12 18:10:22 +0100

    [coverage-report] fix coveralls.io integration
    Fix implies editing source file paths in .coverage file, which
    is not meant to be edited. Pinned version of coverage to ==4 to
    avoid breaking this again.
    
    Fixes #609

--- tools/conda-recipe/run_test.py
+++ tools/conda-recipe/run_test.py
@@ -1,24 +1,48 @@
+
 import subprocess
 import os
 import sys
 import shutil
+import re
 
 src_dir = os.getenv('SRC_DIR')
+
 
 # matplotlib headless backend
 with open('matplotlibrc', 'w') as fh:
     fh.write('backend: Agg')
 
-nose_run = "nosetests pyemma -vv --with-coverage --cover-inclusive --cover-package=pyemma" \
-           " --with-doctest --doctest-options=+NORMALIZE_WHITESPACE,+ELLIPSIS".split(' ')
-res = subprocess.call(nose_run)
 
-# move .coverage file to git clone on Travis CI
-if os.getenv('TRAVIS', False):
+def coverage_report():
     fn = '.coverage'
     assert os.path.exists(fn)
-    dest = os.path.join(os.getenv('TRAVIS_BUILD_DIR'), fn)
+    build_dir = os.getenv('TRAVIS_BUILD_DIR')
+    dest = os.path.join(build_dir, fn)
     print( "copying coverage report to", dest)
     shutil.copy(fn, dest)
     assert os.path.exists(dest)
+
+    # fix paths in .coverage file
+    with open(dest, 'r') as fh:
+        data = fh.read()
+    match= '"/home/travis/miniconda/envs/_test/lib/python.+?/site-packages/.+?/(pyemma/.+?)"'
+    repl = '"%s/\\1"' % build_dir
+    data = re.sub(match, repl, data)
+    os.unlink(dest)
+    with open(dest, 'w+') as fh:
+       fh.write(data)
+
+nose_run = "nosetests pyemma -vv" \
+           " --with-coverage --cover-inclusive --cover-package=pyemma" \
+           " --with-doctest --doctest-options=+NORMALIZE_WHITESPACE,+ELLIPSIS" \
+           .split(' ')
+
+res = subprocess.call(nose_run)
+
+
+# move .coverage file to git clone on Travis CI
+if os.getenv('TRAVIS', False):
+   coverage_report()
+
 sys.exit(res)
+
