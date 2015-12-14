435020d528958ac78fffaf5fce7e069133ca51e0
marscher <m.scherer@fu-berlin.de>
2015-12-14 15:58:33 +0100

    made assign_to_centers work

--- pyemma/coordinates/clustering/assign.py
+++ pyemma/coordinates/clustering/assign.py
@@ -16,16 +16,18 @@
 # You should have received a copy of the GNU Lesser General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-from __future__ import absolute_import
-from pyemma.util import types
-import six
-
 '''
 Created on 18.02.2015
 
 @author: marscher
 '''
+
+from __future__ import absolute_import
+
 from pyemma.coordinates.clustering.interface import AbstractClustering
+from pyemma.util import types
+import six
+
 import numpy as np
 
 
@@ -56,7 +58,7 @@ class AssignCenters(AbstractClustering):
 
     """
 
-    def __init__(self, clustercenters, metric='euclidean'):
+    def __init__(self, clustercenters, metric='euclidean', stride=1):
         super(AssignCenters, self).__init__(metric=metric)
 
         if isinstance(clustercenters, six.string_types):
@@ -70,8 +72,10 @@ class AssignCenters(AbstractClustering):
         if not self.clustercenters.ndim == 2:
             raise ValueError('cluster centers have to be 2d')
 
+        self.set_params(clustercenters=clustercenters, metric=metric, stride=stride)
+
         # since we provided centers, this transformer is already parametrized.
-        self._parametrized = True
+        self._estimated = True
 
     def describe(self):
         return "[AssignCenters c=%s]" % self.clustercenters
@@ -85,15 +89,20 @@ class AssignCenters(AbstractClustering):
                              ', but input has %i' % (dim, dp.dimension()))
         AbstractClustering.data_producer.fset(self, dp)
 
-    def _param_add_data(self, X, itraj, t, first_chunk, last_chunk_in_traj,
-                        last_chunk, ipass, Y=None, stride=1):
-        # discretize all
-        if t == 0:
-            n = self.data_producer.trajectory_length(itraj, stride=stride)
-            self._dtrajs.append(np.empty(n, dtype=self.output_type()))
+    def _estimate(self, iterable, **kw):
+        # assign all data from iterable to the given centers
 
-        L = np.shape(X)[0]
-        self._dtrajs[itraj][t:t+L] = self._transform_array(X).squeeze()
+        iterator = iterable.iterator(return_trajindex=True, stride=self.stride, **kw)
 
-        if last_chunk:
-            return True
+        last_itraj = -1
+        for itraj, X in iterator:
+            # new trajectory?
+            if itraj != last_itraj:
+                last_itraj = itraj
+                t = 0
+                n = self.data_producer.trajectory_length(itraj, stride=self.stride)
+
+                self._dtrajs.append(np.empty(n, dtype=self.output_type()))
+            # assign
+            L = np.shape(X)[0]
+            self._dtrajs[itraj][t:t+L] = self._transform_array(X).squeeze()
