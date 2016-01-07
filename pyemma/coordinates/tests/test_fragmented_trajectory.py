0077e72768474944983a67622e5450c7d75df455
Moritz Hoffmann <clonker@gmail.com>
2016-01-07 12:29:56 +0100

    fix fragmented trajectory reader

--- pyemma/coordinates/tests/test_fragmented_trajectory.py
+++ pyemma/coordinates/tests/test_fragmented_trajectory.py
@@ -44,7 +44,7 @@ class TestFragmentedTrajectory(unittest.TestCase):
             reader.chunksize = 0
             expected = np.vstack((self.d, self.d))[::stride]
             out = reader.get_output(stride=stride)[0]
-            np.testing.assert_array_almost_equal(expected, out)
+            np.testing.assert_array_almost_equal(expected, out, err_msg="Failed for stride=%s" % stride)
 
     def test_full_trajectory_stridden_with_lag(self):
         reader = FragmentedTrajectoryReader([self.d, self.d])
@@ -58,7 +58,7 @@ class TestFragmentedTrajectory(unittest.TestCase):
                 for itraj, X, Y in reader.iterator(stride=stride, lag=lag):
                     pass
 
-                np.testing.assert_array_almost_equal(data[::stride], X)
+                np.testing.assert_array_almost_equal(data[::stride][0:len(Y)], X)
                 np.testing.assert_array_almost_equal(data[lag::stride], Y)
 
     def test_fragmented_xtc(self):
@@ -115,7 +115,7 @@ class TestFragmentedTrajectory(unittest.TestCase):
                         for itraj, X, Y in reader.iterator(stride=stride, lag=lag):
                             collected = X if collected is None else np.vstack((collected, X))
                             collected_lagged = Y if collected_lagged is None else np.vstack((collected_lagged, Y))
-                        np.testing.assert_array_almost_equal(data[::stride], collected)
+                        np.testing.assert_array_almost_equal(data[::stride][0:len(collected_lagged)], collected)
                         np.testing.assert_array_almost_equal(data[lag::stride], collected_lagged)
                     else:
                         collected = None
