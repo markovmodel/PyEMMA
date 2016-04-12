6bf7e2cff75949f40bbeeda672b5b1dc9c1a74ed
marscher <m.scherer@fu-berlin.de>
2016-04-12 15:08:08 +0200

    [ompthreads] respect OMP_NUM_THREADS env variable

--- pyemma/util/omp_threads.pyx
+++ pyemma/util/omp_threads.pyx
@@ -1,5 +1,6 @@
 from __future__ import print_function
 
+import os
 import omp_threads as omp
 cimport omp_threads as omp
 
@@ -7,15 +8,16 @@ def get_num_threads():
     return int(omp.omp_get_num_threads())
 
 class num_threads(object):
-    """
-    Set the thread context:
+    """ Within the context set the number of threads OpenMP will use.
 
-    >>> print("Before ", get_num_threads()) # doctest: +ELLIPSIS
+    If environment variable 'OMP_NUM_THREADS' is set, the context does nothing.
+
+    >>> print("Before", get_num_threads()) # doctest: +ELLIPSIS
     Before ...
     >>> with num_threads(2):
-    ...     print( "In thread context: ", get_num_threads())
+    ...     print( "In thread context:", get_num_threads())
     In thread context: 2
-    >>> print("After ", get_num_threads()) # doctest: +ELLIPSIS
+    >>> print("After", get_num_threads()) # doctest: +ELLIPSIS
     After ...
     """
 
@@ -24,10 +26,16 @@ class num_threads(object):
         self.num_threads = num_threads
 
     def __enter__(self):
-        cdef int i = self.num_threads
-        omp.omp_set_num_threads(i)
+        try:
+            os.environ['OMP_NUM_THREADS']
+            self._have_env_setting = True
+        except KeyError:
+            self._have_env_setting = False
+            cdef int i = self.num_threads
+            omp.omp_set_num_threads(i)
 
     def __exit__(self, *args):
-        cdef int i = self._old_num_threads
-        omp.omp_set_num_threads(i)
+        if self._have_env_setting:
+            cdef int i = self._old_num_threads
+            omp.omp_set_num_threads(i)
 
