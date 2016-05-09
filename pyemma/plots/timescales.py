053d4ffd9c969c52c093293f696644fb3192356d
Christoph Wehmeyer <christoph.wehmeyer@fu-berlin.de>
2016-05-09 18:28:27 +0200

    [plots] adding sorting to plot_implied_timescales

--- pyemma/plots/timescales.py
+++ pyemma/plots/timescales.py
@@ -77,23 +77,77 @@ def plot_implied_timescales(ITS, ax=None, outfile=None, show_mle=True, show_mean
     ax : Axes object containing the plot
 
     """
-    import matplotlib.pylab as _plt
+def plot_implied_timescales(ITS, ax=None, outfile=None, show_mle=True, show_mean=True,
+                            xlog=False, ylog=True, confidence=0.95, refs=None, nits=-1,
+                            process=None, units='steps', dt=1., **kwargs):
+    r"""Implied timescale plot
 
+    Parameters
+    ----------
+    ITS : implied timescales object.
+        Object whose data will be plotted. Must provide the functions: get_timescales() and get_timescales(i) where i is the
+        the property samples_available
+    ax : matplotlib Axes object, optional, default = None
+        the axes to plot to. When set to None the default Axes object will be used.
+    outfile : str, optional, default = None
+        output file to write the figure to. When not given, the plot will be displayed
+    show_mean : bool, default = True
+        Line for mean value will be shown, if available
+    show_mle : bool, default = True
+        Line for maximum likelihood estimate will be shown
+    xlog : bool, optional, default = False
+        Iff true, the x-Axis is logarithmized
+    ylog : bool, optional, default = True
+        Iff true, the y-Axis is logarithmized
+    confidence : float, optional, default = 0.95
+        The confidence interval for plotting error bars (if available)
+    refs : ndarray((m), dtype=float), optional, default = None
+        Reference (exact solution or other reference) timescales if known. The number of timescales must match those
+        in the ITS object
+    nits: integer, default = -1
+        Number of implied timescales to be shown. The default behaviour (-1) is to show all timescales available.
+        :py:obj:`nits` != -1 and :py:obj:`process` != None are mutually exclusive
+    process : iterable of integers, default is None
+        list or ndarray((m), dtype=int) containing a list of the processes to be shown. The default behaviour is
+        to show all timescales available.
+        :py:obj:`process` != None and :py:obj:`nits` != -1 are mutually exclusive
+    units: str or list (len=2) of strings, optional, default = 'steps'
+        Affects the labeling of the axes. Used with :py:obj:`dt`, allows for changing the physical units of the axes.
+        Accepts simple LaTeX math strings, eg. '$\mu$s'
+        If this parameter is a list, it will be assumed that units[0] is for the x-axis and units[1] for the y-axis.
+    dt: float or list(len=2) of floats, optional, default = 1.0
+        Physical time between frames, expressed the units given in :py:obj:`units`. E.g, if you know that each
+        frame corresponds to .010 ns, you can use the combination of parameters :py:obj:`dt` =0.01,
+        :py:obj:`units` ='ns' to display the implied timescales in ns (instead of frames)
+        If this parameter is a list, it will be assumed that dt[0] is for the x-axis and dt[1] for the y-axis.
+
+    **kwargs: Will be parsed to pyplot.plo when plotting the MLE datapoints (not the bootstrapped means).
+            See the doc of pyplot for more options. Most useful lineproperties like `marker='o'` and/or :markersize=5
+
+    Returns
+    -------
+    ax : Axes object containing the plot
+
+    """
+    import matplotlib.pyplot as _plt
     # check input
-    if (ax is None):
+    if ax is None:
         ax = _plt.gca()
-    colors = ['blue','red','green','cyan','purple','orange','violet']
+    colors = ['blue', 'red', 'green', 'cyan', 'purple', 'orange', 'violet']
     lags = ITS.lagtimes
     xmax = _np.max(lags)
-
+    srt = _np.argsort(lags)
     # Check the processes to be shown
     if process is not None:
         if nits != -1:
-            raise TypeError('optional arguments nits and process are mutually exclusive:', nits, process)
+            raise TypeError(
+                'optional arguments nits and process are mutually exclusive:', nits, process)
         if not _is_iterable_of_int(process):
             raise ValueError('process has to be an iterable of integers')
         if _np.max(process)+1 > ITS.number_of_timescales:
-            raise ValueError('requested process %u, whereas ITS only contains %u timescales'%(_np.max(process), ITS.number_of_timescales))
+            raise ValueError(
+                'requested process %u, whereas ITS only contains %u timescales' % (
+                    _np.max(process), ITS.number_of_timescales))
         # Now that it's for sure that nits==-1, process is iter_of_ints, and the requested processes exist in its object:
         its_idx = process
     else:
@@ -102,54 +156,56 @@ def plot_implied_timescales(ITS, ax=None, outfile=None, show_mle=True, show_mean
         if nits == -1:
             nits = ITS.number_of_timescales
         its_idx = _np.arange(ITS.number_of_timescales)[:nits]
-
     # Check units and dt for user error.
-    if isinstance(units,list) and len(units)!=2:
+    if isinstance(units, list) and len(units) != 2:
         raise TypeError("If units is a list, len(units) has to be = 2")
-    if isinstance(dt,list) and len(dt)!=2:
+    if isinstance(dt, list) and len(dt) != 2:
         raise TypeError("If dt is a list, len(dt) has to be = 2")
-
     # Create list of units and dts for different axis
-    if isinstance(units,str):
-        units = [units]*2
-    if isinstance(dt,(float, int)):
-        dt = [dt]*2
-
+    if isinstance(units, str):
+        units = [units] * 2
+    if isinstance(dt, (float, int)):
+        dt = [dt] * 2
     #ymin = min(_np.min(lags), _np.min(ITS.get_timescales()))
     #ymax = 1.5*_np.min(ITS.get_timescales())
     for i in its_idx:
         # plot estimate
         if show_mle:
-            ax.plot(lags*dt[0], ITS.get_timescales(process=i)*dt[1], color=colors[i % len(colors)], **kwargs)
+            ax.plot(
+                lags[srt] * dt[0], ITS.get_timescales(process=i)[srt] * dt[1],
+                color=colors[i % len(colors)], **kwargs)
         # sample available?
-        if (ITS.samples_available):# and ITS.sample_number_of_timescales > i):
+        if ITS.samples_available:# and ITS.sample_number_of_timescales > i):
             # plot sample mean
             if show_mean:
-                ax.plot(lags*dt[0], ITS.get_sample_mean(process=i)*dt[1], marker='o',
-                        color=colors[i % len(colors)], linestyle='dashed')
+                ax.plot(
+                    lags[srt] * dt[0], ITS.get_sample_mean(process=i)[srt] * dt[1], marker='o',
+                    color=colors[i % len(colors)], linestyle='dashed')
             (lconf, rconf) = ITS.get_sample_conf(confidence, i)
-            ax.fill_between(lags*dt[0], lconf*dt[1], rconf*dt[1], alpha=0.2, color=colors[i % len(colors)])
+            ax.fill_between(
+                lags[srt] * dt[0], lconf[srt] * dt[1], rconf[srt] * dt[1],
+                alpha=0.2, color=colors[i % len(colors)])
         # reference available?
-        if (refs is not None):
-            tref = refs[i]*dt[1]
-            ax.plot([0,min(tref,xmax)*dt[0]], [tref,tref], color='black', linewidth=1)
+        if refs is not None:
+            tref = refs[i] * dt[1]
+            ax.plot([0, min(tref, xmax) * dt[0]], [tref, tref], color='black', linewidth=1)
     # cutoff
-    ax.plot(lags*dt[0], lags*dt[1], linewidth=2, color='black')
-    ax.set_xlim([1*dt[0], xmax*dt[0]])
+    ax.plot(lags[srt] * dt[0], lags[srt] * dt[1], linewidth=2, color='black')
+    ax.set_xlim([1.0 * dt[0], xmax * dt[0]])
     #ax.set_ylim([ymin,ymax])
-    ax.fill_between(lags*dt[0], ax.get_ylim()[0]*_np.ones(len(lags))*dt[1], lags*dt[1], alpha=0.5, color='grey')
+    ax.fill_between(
+        lags[srt] * dt[0], ax.get_ylim()[0]*_np.ones(len(lags))*dt[1], lags[srt] * dt[1],
+        alpha=0.5, color='grey')
     # formatting
-    ax.set_xlabel('lag time / %s'%units[0])
-    ax.set_ylabel('timescale / %s'%units[1])
+    ax.set_xlabel('lag time / %s' % units[0])
+    ax.set_ylabel('timescale / %s' % units[1])
     if (xlog):
         ax.set_xscale('log')
     if (ylog):
         ax.set_yscale('log')
-
     # show or save
     # if outfile is None:
     #    _plt.show()
     if outfile is not None:
         _plt.savefig(outfile)
-
     return ax
