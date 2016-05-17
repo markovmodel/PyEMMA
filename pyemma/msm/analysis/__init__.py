0629fede2dd919175b611ff212905c6e1d5e7e49
marscher <m.scherer@fu-berlin.de>
2016-05-17 22:12:12 +0200

    fix imp

--- pyemma/msm/analysis/__init__.py
+++ pyemma/msm/analysis/__init__.py
@@ -1,10 +1,11 @@
 import sys
 import warnings
 
-from pyemma.util._ext.shimmodule import ShimModule, ShimWarning
+from pyemma.util._ext.shimmodule import ShimModule
+from pyemma.util.exceptions import PyEMMA_DeprecationWarning
 
 warnings.warn("The pyemma.msm.analysis module has been deprecated. "
-              "You should import msmtools.analysis now.", ShimWarning)
+              "You should import msmtools.analysis now.", PyEMMA_DeprecationWarning)
 
 sys.modules['pyemma.msm.analysis'] = ShimModule(src='pyemma.msm.analysis', mirror='msmtools.analysis')
 
--- pyemma/msm/generation/__init__.py
+++ pyemma/msm/generation/__init__.py
@@ -1,11 +1,11 @@
 import sys
 import warnings
 
-from pyemma.util._ext.shimmodule import ShimModule, ShimWarning
+from pyemma.util._ext.shimmodule import ShimModule
+from pyemma.util.exceptions import PyEMMA_DeprecationWarning
 
 warnings.warn("The pyemma.msm.generation module has been deprecated. "
-              "You should import msmtools.generation now.", ShimWarning)
+              "You should import msmtools.generation now.", PyEMMA_DeprecationWarning)
 
 sys.modules['pyemma.msm.generation'] = ShimModule(src='pyemma.msm.generation', mirror='msmtools.generation')
 
-from msmtools.generation import *
