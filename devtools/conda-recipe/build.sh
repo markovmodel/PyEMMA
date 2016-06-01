714c047eeeb5111a5b025020c38d172f30b7a711
marscher <m.scherer@fu-berlin.de>
2016-06-01 15:05:07 +0200

    f

--- devtools/conda-recipe/build.sh
+++ devtools/conda-recipe/build.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 $PYTHON setup.py install
 version=$($PYTHON -c "from __future__ import print_function; import pyemma; print(pyemma.__version__)")
-export PYEMMA_VERSION $version 
+export PYEMMA_VERSION=$version 
