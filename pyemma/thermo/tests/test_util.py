a0379a160d9d7ef8eaf5dc2d6302599e16b4a7c7
Christoph Wehmeyer <christoph.wehmeyer@fu-berlin.de>
2016-06-06 17:04:59 +0200

    [thermo] adding tests for the protected umbrella sampling helper functions for the case that only one trajectory is supplied

--- pyemma/thermo/tests/test_util.py
+++ pyemma/thermo/tests/test_util.py
@@ -243,6 +243,12 @@ class TestProtectedUmbrellaSamplingParameters(unittest.TestCase):
             [np.array([0, 0, 0]), np.array([1, 1, 1]), np.array([2, 2, 2])],
             ref_umbrella_centers, ref_force_constants, 2)
         # with kT parameter
+        with self.assertRaises(ValueError):
+            util._get_umbrella_sampling_parameters(
+                us_trajs, ref_umbrella_centers, ref_force_constants, md_trajs=md_trajs, kT=0.0)
+        with self.assertRaises(ValueError):
+            util._get_umbrella_sampling_parameters(
+                us_trajs, ref_umbrella_centers, ref_force_constants, md_trajs=md_trajs, kT='kT')
         ttrajs, umbrella_centers, force_constants, unbiased_state = \
             util._get_umbrella_sampling_parameters(
                 us_trajs, ref_umbrella_centers, ref_force_constants, md_trajs=md_trajs, kT=2.0)
@@ -250,6 +256,18 @@ class TestProtectedUmbrellaSamplingParameters(unittest.TestCase):
             ttrajs, umbrella_centers, force_constants * 2.0, unbiased_state,
             [np.array([0, 0, 0]), np.array([1, 1, 1]), np.array([2, 2, 2])],
             ref_umbrella_centers, ref_force_constants, 2)
+        # single trajectory outside list
+        ref_umbrella_centers = 1.0
+        ref_force_constants = 1.0
+        us_trajs = np.array([0.0, 0.1, 0.2])
+        md_trajs = np.array([0.0, 0.5, 1.0])
+        ttrajs, umbrella_centers, force_constants, unbiased_state = \
+            util._get_umbrella_sampling_parameters(
+                us_trajs, ref_umbrella_centers, ref_force_constants, md_trajs=md_trajs, kT=2.0)
+        self._assert_parameters(
+            ttrajs, umbrella_centers, force_constants * 2.0, unbiased_state,
+            [np.array([0, 0, 0]), np.array([1, 1, 1])],
+            [ref_umbrella_centers], [ref_force_constants], 1)
 
     def test_umbrella_sampling_parameters_1x1(self):
         ref_umbrella_centers = [0.0, 1.0]
@@ -272,6 +290,12 @@ class TestProtectedUmbrellaSamplingParameters(unittest.TestCase):
             [np.array([0, 0, 0]), np.array([1, 1, 1]), np.array([2, 2, 2])],
             ref_umbrella_centers, ref_force_constants, 2)
         # with kT parameter
+        with self.assertRaises(ValueError):
+            util._get_umbrella_sampling_parameters(
+                us_trajs, ref_umbrella_centers, ref_force_constants, md_trajs=md_trajs, kT=0.0)
+        with self.assertRaises(ValueError):
+            util._get_umbrella_sampling_parameters(
+                us_trajs, ref_umbrella_centers, ref_force_constants, md_trajs=md_trajs, kT='kT')
         ttrajs, umbrella_centers, force_constants, unbiased_state = \
             util._get_umbrella_sampling_parameters(
                 us_trajs, ref_umbrella_centers, ref_force_constants, md_trajs=md_trajs, kT=2.0)
@@ -279,6 +303,19 @@ class TestProtectedUmbrellaSamplingParameters(unittest.TestCase):
             ttrajs, umbrella_centers, force_constants * 2.0, unbiased_state,
             [np.array([0, 0, 0]), np.array([1, 1, 1]), np.array([2, 2, 2])],
             ref_umbrella_centers, ref_force_constants, 2)
+        # single trajectory outside list
+        ref_umbrella_centers = 1.0
+        ref_force_constants = 1.0
+        us_trajs = np.array([[0.0], [0.1], [0.2]])
+        md_trajs = np.array([[0.0], [0.5], [1.0]])
+        ttrajs, umbrella_centers, force_constants, unbiased_state = \
+            util._get_umbrella_sampling_parameters(
+                us_trajs, ref_umbrella_centers, ref_force_constants, md_trajs=md_trajs, kT=2.0)
+        self._assert_parameters(
+            ttrajs, umbrella_centers, force_constants * 2.0, unbiased_state,
+            [np.array([0, 0, 0]), np.array([1, 1, 1])],
+            [ref_umbrella_centers], [ref_force_constants], 1)
+
 
 class TestProtectedUmbrellaSamplingBiasSequence(unittest.TestCase):
 
@@ -352,17 +389,3 @@ class TestProtectedUmbrellaSamplingBiasSequence(unittest.TestCase):
             util._get_umbrella_bias_sequences(
                 [np.array([[0.0, 1.0, 2.0], [0.5, 1.0, 2.0], [1.0, 1.0, 2.0]])],
                 np.array([[1.0, 1.0]]), np.array([[[1.0, 0.0], [1.0, 0.0]]]))
-
-
-
-
-
-
-
-
-
-
-
-
-
-
