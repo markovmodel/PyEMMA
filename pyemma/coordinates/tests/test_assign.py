f98d65e4c78c9cb237abf0563f7c9676ce7790c4
Martin K. Scherer <m.scherer@fu-berlin.de>
2016-06-20 17:46:58 +0200

    added test

--- pyemma/coordinates/tests/test_assign.py
+++ pyemma/coordinates/tests/test_assign.py
@@ -263,6 +263,23 @@ class TestClusterAssign(unittest.TestCase):
 
         np.testing.assert_equal(assignment_mp, assignment_sp)
 
+    def test_min_rmsd(self):
+        import pyemma.datasets as data
+        d = data.get_bpti_test_data()
+        reader = coor.source(d['trajs'], top=d['top'])
+
+        N_centers = 9
+        centers = np.asarray((reader.ra_itraj_jagged[0, [0, 1, 7]],
+                              reader.ra_itraj_jagged[1, [32, 1, 23]],
+                              reader.ra_itraj_jagged[2, [17, 8, 15]])
+                             ).reshape((N_centers, -1))
+        dtraj = coor.assign_to_centers(reader, centers=centers, metric='minRMSD', return_dtrajs=True)
+
+        num_assigned_states = len(np.unique(np.concatenate(dtraj)))
+        self.assertEqual(num_assigned_states, N_centers,
+                         "assigned states=%s out of %s possible ones."
+                         % (num_assigned_states, N_centers))
+
 
 if __name__ == "__main__":
     unittest.main()
