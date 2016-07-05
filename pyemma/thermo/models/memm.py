ae245331556fbd1ab0a126dab283e5f6cd9c7dbf
Christoph Wehmeyer <christoph.wehmeyer@fu-berlin.de>
2016-07-05 18:01:29 +0200

    [subset] adding module with intermediate MSM class for MEMMs

---
+++ pyemma/thermo/models/memm.py
@@ -0,0 +1,28 @@
+# This file is part of PyEMMA.
+#
+# Copyright (c) 2016 Computational Molecular Biology Group, Freie Universitaet Berlin (GER)
+#
+# PyEMMA is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Lesser General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+from pyemma.msm import MSM as _MSM
+from pyemma._base.subset import SubSet as _SubSet
+
+class ThermoMSM(_MSM, _SubSet):
+    def __init__(
+        self, P, active_set, nstates_full,
+        pi=None, reversible=None, dt_model='1 step', neig=None, ncv=None):
+        super(ThermoMSM, self).__init__(
+            P, pi=pi, reversible=reversible, dt_model=dt_model, neig=neig, ncv=ncv)
+        self.active_set = active_set
+        self.nstates_full = nstates_full
