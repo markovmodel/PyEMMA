2a12a0f85f5ff9aeaa4abebe4afdd9e7e755e8f2
Christoph Wehmeyer <christoph.wehmeyer@fu-berlin.de>
2016-08-18 18:20:03 +0200

    [subset, thermo] decorating ThermoMSM

--- pyemma/thermo/models/memm.py
+++ pyemma/thermo/models/memm.py
@@ -18,20 +18,74 @@
 import numpy as _np
 from pyemma.msm import MSM as _MSM
 from pyemma.msm.util.subset import SubSet as _SubSet
+from pyemma.msm.util.subset import add_full_state_methods as _add_full_state_methods
+from pyemma.msm.util.subset import map_to_full_state as _map_to_full_state
 from pyemma.thermo.models.multi_therm import MultiThermModel as _MultiThermModel
 
+@_add_full_state_methods
 class ThermoMSM(_MSM, _SubSet):
     def __init__(
         self, P, active_set, nstates_full,
         pi=None, reversible=None, dt_model='1 step', neig=None, ncv=None):
+        r"""Markov model with a given transition matrix
+
+        Parameters
+        ----------
+        P : ndarray(n,n)
+            transition matrix
+
+        active_set : arraylike of int
+            indices of the configuration states for which P is defined.
+
+        nstates_full : int
+            total number of configuration states.
+
+        pi : ndarray(n), optional, default=None
+            stationary distribution. Can be optionally given in case if it was
+            already computed, e.g. by the estimator.
+
+        reversible : bool, optional, default=None
+            whether P is reversible with respect to its stationary distribution.
+            If None (default), will be determined from P
+
+        dt_model : str, optional, default='1 step'
+            Description of the physical time corresponding to one time step of the
+            MSM (aka lag time). May be used by analysis algorithms such as plotting
+            tools to pretty-print the axes.
+            By default '1 step', i.e. there is no physical time unit. Specify by a
+            number, whitespace and unit. Permitted units are
+            (* is an arbitrary string):
+
+            |  'fs',  'femtosecond*'
+            |  'ps',  'picosecond*'
+            |  'ns',  'nanosecond*'
+            |  'us',  'microsecond*'
+            |  'ms',  'millisecond*'
+            |  's',   'second*'
+
+        neig : int or None
+            The number of eigenvalues / eigenvectors to be kept. If set to None,
+            defaults will be used. For a dense MSM the default is all eigenvalues.
+            For a sparse MSM the default is 10.
+
+        ncv : int (optional)
+            Relevant for eigenvalue decomposition of reversible transition
+            matrices. ncv is the number of Lanczos vectors generated, `ncv` must
+            be greater than k; it is recommended that ncv > 2*k.
+
+        """
         super(ThermoMSM, self).__init__(
             P, pi=pi, reversible=reversible, dt_model=dt_model, neig=neig, ncv=ncv)
         self.active_set = active_set
         self.nstates_full = nstates_full
+
     @property
+    @_map_to_full_state(default_arg=_np.inf)
     def f(self):
         return self.free_energies
+
     @property
+    @_map_to_full_state(default_arg=_np.inf)
     def free_energies(self):
         return -_np.log(self.stationary_distribution)
 
