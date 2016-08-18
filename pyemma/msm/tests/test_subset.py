95ca6d181fc29208b43fff8c8da17cb4de60a5bd
Christoph Wehmeyer <christoph.wehmeyer@fu-berlin.de>
2016-08-18 18:18:41 +0200

    [subset] amend subset tests

--- pyemma/msm/tests/test_subset.py
+++ pyemma/msm/tests/test_subset.py
@@ -13,12 +13,7 @@ class test_class(SubSet):
     def __init__(self, active_set=(2, 3, 4), nstates_full=10):
         self.active_set = active_set
         self.nstates_full = nstates_full
-
-    @map_to_full_state(default_arg=0)
-    def eigenvalues(self, k=None):
-        assert k == k_global, k
-        return [0, 1, 2]
-
+    
     @map_to_full_state(default_arg=np.inf)
     def right_eigenvectors(self):
         return np.arange(27).reshape(-1, 3)
@@ -36,16 +31,12 @@ class test_class(SubSet):
 class TestSubset(unittest.TestCase):
     def test_has_member(self):
         inst = test_class()
-        assert hasattr(inst, 'eigenvalues_full_state')
-        assert hasattr(test_class, 'eigenvalues_full_state')
-        self.assertIsInstance(inst.eigenvalues_full_state, types.MethodType)
-        self.assertIsInstance(test_class.test_property, property)
-
-    def test_ev(self):
-        inst = test_class()
-        expected = np.zeros(inst.nstates_full)
-        expected[inst.active_set] = inst.eigenvalues(k=k_global)
-        np.testing.assert_equal(inst.eigenvalues_full_state(k=k_global), expected)
+        assert hasattr(inst, 'left_eigenvectors_full_state')
+        assert hasattr(test_class, 'left_eigenvectors_full_state')
+        assert hasattr(inst, 'test_property_full_state')
+        assert hasattr(test_class, 'test_property_full_state')
+        self.assertIsInstance(inst.left_eigenvectors_full_state, types.MethodType)
+        self.assertIsInstance(test_class.test_property_full_state, property)
 
     def test_shape_left_ev(self):
         inst = test_class(np.arange(start=23, stop=32), nstates_full=60)
