3ffe84a3d17a3d445a149356b9b384abc9c16451
Christoph Wehmeyer <christoph.wehmeyer@fu-berlin.de>
2016-09-07 12:22:31 +0200

    [thermo, subset] enabled subset in thermo.stationary_model

--- pyemma/thermo/models/stationary.py
+++ pyemma/thermo/models/stationary.py
@@ -18,12 +18,14 @@
 import numpy as _np
 from pyemma._base.model import Model as _Model
 from pyemma.msm.util.subset import SubSet as _SubSet
+from pyemma.msm.util.subset import add_full_state_methods as _add_full_state_methods
+from pyemma.msm.util.subset import map_to_full_state as _map_to_full_state
 from pyemma.util import types as _types
 from thermotools.util import logsumexp as _logsumexp
 
 __author__ = 'noe'
 
-
+@_add_full_state_methods
 class StationaryModel(_Model, _SubSet):
     r""" StationaryModel combines a stationary vector with discrete-state free energies.
 
@@ -103,14 +105,24 @@ class StationaryModel(_Model, _SubSet):
         return self._nstates
 
     @property
+    @_map_to_full_state(default_arg=0.0)
     def stationary_distribution(self):
         """The stationary distribution"""
         return self.pi
 
     @property
+    def pi_full_state(self):
+        return self.stationary_distribution_full_state
+
+    @property
+    @_map_to_full_state(default_arg=_np.inf)
     def free_energies(self):
-        """The free energies of discrete states"""
         return self.f
+
+    @property
+    def f_full_state(self):
+        """The free energies of discrete states"""
+        return self.free_energies_full_state
 
     def expectation(self, a):
         r"""Equilibrium expectation value of a given observable.
