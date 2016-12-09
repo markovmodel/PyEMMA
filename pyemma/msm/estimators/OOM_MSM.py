a86eb3ec1a5752c076d187ab95511166117da614
fnueske <feliks.nueske@fu-berlin.de>
2016-12-09 19:11:05 +0100

    First implementation of four basis functions for OOM-MSM estimation.

--- pyemma/msm/estimators/OOM_MSM.py
+++ pyemma/msm/estimators/OOM_MSM.py
@@ -1,6 +1,8 @@
 import numpy as np
 import scipy.linalg as scl
 import scipy.sparse
+from variational.solvers.direct import sort_by_norm
+import msmtools.estimation as me
 
 def bootstrapping_count_matrix(dtrajs, lag, nstates, nbs=500):
     """
@@ -15,7 +17,7 @@ def bootstrapping_count_matrix(dtrajs, lag, nstates, nbs=500):
     nstates : int
         the number of states in the discrete trajectories.
     nbs : int, optional, default=500
-        the number of re-samplings to be drawn from dtrajs.
+        the number of re-samplings to be drawn from dtrajs
 
     Returns
     -------
@@ -24,44 +26,38 @@ def bootstrapping_count_matrix(dtrajs, lag, nstates, nbs=500):
     sdev : ndarray(N,)
         standard deviations of singular values
     Ct : ndarray(N, N)
-        actual count matrix of the data.
+        actual count matrix of the data
     """
-    # List all transition pairs. Note that we only use pairs up to step -lag:
-    rows = []
-    cols = []
-    for dtraj in dtrajs:
-        if dtraj.size > 2*lag:
-            rows.append(dtraj[0:-2*lag])
-            cols.append(dtraj[lag:-lag])
-    # Perform bootstrapping:
-    ntraj = len(rows)
-    svals = np.array((nbs, nstates))
+    # Reduce dtrajs by discarding last tau steps:
+    dtrajs_lag = [traj[:-lag] for traj in dtrajs]
+    ntraj = len(dtrajs)
+    # Estimate count matrices for all dtrajs:
+    C = np.zeros((ntraj, nstates, nstates), dtype=int)
+    for i in range(ntraj):
+        C[i, :, :] = me.count_matrix(dtrajs_lag[i], lag, sparse_return=False, nstates=nstates)
+    # Perform the bootstrapping:
+    svals = np.zeros((nbs, nstates))
     for s in range(nbs):
         # Draw sample:
         sel = np.random.choice(ntraj, ntraj, replace=True)
-        srows = rows[sel]
-        scols = cols[sel]
-        # Compute count matrix:
-        srows = np.concatenate(srows)
-        scols = np.concatenate(scols)
-        sdata = np.ones(rows.size)
-        sC = scipy.sparse.coo_matrix((sdata, (srows, scols)), shape=(nstates, nstates))
-        sC = sC.toarray()
+        # Determine number of occurences of each traj:
+        uind, uc = np.unique(sel, return_counts=True)
+        ucount = np.zeros(ntraj, dtype=int)
+        ucount[uind] = uc
+        # Multiply each count matrix by its number of occurences and sum up:
+        sC = ucount[:, None, None] * C
+        sC = np.sum(sC, axis=0)
         # Compute singular values:
         svals[s, :] = scl.svdvals(sC)
     # Compute mean and uncertainties:
     smean = np.mean(svals, axis=0)
     sdev = np.std(svals, axis=0)
     # Compute the actual count matrix if needed:
-    row = np.concatenate(rows)
-    col = np.concatenate(cols)
-    data = np.ones(row.size)
-    Ct = scipy.sparse.coo_matrix((data, (row, col)), shape=(nstates, nstates))
-    Ct = Ct.toarray()
+    Ct = me.count_matrix(dtrajs_lag, lag, sparse_return=False, nstates=nstates)
 
     return smean, sdev, Ct
 
-def compute_twostep_count_matrix(dtrajs, lag, nstates):
+def twostep_count_matrix(dtrajs, lag, nstates):
     """
     Compute all two-step count matrices from discrete trajectories.
 
@@ -77,7 +73,7 @@ def compute_twostep_count_matrix(dtrajs, lag, nstates):
     Returns
     -------
     C2t : sparse csc-matrix (N, N, N)
-        two-step count matrices for all states. C2t[:, n, :] is a count matrix for each n.
+        two-step count matrices for all states. C2t[:, n, :] is a count matrix for each n
 
     """
     # List all transition triples:
@@ -92,11 +88,103 @@ def compute_twostep_count_matrix(dtrajs, lag, nstates):
     row = np.concatenate(rows)
     col = np.concatenate(cols)
     state = np.concatenate(states)
+    data = np.ones(row.size)
     # Transform the rows and cols into a single list with N*+2 possible values:
     pair = nstates * row + col
     # Estimate sparse matrix:
-    data = np.ones(pair.size)
     C2t = scipy.sparse.coo_matrix((data, (pair, state)), shape=(nstates*nstates, nstates))
 
     return C2t.tocsc()
 
+def oom_components(Ct, C2t, smean, sdev, tol=10.0):
+    """
+    Compute OOM components and eigenvalues from count matrices:
+
+    Parameters
+    ----------
+    Ct : ndarray(N, N)
+        count matrix from data
+    C2t : sparse csc-matrix (N, N, N)
+        two-step count matrix from data for all states
+    smean : ndarray(N,)
+        mean values of singular values for Ct
+    sdev : ndarray(N,)
+        standard errors of singular values for Ct
+    tol : float, optional default
+        accept singular values with signal-to-noise ratio >= tol
+
+    Returns
+    -------
+    Xi : ndarray(M, N, M)
+        matrix of set-observable operators
+    omega: ndarray(M,)
+        information state vector of OOM
+    sigma : ndarray(M,)
+        evaluator of OOM
+    l : ndarray(M,)
+        eigenvalues from OOM
+    """
+    # Determine signal-to-noise ratios of singular values:
+    sratio = smean / sdev
+    # Decompose count matrix by SVD:
+    V, s, W = scl.svd(Ct, full_matrices=False)
+    # Make rank decision:
+    ind = np.where(sratio >= tol)[0]
+    V = V[:, ind]
+    s = s[ind]
+    W = W[ind, :].T
+
+    # Compute transformations:
+    F1 = np.dot(V, np.diag(s**-0.5))
+    F2 = np.dot(W, np.diag(s**-0.5))
+
+    # Apply the transformations to C2t:
+    N = Ct.shape[0]
+    M = F1.shape[1]
+    Xi = np.zeros((M, N, M))
+    for n in range(N):
+        C2t_n = C2t[:, n].toarray()
+        C2t_n = np.reshape(C2t_n, (N, N))
+        Xi[:, n, :] = np.dot(F1.T, np.dot(C2t_n, F2))
+
+    # Compute sigma:
+    c = np.sum(Ct, axis=1)
+    sigma = np.dot(F1.T, c)
+    # Compute omega and all eigenvalues:
+    Xi_S = np.sum(Xi, axis=1)
+    l, R = scl.eig(Xi_S.T)
+    l, R = sort_by_norm(l, R)
+    omega = R[:, 0]
+    omega = omega / np.dot(omega, sigma)
+
+    return Xi, omega, sigma, l
+
+def equilibrium_transition_matrix(Xi, omega, sigma):
+    """
+    Compute equilibrium transition matrix from OOM components:
+
+    Parameters
+    ----------
+    Xi : ndarray(M, N, M)
+        matrix of set-observable operators
+    omega: ndarray(M,)
+        information state vector of OOM
+    sigma : ndarray(M,)
+        evaluator of OOM
+
+    Returns
+    -------
+    Tt_Eq : ndarray(N, N)
+        equilibrium transition matrix
+    """
+    # Compute equilibrium transition matrix:
+    Ct_Eq = np.einsum('j,jkl,lmn,n->km', omega, Xi, Xi, sigma)
+    # Remove negative entries:
+    Ct_Eq[Ct_Eq < 0.0] = 0.0
+    # Compute transition matrix after symmetrization:
+    pi_r = np.sum(Ct_Eq, axis=0)
+    pi_c = np.sum(Ct_Eq, axis=1)
+    pi_sym = np.dot(np.diag(pi_r + pi_c), np.ones(Ct_Eq.shape))
+    Tt_Eq = (Ct_Eq + Ct_Eq.T) / pi_sym
+
+    return Tt_Eq
