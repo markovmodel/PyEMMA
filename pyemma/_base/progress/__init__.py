66542f7a87f94e5b3c514c5bb40df660c25fffef
Martin Scherer <m.scherer@fu-berlin.de>
2017-02-13 21:32:50 +0100

    f

--- pyemma/_base/progress/__init__.py
+++ pyemma/_base/progress/__init__.py
@@ -16,27 +16,4 @@
 # You should have received a copy of the GNU Lesser General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-
-def _ProgressReporter_pyemma_config(cls):
-    # monkey patch the getter to respect pyemmas config.
-
-    @cls.show_progress.getter
-    def show_progress(self):
-        """ whether to show the progress of heavy calculations on this object. """
-        from pyemma import config
-        # no value yet, obtain from config
-        if not hasattr(self, "_show_progress"):
-            val = config.show_progress_bars
-            self._show_progress = val
-        # config disabled progress?
-        elif not config.show_progress_bars:
-            return False
-
-        return self._show_progress
-
-    cls.show_progress = show_progress
-    return cls
-
-
-from progress_reporter import ProgressReporter as _impl
-ProgressReporter = _ProgressReporter_pyemma_config(_impl)
+from .reporter import ProgressReporter
