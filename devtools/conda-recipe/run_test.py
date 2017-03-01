9d0d79f2fcf248f73b0eeebde15d3ddaa8611bb1
Martin K. Scherer <m.scherer@fu-berlin.de>
2017-03-01 17:41:12 +0100

    simplify

--- devtools/conda-recipe/run_test.py
+++ devtools/conda-recipe/run_test.py
@@ -1,26 +1,19 @@
-import tempfile
 import os
 import sys
 import pytest
 import shutil
-import pkg_resources
 
 test_pkg = 'pyemma'
 cover_pkg = test_pkg
 
 junit_xml = os.path.join(os.getenv('CIRCLE_TEST_REPORTS', '.'), 'junit.xml')
 
-if os.getenv('CONDA_BUILD', False):
-    pytest_cfg = pkg_resources.resource_filename(test_pkg, 'setup.cfg')
-else:
-    pytest_cfg = os.path.join(os.path.abspath(os.path.dirname(__file__)), '../../setup.cfg')
+pytest_cfg = 'setup.cfg'
 
 print("Using pytest config file: %s" % pytest_cfg)
 assert os.path.exists(pytest_cfg), "pytest cfg not found"
 
-# chdir to an path outside of conda-bld, which is known to persist the build phase
-run_dir = tempfile.mkdtemp()
-os.chdir(run_dir)
+print("current cwd:", os.getcwd())
 
 # matplotlib headless backend
 with open('matplotlibrc', 'w') as fh:
