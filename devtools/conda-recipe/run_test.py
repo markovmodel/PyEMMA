cc6edcf0475b291c7529e42a2551fa72611fbec6
Martin K. Scherer <m.scherer@fu-berlin.de>
2017-03-02 15:57:50 +0100

    wip

--- devtools/conda-recipe/run_test.py
+++ devtools/conda-recipe/run_test.py
@@ -8,13 +8,15 @@ test_pkg = 'pyemma'
 cover_pkg = test_pkg
 
 junit_xml = os.path.join(os.getenv('CIRCLE_TEST_REPORTS', '.'), 'junit.xml')
-
+print("cwd:", os.getcwd())
 pytest_cfg = 'setup.cfg'
+conftest = 'conftest.py'
 
 print("Using pytest config file: %s" % pytest_cfg)
 assert os.path.exists(pytest_cfg), "pytest cfg not found"
 tmp = tempfile.mkdtemp()
 shutil.copy(pytest_cfg, tmp)
+shutil.copy(conftest, tmp)
 os.chdir(tmp)
 print("current cwd:", os.getcwd())
 
@@ -24,11 +26,11 @@ with open('matplotlibrc', 'w') as fh:
 
 pytest_args = ("-v --pyargs {test_pkg} "
                "--cov={cover_pkg} "
-               "--cov-report=xml "
+               "--cov-report=html "
                "--doctest-modules "
                #"-n 2 "# -p no:xdist" # disable xdist in favour of coverage plugin
                "--junit-xml={junit_xml} "
-               "-c {pytest_cfg}"
+               "-c {pytest_cfg} --fixtures"
                .format(test_pkg=test_pkg, cover_pkg=cover_pkg,
                        junit_xml=junit_xml, pytest_cfg=pytest_cfg)
                .split(' '))
