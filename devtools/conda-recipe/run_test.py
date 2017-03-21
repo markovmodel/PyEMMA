772e3a94ca8e160818e6e2ba8ab33abc3a003caa
Martin K. Scherer <m.scherer@fu-berlin.de>
2017-03-21 16:45:35 +0100

    cleanup

--- devtools/conda-recipe/run_test.py
+++ devtools/conda-recipe/run_test.py
@@ -1,35 +1,25 @@
 import os
 import sys
+
 import pytest
-import shutil
-import tempfile
 
 test_pkg = 'pyemma.coordinates'
 cover_pkg = test_pkg
 
 junit_xml = os.path.join(os.getenv('CIRCLE_TEST_REPORTS', '.'), 'junit.xml')
-#print("cwd:", os.getcwd())
-#pytest_cfg = 'setup.cfg'
-#conftest = 'conftest.py'
 
-#print("Using pytest config file: %s" % pytest_cfg)
-#assert os.path.exists(pytest_cfg), "pytest cfg not found"
-#tmp = tempfile.mkdtemp()
-#shutil.copy(pytest_cfg, tmp)
-#shutil.copy(conftest, tmp)
-#os.chdir(tmp)
-#print("current cwd:", os.getcwd())
 
 pytest_args = ("-v --pyargs {test_pkg} "
                "--cov={cover_pkg} "
-               "--cov-report=html:{dest_report} "
+               "--cov-report=xml:{dest_report} "
                "--doctest-modules "
                #"-n 2 "# -p no:xdist" # disable xdist in favour of coverage plugin
                "--junit-xml={junit_xml} "
                "-c {pytest_cfg}"
                .format(test_pkg=test_pkg, cover_pkg=cover_pkg,
                        junit_xml=junit_xml, pytest_cfg='setup.cfg',
-                       dest_report=os.path.expanduser('~/'))
+                       dest_report=os.path.join(os.path.expanduser('~/'), 'coverage.xml'),
+                       )
                .split(' '))
 print("args:", pytest_args)
 res = pytest.main(pytest_args)
