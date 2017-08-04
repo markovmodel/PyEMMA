c3b751fda716006bcfeb64c5b58912ea755be5ef
marscher <m.scherer@fu-berlin.de>
2017-08-04 19:29:32 +0200

    openmp for cluster, same pattern as in assign.

--- pyemma/coordinates/clustering/include/bits/kmeans_bits.h
+++ pyemma/coordinates/clustering/include/bits/kmeans_bits.h
@@ -64,33 +64,33 @@ KMeans<dtype>::cluster(const np_array& np_chunk, const np_array& np_centers) con
         }
     }
 #else
+    std::vector<dtype> dists(N_centers);
     #pragma omp parallel
     {
-        size_t local_index = 0;
-        for (i = 0; i < N_frames; ++i) {
-            auto mindist = std::numeric_limits<dtype>::max();
-            #pragma omp for nowait
-            for (j = 0; j < N_centers; ++j) {
-                auto d = parent_t::metric->compute(&chunk(i, 0), &centers(j, 0));
+    for (size_t i = 0; i < N_frames; ++i) {
+        auto mindist = std::numeric_limits<dtype>::max();
+        #pragma omp for
+        for (size_t j = 0; j < N_centers; ++j) {
+            dists[j] = parent_t::metric->compute(&chunk(i, 0), &centers(j, 0));
+        }
+        #pragma omp flush(dists)
 
-                if (d < mindist) {
-                    mindist = d;
-                    local_index = j;
+        #pragma omp single
+        {
+            dtype min_dist = std::numeric_limits<dtype>::max();
+
+            for (size_t j = 0; j  < N_centers; ++j) {
+                if (dists[j] < min_dist) {
+                    min_dist = dists[j];
+                    closest_center_index = j;
                 }
             }
 
-            #pragma omp critical
-            {
-                if(mindist < min) {
-                    min = mindist;
-                    closest_center_index = local_index;
-                }
-
-                (*(centers_counter_p + closest_center_index))++;
-                for (j = 0; j < dim; ++j) {
-                    new_centers(closest_center_index, j) += chunk(i, j);
-                }
+            (*(centers_counter_p + closest_center_index))++;
+            for (j = 0; j < dim; j++) {
+                new_centers(closest_center_index, j) += chunk(i, j);
             }
+        }
         }
     }
 #endif
@@ -168,7 +168,6 @@ initCentersKMpp(const np_array& np_data, unsigned int random_seed) const {
     std::default_random_engine generator(random_seed);
     std::uniform_int_distribution<size_t> uniform_dist(0, n_frames - 1);
     first_center_index = uniform_dist(generator);
-    printf("first center index: %d\n", first_center_index);
     /* and mark it as assigned */
     taken_points[first_center_index] = true;
     /* write its coordinates into the init_centers array */
@@ -185,7 +184,6 @@ initCentersKMpp(const np_array& np_data, unsigned int random_seed) const {
     /* iterate over all data points j, measuring the squared distance between j and the initial center i: */
     /* squared_distances[i] = distance(x_j, x_i)*distance(x_j, x_i) */
     dtype dist_sum = 0.0;
-    printf("first center: %f, %f \n", data(first_center_index, 0), data(first_center_index, 1));
     for (i = 0; i < n_frames; i++) {
         if (i != first_center_index) {
             auto value = parent_t::metric->compute(&data(i, 0), &data(first_center_index, 0));
@@ -195,7 +193,6 @@ initCentersKMpp(const np_array& np_data, unsigned int random_seed) const {
             dist_sum += value;
         }
     }
-    printf("dist sum: %f\n", dist_sum);
 
     /* keep picking centers while we do not have enough of them... */
     while (centers_found < k) {
@@ -204,14 +201,8 @@ initCentersKMpp(const np_array& np_data, unsigned int random_seed) const {
         for (j = 0; j < n_trials; j++) {
             next_center_candidates[j] = -1;
             auto point_index = uniform_dist(generator);
-            printf("trial=%d, random point=%d\n", j, point_index);
-            assert (point_index != 0);
             next_center_candidates_rand[j] = dist_sum * ((dtype) point_index / (dtype) uniform_dist.max());
             next_center_candidates_potential[j] = 0.0;
-        }
-        for (int count = 0; count < n_trials; ++count) {
-            //printf("next_center_candidates_rand: %f\n", next_center_candidates_rand[count]);
-            //printf("next_center_candidates: %f\n", next_center_candidates[count]);
         }
         /* pick candidate data points corresponding to their random value */
         sum = 0.0;
@@ -229,9 +220,7 @@ initCentersKMpp(const np_array& np_data, unsigned int random_seed) const {
                         }
                     }
                 }
-                if (!some_not_done) {
-                    break;
-                }
+                if (!some_not_done) break;
             }
         }
 
