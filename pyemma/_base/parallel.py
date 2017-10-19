1fdd95fa74cabae0c16e26a9da2b0f299e30e8d1
marscher <m.scherer@fu-berlin.de>
2017-10-19 18:16:04 +0200

    [parallel] provide utitily function to obtain njobs from env.

--- pyemma/_base/parallel.py
+++ pyemma/_base/parallel.py
@@ -1,4 +1,36 @@
 
+def get_n_jobs(logger=None):
+    import psutil
+
+    def _from_hardware():
+        return psutil.cpu_count(logical=False)
+
+    def _from_env(var):
+        import os
+        e = os.getenv(var, None)
+        if e:
+            try:
+                return int(e)
+            except ValueError as ve:
+                if logger is not None:
+                    logger.warning("could not parse env variable '{var}'."
+                                   " Value='{val}'. Error={err}. Will use {val} jobs."
+                                   .format(err=ve, val=val, var=var))
+        return None
+
+    slurm_njobs = _from_env('SLURM_CPUS_ON_NODE')  # Number of CPUS on the allocated SLURM node.
+    pyemma_njobs = _from_env('PYEMMA_NJOBS')
+
+    if slurm_njobs and pyemma_njobs:
+        import warning
+        warning.warn('two settings for n_jobs from environment: PYEMMA_NJOBS and SLURM_CPUS_ON_NODE. '
+                     'Respecting the SLURM setting to avoid overprovisioning')
+
+    # slurm njobs will be used preferably.
+    val = slurm_njobs or pyemma_njobs
+    if not val:
+        val = _from_hardware()
+
 
 class NJobsMixIn(object):
     # mixin for sklearn-like estimators (estimation/ctor parameter has to contain n_jobs).
@@ -16,7 +48,6 @@ class NJobsMixIn(object):
         By setting the environment variable 'OMP_NUM_THREADS' to an integer,
         one will override the default argument of n_jobs (currently None).
         """
-        assert isinstance(self._n_jobs, int)
         return self._n_jobs
 
     @n_jobs.setter
@@ -35,39 +66,6 @@ class NJobsMixIn(object):
         to obtain a job number.
 
         """
-        import psutil
-
         if val is None:
-
-            def _from_hardware():
-                return psutil.cpu_count(logical=False)
-
-            def _from_env(var):
-                import os
-                e = os.getenv(var, None)
-                if e:
-                    try:
-                        return int(e)
-                    except ValueError as ve:
-                        if hasattr(self, 'logger'):
-                            self.logger.warning("could not parse env variable '{var}'."
-                                                " Value='{val}'. Error={err}. Will use {val} jobs."
-                                                .format(err=ve, val=val, var=var))
-                return None
-
-            slurm_njobs = _from_env('SLURM_CPUS_ON_NODE')  # Number of CPUS on the allocated SLURM node.
-            pyemma_njobs = _from_env('PYEMMA_NJOBS')
-
-            if slurm_njobs and pyemma_njobs:
-                import warning
-                warning.warn('two settings for n_jobs from environment: PYEMMA_NJOBS and SLURM_CPUS_ON_NODE. '
-                             'Respecting the SLURM setting to avoid overprovisioning')
-
-            # slurm njobs will be used preferably.
-            val = slurm_njobs or pyemma_njobs
-            if not val:
-                val = _from_hardware()
-
+            val = get_n_jobs(logger=getattr(self, 'logger'))
         self._n_jobs = int(val)
-        # possible optimization: set affinity to first cpus to avoid switching cores and trashing the cache.
-        #psutil.Process().cpu_affinity(range(self._n_jobs))
