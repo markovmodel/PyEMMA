f9301cb0652dfe5b928278f528ec9d5057b0b7ba
Martin K. Scherer <m.scherer@fu-berlin.de>
2017-11-29 23:05:47 +0100

    [lagged covar] chunksize is not an estimation parameter.

--- pyemma/coordinates/estimation/covariance.py
+++ pyemma/coordinates/estimation/covariance.py
@@ -77,14 +77,14 @@ class LaggedCovariance(StreamingEstimator):
          Use only every stride-th time step. By default, every time step is used.
      skip : int, optional, default=0
          skip the first initial n frames per trajectory.
-     chunksize : int, optional, default=None
-         The chunk size at which the input files are being processed.
+     chunksize : deprecated, default=NoTImplemented
+         The chunk size can be se during estimation.
 
      """
     def __init__(self, c00=True, c0t=False, ctt=False, remove_constant_mean=None, remove_data_mean=False, reversible=False,
                  bessel=True, sparse_mode='auto', modify_data=False, lag=0, weights=None, stride=1, skip=0,
-                 chunksize=None, ncov_max=float('inf')):
-        super(LaggedCovariance, self).__init__(chunksize=chunksize)
+                 chunksize=NotImplemented, ncov_max=float('inf')):
+        super(LaggedCovariance, self).__init__()
 
         if (c0t or ctt) and lag == 0:
             raise ValueError("lag must be positive if c0t=True or ctt=True")
