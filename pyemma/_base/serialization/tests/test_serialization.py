fd08fea994f12515eb8af836e6dafdb0289bf835
marscher <m.scherer@fu-berlin.de>
2017-12-19 13:30:34 +0100

    fix

--- pyemma/_base/serialization/tests/test_serialization.py
+++ pyemma/_base/serialization/tests/test_serialization.py
@@ -9,7 +9,7 @@ import pyemma
 from pyemma._base.serialization.serialization import DeveloperError
 from pyemma._base.serialization.serialization import SerializableMixIn
 from ._test_classes import (test_cls_v1, test_cls_v2, test_cls_v3, _deleted_in_old_version, test_cls_with_old_locations,
-                            to_interpolate_with_functions, test_cls_property, test_cls_property_v2)
+                            to_interpolate_with_functions, )
 
 
 class np_container(SerializableMixIn):
