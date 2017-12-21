c36e813829e9cf06a97242d644b483fae80dd2b7
Martin K. Scherer <m.scherer@fu-berlin.de>
2017-12-22 00:07:03 +0100

    [pickler] remember references to already stored objects.

--- pyemma/_base/serialization/pickle_extensions.py
+++ pyemma/_base/serialization/pickle_extensions.py
@@ -31,24 +31,26 @@ class HDF5PersistentPickler(Pickler):
     def __init__(self, group, file):
         super().__init__(file=file, protocol=4)
         self.group = group
-        from itertools import count
-        self.next_array_id = count(0)
+        self._seen_ids = []
 
     def _store(self, array):
-        array_id = next(self.next_array_id)
-        self.group.create_dataset(name=str(array_id), data=array,
+        id_ = id(array)
+        self.group.create_dataset(name=str(id_), data=array,
                                   chunks=True, compression='gzip', compression_opts=4, shuffle=True)
-        return array_id
+        self._seen_ids.append(id_)
+        return id_
 
     def persistent_id(self, obj):
-        if isinstance(obj, np.ndarray) and obj.dtype != np.object_:
+        if (isinstance(obj, np.ndarray) and obj.dtype != np.object_
+                and id(obj) not in self._seen_ids):
             array_id = self._store(obj)
             return 'np_array', array_id
 
-        if isinstance(obj, mdtraj.Topology):
+        if isinstance(obj, mdtraj.Topology) and id(obj) not in self._seen_ids:
             atoms, bonds = topology_to_numpy(obj)
             atom_i = self._store(atoms)
             bond_i = self._store(bonds)
+            self._seen_ids.append(id(obj))
             return 'md/Topology', (atom_i, bond_i)
 
         return None
