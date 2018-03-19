057bef5816bc0bca1e7cc702a20cf8a537d8e66d
marscher <m.scherer@fu-berlin.de>
2018-03-19 14:39:07 +0100

    lower dt.

--- pyemma/datasets/api.py
+++ pyemma/datasets/api.py
@@ -120,7 +120,7 @@ def get_multi_temperature_data(kt0=1.0, kt1=5.0, length0=10000, length1=10000, n
     return mt_data
 
 
-def get_quadwell_data(ntraj=10, nstep=10000, x0=0., nskip=1, dt=0.01, kT=1.0, mass=1.0, damping=1.0):
+def get_quadwell_data(ntraj=10, nstep=10000, x0=0., nskip=1, dt=0.001, kT=1.0, mass=1.0, damping=1.0):
     r""" Performs a Brownian dynamics simulation in the Prinz potential (quad well).
 
     Parameters
@@ -133,7 +133,7 @@ def get_quadwell_data(ntraj=10, nstep=10000, x0=0., nskip=1, dt=0.01, kT=1.0, ma
         starting point for sampling
     nskip: int, default=1
         number of integrator steps
-    dt: float, default=0.01
+    dt: float, default=0.001
         time step size
     kT: float, default=1.0
         temperature factor
