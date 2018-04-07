[coordinates/readers] fixed default chunksize calculation and return correct dtype for data in memory [coordinates/pipeline] pass chunksize into estimate

pyemma/coordinates/data/_base/iterable.py
@@ -51,9 +51,11 @@ class Iterable(six.with_metaclass(ABCMeta, InMemoryMixin, Loggable)):
 
         # TODO: consider rounding this to some cache size of CPU? e.g py-cpuinfo can obtain it.
         # if one time step is already bigger than max_memory, we set the chunksize to 1.
-        max_elements = max(1, int(np.floor(max_bytes / (itemsize * dim))))
-        assert max_elements * dim * itemsize <= max_bytes or max_elements == 1
-        result = max(1, max_elements // dim)
+        bytes_per_frame = itemsize * dim
+        max_frames = max(1, int(np.floor(max_bytes / bytes_per_frame)))
+        assert max_frames * dim * itemsize <= max_bytes or max_frames == 1, \
+            "number of frames times dim times sizeof(dtype) should be smaller or equal than max_bytes"
+        result = max_frames
 
         assert result > 0
         if logger is not None:
@@ -77,7 +79,7 @@ class Iterable(six.with_metaclass(ABCMeta, InMemoryMixin, Loggable)):
                 self._default_chunksize = Iterable._FALLBACK_CHUNKSIZE
             else:
                 self._default_chunksize = Iterable._compute_default_cs(self.dimension(),
-                                                                       self.output_type()().itemsize, self.logger)
+                                                                       self.output_type().itemsize, self.logger)
         return self._default_chunksize
 
     @property
@@ -172,7 +174,7 @@ class Iterable(six.with_metaclass(ABCMeta, InMemoryMixin, Loggable)):
 
     def output_type(self):
         r""" By default transformers return single precision floats. """
-        return np.float32
+        return np.float32()
 
     def __iter__(self):
         return self.iterator()

pyemma/coordinates/tests/test_sources_merger.py
@@ -44,8 +44,8 @@ class TestSourcesMerger(unittest.TestCase):
         for r in self.readers:
             for i, x in enumerate(r.get_output(stride=stride, chunk=chunk, skip=skip)):
                 outs[i].append(x)
-        combined = [np.hstack(outs[i]) for i in range(3)]
-        np.testing.assert_equal(out, combined)
+        combined = [np.hstack(outs[i]).astype(np.float32) for i in range(3)]
+        np.testing.assert_equal([o.astype(np.float32) for o in out], combined)
 
     def test_combined_output(self):
         j = SourcesMerger(self.readers)
@@ -57,7 +57,6 @@ class TestSourcesMerger(unittest.TestCase):
     def test_ra_stride(self):
         ra_indices = np.array([[0,7], [0, 23], [1, 30], [2, 9]])
         j = SourcesMerger(self.readers)
-
         self._get_output_compare(j, stride=ra_indices)
 
     def test_non_matching_lengths(self):

pyemma/coordinates/tests/test_tica.py
@@ -372,7 +372,7 @@ class TestTICAExtensive(unittest.TestCase):
         self.tica_obj.number_of_trajectories() == 1
 
     def test_output_type(self):
-        assert self.tica_obj.output_type() == np.float32
+        assert self.tica_obj.output_type() == np.float32()
 
     def test_trajectory_length(self):
         assert self.tica_obj.trajectory_length(0) == self.T