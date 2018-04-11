55bcbfb9b07de5d937defebfb109c89d42a7759d
marscher <m.scherer@fu-berlin.de>
2018-04-11 17:09:03 +0200

    added test

--- pyemma/coordinates/tests/test_sources_merger.py
+++ pyemma/coordinates/tests/test_sources_merger.py
@@ -4,7 +4,7 @@ import os
 from glob import glob
 import numpy as np
 
-from pyemma.coordinates import source
+from pyemma.coordinates import source, tica
 from pyemma.coordinates.data.sources_merger import SourcesMerger
 from pyemma import config
 
@@ -32,12 +32,13 @@ class TestSourcesMerger(unittest.TestCase):
         arrays = [np.random.random( (length, ndim) ) for length in lengths]
         self.readers.append(source(arrays))
 
+        self.readers.append(tica(self.readers[-1], dim=20))
+
     def _get_output_compare(self, joiner, stride=1, chunk=0, skip=0):
-        j = joiner
-        out = j.get_output(stride=stride, chunk=chunk, skip=skip)
-        assert len(out) == 3
-        assert j.ndim == self.readers[0].ndim * 2
-        np.testing.assert_equal(j.trajectory_lengths(), self.readers[0].trajectory_lengths())
+        out = joiner.get_output(stride=stride, chunk=chunk, skip=skip)
+        assert len(out) == 3  # 3 trajs
+        assert joiner.ndim == sum(r.dimension() for r in self.readers)
+        np.testing.assert_equal(joiner.trajectory_lengths(), self.readers[0].trajectory_lengths())
 
         from collections import defaultdict
         outs = defaultdict(list)
