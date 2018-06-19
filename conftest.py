1dd821dcf93cc6ccc4c73ab4acf9c0e634d83bac
marscher <m.scherer@fu-berlin.de>
2018-06-19 16:02:07 +0200

    [conftest] distribute tests

--- conftest.py
+++ conftest.py
@@ -1,5 +1,6 @@
 import pytest
 import sys
+import os
 
 # this is import for measuring coverage
 assert 'pyemma' not in sys.modules
@@ -34,3 +35,38 @@ def filter_warnings():
     warnings.filterwarnings('ignore', message='You have not selected any features. Returning plain coordinates.')
     yield
     warnings.filters = old_filters
+
+
+def pytest_collection_modifyitems(session, config, items):
+
+    circle_node_total, circle_node_index = read_circleci_env_variables()
+    deselected = []
+    # round robbin:
+    # first group by parent, then skip by ci node index
+    from collections import defaultdict
+    by_parents = defaultdict(list)
+    for index, item in enumerate(items):
+        by_parents[item.parent].append(item)
+    for i, p in enumerate(by_parents.keys()):
+        if i % circle_node_total != circle_node_index:
+            deselected.extend(by_parents[p])
+    for d in deselected:
+        items.remove(d)
+    config.hook.pytest_deselected(items=deselected)
+
+
+def read_circleci_env_variables():
+    """Read and convert CIRCLE_* environment variables"""
+    circle_node_total = int(os.environ.get("CIRCLE_NODE_TOTAL", "1").strip() or "1")
+    circle_node_index = int(os.environ.get("CIRCLE_NODE_INDEX", "0").strip() or "0")
+
+    if circle_node_index >= circle_node_total:
+        raise RuntimeError("CIRCLE_NODE_INDEX={} >= CIRCLE_NODE_TOTAL={}, should be less".format(circle_node_index, circle_node_total))
+
+    return circle_node_total, circle_node_index
+
+
+def pytest_report_header(config):
+    """Add CircleCI information to report"""
+    circle_node_total, circle_node_index = read_circleci_env_variables()
+    return "CircleCI total nodes: {}, this node index: {}".format(circle_node_total, circle_node_index)
