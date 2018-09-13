ca0a938b3d2bbea05e76942f7d382b2b171ad0a9
marscher <m.scherer@fu-berlin.de>
2018-09-13 16:51:04 +0200

    fix deprecation warning, scipy.misc -> scipy.special

--- pyemma/util/indices.py
+++ pyemma/util/indices.py
@@ -44,7 +44,7 @@ def combinations(seq, k):
      [1 2]]
     """
     from itertools import combinations as _combinations, chain
-    from scipy.misc import comb
+    from scipy.special import comb
 
     count = comb(len(seq), k, exact=True)
     res = np.fromiter(chain.from_iterable(_combinations(seq, k)),
