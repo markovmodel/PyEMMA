fa85431d93fc16e366dbb8c521e75a363cbda127
Moritz Hoffmann <clonker@gmail.com>
2021-07-19 18:32:36 +0200

    remove xdist

--- devtools/conda-recipe/run_test.py
+++ devtools/conda-recipe/run_test.py
@@ -18,21 +18,18 @@ junit_xml = os.path.join(xml_results_dest, 'junit.xml')
 cov_xml = os.path.join(xml_results_dest, 'coverage.xml')
 
 print('junit destination:', junit_xml)
-njobs_args = '-p no:xdist' if os.getenv('TRAVIS') or os.getenv('CIRCLECI') else '-n2'
+# njobs_args = '-p no:xdist' if os.getenv('TRAVIS') or os.getenv('CIRCLECI') else '-n2'
 
 pytest_args = ("-v --pyargs {test_pkg} "
                "--cov={cover_pkg} "
                "--cov-report=xml:{dest_report} "
                "--doctest-modules "
-               "{njobs_args} "
                "--junit-xml={junit_xml} "
                "-c {pytest_cfg}"
                "--durations=20 "
                .format(test_pkg=test_pkg, cover_pkg=cover_pkg,
                        junit_xml=junit_xml, pytest_cfg='setup.cfg',
-                       dest_report=cov_xml,
-                       njobs_args=njobs_args,
-                       )
+                       dest_report=cov_xml)
                .split(' '))
 print("args:", pytest_args)
 res = pytest.main(pytest_args)
