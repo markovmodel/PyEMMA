f691cfbb3ca69438bcdf791b15eeb4d5afb4bbe8
Moritz Hoffmann <clonker@gmail.com>
2021-07-19 15:35:55 +0200

    [CI] azure pipelines setup

--- devtools/conda-recipe/run_test.py
+++ devtools/conda-recipe/run_test.py
@@ -1,5 +1,6 @@
 import os
 import sys
+import tempfile
 
 import pytest
 
@@ -7,11 +8,15 @@ test_pkg = 'pyemma'
 cover_pkg = test_pkg
 
 # where to write junit xml
-junit_xml = os.path.join(os.getenv('CIRCLE_TEST_REPORTS', os.path.expanduser('~')),
-                         'reports', 'junit.xml')
-target_dir = os.path.dirname(junit_xml)
+xml_results_dest = os.getenv('SYSTEM_DEFAULTWORKINGDIRECTORY', tempfile.gettempdir())
+assert os.path.isdir(xml_results_dest), 'no dest dir available'
+target_dir = os.path.dirname(xml_results_dest)
 if not os.path.exists(target_dir):
     os.makedirs(target_dir)
+
+junit_xml = os.path.join(xml_results_dest, 'junit.xml')
+cov_xml = os.path.join(xml_results_dest, 'coverage.xml')
+
 print('junit destination:', junit_xml)
 njobs_args = '-p no:xdist' if os.getenv('TRAVIS') or os.getenv('CIRCLECI') else '-n2'
 
@@ -22,10 +27,10 @@ pytest_args = ("-v --pyargs {test_pkg} "
                "{njobs_args} "
                "--junit-xml={junit_xml} "
                "-c {pytest_cfg}"
-               #"--durations=20 "
+               "--durations=20 "
                .format(test_pkg=test_pkg, cover_pkg=cover_pkg,
                        junit_xml=junit_xml, pytest_cfg='setup.cfg',
-                       dest_report=os.path.join(os.path.expanduser('~/'), 'coverage.xml'),
+                       dest_report=cov_xml,
                        njobs_args=njobs_args,
                        )
                .split(' '))
